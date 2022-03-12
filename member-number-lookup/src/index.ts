562632546638561c5198342757637236afe2a5bc
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-12 13:56:34 +0000

    Whoops page if email not in req.body

--- member-number-lookup/src/index.ts
+++ member-number-lookup/src/index.ts
@@ -1,8 +1,10 @@
 import express, {Application, Request, Response} from 'express';
+import {pipe} from 'fp-ts/lib/function';
 import path from 'path';
 import {checkYourMailPage} from './check-your-mail-page';
 import {invalidEmailPage} from './invalid-email-page';
 import {landingPage} from './landing-page';
+import * as E from 'fp-ts/Either';
 
 const app: Application = express();
 const port = 8080;
@@ -18,14 +20,18 @@ app.get('/check-your-mail', (req: Request, res: Response) => {
   res.status(200).send(checkYourMailPage(email.toString()));
 });
 
-app.get('/invalid-email', (req: Request, res: Response) => {
-  res.status(400).send(invalidEmailPage);
-});
-
 app.use('/static', express.static(path.resolve(__dirname, './static')));
 
 app.post('/send-member-number-by-email', (req: Request, res: Response) => {
-  res.redirect(`/check-your-mail?email=${req.body.email}`);
+  pipe(
+    req.body.email,
+    E.fromNullable(() => "Error: No 'email' field on body"),
+    E.map(() => 'foo'),
+    E.matchW(
+      () => res.status(400).send(invalidEmailPage),
+      email => res.redirect(`/check-your-mail?email=${email}`)
+    )
+  );
 });
 
 app.listen(port, () =>
