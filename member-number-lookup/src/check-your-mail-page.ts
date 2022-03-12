fb273c41d33cee7376c0b7f7e53b383ee0e2985f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-12 13:28:33 +0000

    Tell users to check their email after they have submitted

---
+++ member-number-lookup/src/check-your-mail-page.ts
@@ -0,0 +1,37 @@
+export const checkYourMailPage = `
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<title>MS Member Number Lookup</title>
+			<link rel="stylesheet" href="static/vanilla.css">
+
+			<!-- Generated using https://realfavicongenerator.net -->
+			<link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png">
+			<link rel="manifest" href="/static/favicons/site.webmanifest">
+			<link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5">
+			<link rel="shortcut icon" href="/static/favicons/favicon.ico">
+			<meta name="msapplication-TileColor" content="#2b5797">
+			<meta name="msapplication-config" content="/static/favicons/browserconfig.xml">
+			<meta name="theme-color" content="#ffffff">
+
+		</head>
+		<body>
+			<h1>Check your mail</h1>
+
+			<p>
+				If this email address is linked to a Makespace number you should receive an email with that number.
+			</p>
+
+			<p>
+				If nothing happens please reach out to the Makespace Database Team.
+			</p>
+
+		</form>
+		</body>
+	</html>
+`;
--- member-number-lookup/src/index.ts
+++ member-number-lookup/src/index.ts
@@ -1,5 +1,6 @@
 import express, {Application, Request, Response} from 'express';
 import path from 'path';
+import {checkYourMailPage} from './check-your-mail-page';
 import {landingPage} from './landing-page';
 
 const app: Application = express();
@@ -9,7 +10,15 @@ app.get('/', (req: Request, res: Response) => {
   res.status(200).send(landingPage);
 });
 
+app.get('/check-your-mail', (req: Request, res: Response) => {
+  res.status(200).send(checkYourMailPage);
+});
+
 app.use('/static', express.static(path.resolve(__dirname, './static')));
+
+app.post('/send-member-number-by-email', (req: Request, res: Response) => {
+  res.redirect('/check-your-mail');
+});
 
 app.listen(port, () =>
   process.stdout.write(`Server is listening on port ${port}!\n`)
