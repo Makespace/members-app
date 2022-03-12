6325a8970e73a0f0473df7266203026403fa4f76
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-12 13:43:24 +0000

    Pass email from form all the way to check-your-mail

--- member-number-lookup/src/index.ts
+++ member-number-lookup/src/index.ts
@@ -6,18 +6,21 @@ import {landingPage} from './landing-page';
 const app: Application = express();
 const port = 8080;
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send(landingPage);
 });
 
 app.get('/check-your-mail', (req: Request, res: Response) => {
-  res.status(200).send(checkYourMailPage('foo'));
+  const email = req.query.email ?? '<no email supplied>';
+  res.status(200).send(checkYourMailPage(email.toString()));
 });
 
 app.use('/static', express.static(path.resolve(__dirname, './static')));
 
 app.post('/send-member-number-by-email', (req: Request, res: Response) => {
-  res.redirect('/check-your-mail');
+  res.redirect(`/check-your-mail?email=${req.body.email}`);
 });
 
 app.listen(port, () =>
