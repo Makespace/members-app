aaf7bc1fe137ad9b335e47c4fb05a33c7a584bd6
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-12 15:33:24 +0000

    Stricter type on sendMemberNumberByEmail return

--- member-number-lookup/src/send-member-number-by-email.ts
+++ member-number-lookup/src/send-member-number-by-email.ts
@@ -2,17 +2,20 @@ import * as E from 'fp-ts/Either';
 import {flow, pipe} from 'fp-ts/lib/function';
 import * as t from 'io-ts';
 import {formatValidationErrors} from 'io-ts-reporters';
+import {Email, EmailCodec} from './email';
 
 const ParamsCodec = t.type({
-  email: t.string,
+  email: EmailCodec,
 });
 
 export const sendMemberNumberByEmail = (
   params: unknown
-): E.Either<string, string> =>
+): E.Either<string, Email> =>
   pipe(
     params,
     ParamsCodec.decode,
-    E.mapLeft(flow(formatValidationErrors, errors => errors.join('\n'))),
-    E.map(() => 'foo')
+    E.bimap(
+      flow(formatValidationErrors, errors => errors.join('\n')),
+      ({email}) => email
+    )
   );
