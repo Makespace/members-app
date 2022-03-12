db414335fa5eff20df366f6e4c9e8a606b2f88d9
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-12 13:02:32 +0000

    Extract html to separate file and add HTML5 boilerplate

--- member-number-lookup/src/index.ts
+++ member-number-lookup/src/index.ts
@@ -1,12 +1,11 @@
 import express, {Application, Request, Response} from 'express';
+import {landingPage} from './landing-page';
 
 const app: Application = express();
 const port = 8080;
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send(`
-    <h1>Makespace Member Number Lookup</h1>
-  `);
+  res.status(200).send(landingPage);
 });
 
 app.listen(port, () =>
