f8b205c0643bee082ed4baff0064b75dfdc58e74
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-12 14:36:29 +0000

    Expand email test list

--- member-number-lookup/tests/email.test.ts
+++ member-number-lookup/tests/email.test.ts
@@ -2,10 +2,18 @@ import * as E from 'fp-ts/Either';
 import {EmailCodec} from '../src/email';
 
 describe('email', () => {
-  describe('when given an empty string', () => {
-    const result = EmailCodec.decode('');
-    it('returns Left', () => {
-      expect(E.isLeft(result)).toBe(true);
+  describe.each([[''], ['foo']])(
+    'when given something that is not an email',
+    (input: unknown) => {
+      const result = EmailCodec.decode(input);
+      it.todo('returns Left');
+    }
+  );
+
+  describe.skip('when given an email', () => {
+    const result = EmailCodec.decode('foo@example.com');
+    it('returns Right', () => {
+      expect(E.isRight(result)).toBe(true);
     });
   });
 });
