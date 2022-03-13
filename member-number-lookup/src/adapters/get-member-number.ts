3714a4b8a3cf5ddb096ec7ce1d95428c23d3eed5
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-13 19:18:19 +0000

    Catch db errors and return them nicely to logger

--- member-number-lookup/src/adapters/get-member-number.ts
+++ member-number-lookup/src/adapters/get-member-number.ts
@@ -1,25 +1,35 @@
 import * as TE from 'fp-ts/TaskEither';
 import {Email} from '../types';
 import mysql from 'mysql';
+import {identity, pipe} from 'fp-ts/lib/function';
 
 type GetMemberNumber = (email: Email) => TE.TaskEither<string, number>;
 
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+});
+
 export const getMemberNumber = (): GetMemberNumber => {
-  const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-  });
-
-  connection.connect(err => {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-
-    console.log('connected as id ' + connection.threadId);
-  });
-
-  return () => TE.left('getMemberNumber not implemented');
+  return () =>
+    pipe(
+      TE.tryCatch(
+        () =>
+          new Promise((resolve, reject) => {
+            pool.query(
+              'SELECT Given_Member_Number FROM InductionFormResponse',
+              (error, elements) => {
+                if (error) {
+                  return reject(error);
+                }
+                return resolve(elements);
+              }
+            );
+          }),
+        identity
+      ),
+      TE.bimap(String, () => 42)
+    );
 };
