ec62d18644f4a5f02de00aa27437420a5be1e1d6
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-13 19:04:16 +0000

    Connect app to empy local db

--- member-number-lookup/src/adapters/get-member-number.ts
+++ member-number-lookup/src/adapters/get-member-number.ts
@@ -1,7 +1,25 @@
 import * as TE from 'fp-ts/TaskEither';
 import {Email} from '../types';
+import mysql from 'mysql';
 
 type GetMemberNumber = (email: Email) => TE.TaskEither<string, number>;
 
-export const getMemberNumber = (): GetMemberNumber => () =>
-  TE.left('getMemberNumber not implemented');
+export const getMemberNumber = (): GetMemberNumber => {
+  const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  });
+
+  connection.connect(err => {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+  });
+
+  return () => TE.left('getMemberNumber not implemented');
+};
