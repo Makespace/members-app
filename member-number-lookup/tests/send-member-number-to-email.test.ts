b9b84644f8c0abcd196a0d22c7933a2f8bbebe70
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-13 11:30:21 +0000

    Implement passing todo test

--- member-number-lookup/tests/send-member-number-to-email.test.ts
+++ member-number-lookup/tests/send-member-number-to-email.test.ts
@@ -25,7 +25,18 @@ describe('send-member-number-to-email', () => {
   });
 
   describe('when the email has no matches in database', () => {
-    it.todo('does not send any emails');
+    const email = faker.internet.email() as Email;
+    const adapters = {
+      sendMemberNumberEmail: jest.fn(() => TE.right(undefined)),
+      getMemberNumberForEmail: () => TE.left('no member number found'),
+    };
+
+    beforeEach(async () => {
+      await sendMemberNumberToEmail(adapters)(email);
+    });
+    it('does not send any emails', () => {
+      expect(adapters.sendMemberNumberEmail).not.toHaveBeenCalled();
+    });
     it.todo('logs an info');
   });
 
