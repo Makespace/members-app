2edb6b2b709dadb4243f53b3031249351eaf09ab
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2022-03-17 21:02:58 +0000

    Move rate limit logic into pipe

--- member-number-lookup/src/adapters/rate-limit-sending-of-emails.ts
+++ member-number-lookup/src/adapters/rate-limit-sending-of-emails.ts
@@ -2,6 +2,7 @@ import * as TE from 'fp-ts/TaskEither';
 import {Email, EmailAddress, failure, Failure} from '../types';
 import * as A from 'fp-ts/Array';
 import {pipe} from 'fp-ts/lib/function';
+import * as B from 'fp-ts/boolean';
 
 type PastActivity = Array<{emailAddress: EmailAddress; timestamp: number}>;
 
@@ -15,20 +16,25 @@ export const rateLimitSendingOfEmails = (
 
   return email => {
     const startOfCurrentWindow = Date.now() - timeWindowInSeconds;
-    const emailsSentToThisAddress = pipe(
+    return pipe(
       pastActivity,
       A.filter(activity => activity.emailAddress === email.recipient),
       A.filter(({timestamp}) => timestamp >= startOfCurrentWindow),
-      A.size
+      A.size,
+      sentInCurrentWindow => sentInCurrentWindow < limit,
+      B.match(
+        () =>
+          TE.left(
+            failure('Email rate limit exceeded')({email, limitPerDay: limit})
+          ),
+        () => {
+          pastActivity.push({
+            emailAddress: email.recipient,
+            timestamp: Date.now(),
+          });
+          return TE.right(email);
+        }
+      )
     );
-
-    if (emailsSentToThisAddress >= limit) {
-      return TE.left(
-        failure('Email rate limit exceeded')({email, limitPerDay: limit})
-      );
-    }
-
-    pastActivity.push({emailAddress: email.recipient, timestamp: Date.now()});
-    return TE.right(email);
   };
 };
