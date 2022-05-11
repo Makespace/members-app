44df44389fafe44e5e7b25334544ecc8ca15acec
Daniel Haarhoff <daniel@rknt.de>
2022-05-11 16:27:41 +0100

    Rudimentary client side validation of form

--- member-number-lookup/src/pages/landing.ts
+++ member-number-lookup/src/pages/landing.ts
@@ -6,7 +6,7 @@ export const landingPage = pipe(
 		<h1>Lookup Your Member Number</h1>
 		<form action="/send-member-number-by-email" method="post">
 			<label for="email">E-Mail: </label>
-			<input id="email" type="text" name="email" value="">
+			<input id="email" type="email" required name="email" value="">
 			<input type="submit" value="Send Member Number">
 		</form>
 	`,
