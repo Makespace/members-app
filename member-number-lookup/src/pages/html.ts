d3531366cffed06a788c7e88417f8442c480fd4b
Daniel Haarhoff <daniel@rknt.de>
2023-06-25 14:08:30 +0100

    Allow html template string tag to be used on template strings containing no substitutions

--- member-number-lookup/src/pages/html.ts
+++ member-number-lookup/src/pages/html.ts
@@ -2,6 +2,9 @@ export const html = (
   literals: TemplateStringsArray,
   ...substitutions: ReadonlyArray<string | number>
 ): string => {
+  if (literals.length === 1 && substitutions.length === 0) {
+    return literals[0];
+  }
   let result = '';
   for (let index = 0; index < substitutions.length; index++) {
     result += literals[index];
