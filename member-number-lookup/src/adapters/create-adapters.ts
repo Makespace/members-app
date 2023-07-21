bc6951e06b7a3e12925c2d88c4842eeb8a1690f4
Daniel Haarhoff <daniel@rknt.de>
2023-07-21 18:25:12 +0100

    Extract getAllEvents

--- member-number-lookup/src/adapters/create-adapters.ts
+++ member-number-lookup/src/adapters/create-adapters.ts
@@ -1,9 +1,6 @@
 import {Config} from '../configuration';
-import * as t from 'io-ts';
-import * as TE from 'fp-ts/TaskEither';
 import {Dependencies} from '../dependencies';
 import {getMemberNumber} from './get-member-number';
-import * as E from 'fp-ts/Either';
 import {getMemberNumberStubbed} from './get-member-number-stubbed';
 import {createRateLimiter} from './rate-limit-sending-of-emails';
 import {sendEmail} from './send-email';
@@ -13,12 +10,7 @@ import smtp from 'nodemailer-smtp-transport';
 import {getTrainersStubbed} from './get-trainers-stubbed';
 import {commitEvent} from './commit-event';
 import {QueryDatabase} from './query-database';
-import {flow, pipe} from 'fp-ts/lib/function';
-import {DomainEvent} from '../types';
-import {formatValidationErrors} from 'io-ts-reporters';
-import {failureWithStatus} from '../types/failureWithStatus';
-import {StatusCodes} from 'http-status-codes';
-import * as tt from 'io-ts-types';
+import {getAllEvents} from './get-all-events';
 
 export const createAdapters = (
   conf: Config,
@@ -43,48 +35,9 @@ export const createAdapters = (
     })
   );
 
-  const EventsFromDb = t.readonlyArray(
-    t.strict({
-      id: t.string,
-      resource_id: t.string,
-      resource_type: t.string,
-      event_type: t.string,
-      payload: t.string,
-    })
-  );
-
   return {
     commitEvent: commitEvent(queryDatabase),
-    getAllEvents: () =>
-      pipe(
-        queryDatabase('SELECT * FROM events;', []),
-        TE.chainEitherK(
-          flow(
-            EventsFromDb.decode,
-            E.chain(
-              E.traverseArray(raw =>
-                pipe(
-                  raw.payload,
-                  tt.JsonFromString.decode,
-                  E.chain(tt.JsonRecord.decode),
-                  E.map(payload => ({
-                    type: raw.event_type,
-                    ...payload,
-                  }))
-                )
-              )
-            ),
-            E.chain(t.readonlyArray(DomainEvent).decode),
-            E.mapLeft(formatValidationErrors),
-            E.mapLeft(
-              failureWithStatus(
-                'Failed to get events from DB',
-                StatusCodes.INTERNAL_SERVER_ERROR
-              )
-            )
-          )
-        )
-      ),
+    getAllEvents: getAllEvents(queryDatabase),
     getMemberNumber: conf.USE_STUBBED_ADAPTERS
       ? getMemberNumberStubbed()
       : getMemberNumber(queryDatabase),
