3fda918375f999064d482fad0b5b38ad35d53db7
Daniel Haarhoff <daniel@rknt.de>
2023-10-26 14:57:03 +0100

    [member service] describe behaviour of super-users page with tests

---
+++ member-number-lookup/tests/routes/super-users/construct-view-model.test.ts
@@ -0,0 +1,48 @@
+import * as E from 'fp-ts/Either';
+import {pipe} from 'fp-ts/lib/function';
+import {arbitraryUser} from '../../types/user.helper';
+import {constructViewModel} from '../../../src/routes/super-users/construct-view-model';
+import {Dependencies} from '../../../src/dependencies';
+import {happyPathAdapters} from '../../adapters/happy-path-adapters.helper';
+import * as TE from 'fp-ts/TaskEither';
+import {constructEvent} from '../../../src/types';
+import * as T from 'fp-ts/Task';
+import {faker} from '@faker-js/faker';
+import * as RA from 'fp-ts/ReadonlyArray';
+
+const shouldNotBeCalled = (): never => {
+  throw new Error('should not be called');
+};
+
+describe('construct-view-model', () => {
+  it('succeeds if the logged in user is not a super user', async () => {
+    const memberToBeSuperUser = arbitraryUser();
+    const deps: Dependencies = {
+      ...happyPathAdapters,
+      getAllEvents: () =>
+        TE.right([
+          constructEvent('SuperUserDeclared')({
+            memberNumber: memberToBeSuperUser.memberNumber,
+            declaredAt: faker.date.anytime(),
+          }),
+        ]),
+    };
+
+    const result = await pipe(
+      arbitraryUser(),
+      constructViewModel(deps),
+      TE.getOrElse(shouldNotBeCalled),
+      T.map(viewModel => viewModel.superUsers),
+      T.map(RA.map(superUser => superUser.memberNumber))
+    )();
+
+    expect(result).toStrictEqual([memberToBeSuperUser.memberNumber]);
+  });
+
+  it.skip('fails if the logged in user is not a super user', async () => {
+    const deps: Dependencies = happyPathAdapters;
+    const result = await pipe(arbitraryUser(), constructViewModel(deps))();
+
+    expect(result).toStrictEqual(E.left(expect.anything));
+  });
+});
