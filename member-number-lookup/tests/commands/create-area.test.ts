127f7f414540d2a613d8042153b9cf81e6218bd2
Daniel Haarhoff <daniel@rknt.de>
2023-10-26 10:59:42 +0100

    [member service] make space in test for new responsibility

--- member-number-lookup/tests/commands/create-area.test.ts
+++ member-number-lookup/tests/commands/create-area.test.ts
@@ -5,46 +5,48 @@ import {constructEvent} from '../../src/types';
 import {NonEmptyString} from 'io-ts-types';
 
 describe('create-area', () => {
-  describe('when the area does not yet exist', () => {
-    const areaName = faker.commerce.productName() as NonEmptyString;
-    const areaDescription = faker.commerce.productDescription();
-    const result = createArea.process({
-      command: {
-        name: areaName,
-        description: areaDescription,
-      },
-      events: [],
-    });
-    it('creates the area', () => {
-      expect(result).toStrictEqual(
-        O.some(
-          expect.objectContaining({
-            type: 'AreaCreated',
-            name: areaName,
-            description: areaDescription,
-          })
-        )
-      );
-    });
-  });
-
-  describe('when the area already exists', () => {
-    const areaName = faker.commerce.productName() as NonEmptyString;
-    const areaDescription = faker.commerce.productDescription();
-    const result = createArea.process({
-      command: {
-        name: areaName,
-        description: areaDescription,
-      },
-      events: [
-        constructEvent('AreaCreated')({
+  describe('process', () => {
+    describe('when the area does not yet exist', () => {
+      const areaName = faker.commerce.productName() as NonEmptyString;
+      const areaDescription = faker.commerce.productDescription();
+      const result = createArea.process({
+        command: {
           name: areaName,
           description: areaDescription,
-        }),
-      ],
+        },
+        events: [],
+      });
+      it('creates the area', () => {
+        expect(result).toStrictEqual(
+          O.some(
+            expect.objectContaining({
+              type: 'AreaCreated',
+              name: areaName,
+              description: areaDescription,
+            })
+          )
+        );
+      });
     });
-    it('does nothing', () => {
-      expect(result).toStrictEqual(O.none);
+
+    describe('when the area already exists', () => {
+      const areaName = faker.commerce.productName() as NonEmptyString;
+      const areaDescription = faker.commerce.productDescription();
+      const result = createArea.process({
+        command: {
+          name: areaName,
+          description: areaDescription,
+        },
+        events: [
+          constructEvent('AreaCreated')({
+            name: areaName,
+            description: areaDescription,
+          }),
+        ],
+      });
+      it('does nothing', () => {
+        expect(result).toStrictEqual(O.none);
+      });
     });
   });
 });
