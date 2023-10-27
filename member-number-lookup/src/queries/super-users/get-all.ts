f3ca2586112d164516c374760c2285df03a09053
Daniel Haarhoff <daniel@rknt.de>
2023-10-27 10:07:00 +0100

    [member service] specify desired superUsers.getAll behaviour

--- member-number-lookup/src/queries/super-users/get-all.ts
+++ member-number-lookup/src/queries/super-users/get-all.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {DomainEvent} from '../../types';
 
-// ts-unused-exports:disable-next-line
-export const getAll = () => (_events: ReadonlyArray<DomainEvent>) => [];
+export const getAll =
+  () =>
+  (
+    _events: ReadonlyArray<DomainEvent>
+  ): ReadonlyArray<{memberNumber: number; since: Date}> => [];
--- member-number-lookup/tests/queries/super-users/get-all.test.ts
+++ member-number-lookup/tests/queries/super-users/get-all.test.ts
@@ -1,13 +1,59 @@
+import {faker} from '@faker-js/faker';
+import * as RA from 'fp-ts/ReadonlyArray';
+import {pipe} from 'fp-ts/lib/function';
+import {constructEvent} from '../../../src/types';
+import {arbitraryUser} from '../../types/user.helper';
+import {getAll} from '../../../src/queries/super-users/get-all';
+
 describe('get-all', () => {
   describe('when there are no super-users', () => {
-    it.todo('returns an empty array');
+    const superUsers = pipe([], getAll());
+    it('returns an empty array', () => {
+      expect(superUsers).toStrictEqual([]);
+    });
   });
 
   describe('when there are super-users', () => {
-    it.todo('returns them');
+    const user = arbitraryUser();
+    const superUsers = pipe(
+      [
+        constructEvent('SuperUserDeclared')({
+          memberNumber: user.memberNumber,
+          declaredAt: faker.date.past(),
+        }),
+      ],
+      getAll(),
+      RA.map(superUser => superUser.memberNumber)
+    );
+
+    it.failing('returns them', () => {
+      expect(superUsers).toStrictEqual([user.memberNumber]);
+    });
   });
 
   describe('when a super-user status was revoked', () => {
-    it.todo('does not return that user');
+    const revokedUser = arbitraryUser();
+    const nonRevokedUser = arbitraryUser();
+    const superUsers = pipe(
+      [
+        constructEvent('SuperUserDeclared')({
+          memberNumber: revokedUser.memberNumber,
+          declaredAt: faker.date.past(),
+        }),
+        constructEvent('SuperUserDeclared')({
+          memberNumber: nonRevokedUser.memberNumber,
+          declaredAt: faker.date.past(),
+        }),
+        constructEvent('SuperUserRevoked')({
+          memberNumber: revokedUser.memberNumber,
+          revokedAt: faker.date.past(),
+        }),
+      ],
+      getAll(),
+      RA.map(superUser => superUser.memberNumber)
+    );
+    it.failing('does not return that user', () => {
+      expect(superUsers).toStrictEqual([nonRevokedUser.memberNumber]);
+    });
   });
 });
