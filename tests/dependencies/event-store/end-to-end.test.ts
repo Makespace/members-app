1ed37cb19bf7c86b3d51e17fac660f0c50b6593d
Daniel Haarhoff <daniel@rknt.de>
2024-05-08 16:49:38 +0100

    Make it easier to understand how a test is failing

--- tests/dependencies/event-store/end-to-end.test.ts
+++ tests/dependencies/event-store/end-to-end.test.ts
@@ -1,3 +1,5 @@
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
 import {faker} from '@faker-js/faker';
 import {Dependencies} from '../../../src/dependencies';
 import {ensureEventTableExists} from '../../../src/init-dependencies/event-store/ensure-event-table-exists';
@@ -6,6 +8,9 @@ import {constructEvent} from '../../../src/types';
 import {UUID} from 'io-ts-types';
 import {commitEvent} from '../../../src/init-dependencies/event-store/commit-event';
 import {getAllEvents} from '../../../src/init-dependencies/event-store/get-all-events';
+import {pipe} from 'fp-ts/lib/function';
+import {error} from 'console';
+import {shouldNotBeCalled} from '../../should-not-be-called.helper';
 
 describe('event-store end-to-end', () => {
   describe('commit event on an empty store', () => {
@@ -25,11 +30,16 @@ describe('event-store end-to-end', () => {
         getAllEvents: getAllEvents(queryEventsDatabase),
       };
       await ensureEventTableExists(queryEventsDatabase)();
-      deps.commitEvent(event);
+      await pipe(
+        event,
+        deps.commitEvent,
+        TE.mapLeft(left => error(left)),
+        TE.getOrElse(shouldNotBeCalled)
+      )();
     });
 
     it.failing('persists and returns the event', async () => {
-      expect(await deps.getAllEvents()()).toStrictEqual([event]);
+      expect(await deps.getAllEvents()()).toStrictEqual(E.right([event]));
     });
   });
 });
