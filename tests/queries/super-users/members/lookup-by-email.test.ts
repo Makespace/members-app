f03c80a70f258bb4635bb9b4d48ee96d35345972
Daniel Haarhoff <daniel@rknt.de>
2024-05-09 08:59:47 +0100

    Implement empty test case

--- tests/queries/super-users/members/lookup-by-email.test.ts
+++ tests/queries/super-users/members/lookup-by-email.test.ts
@@ -1,10 +1,21 @@
+import * as O from 'fp-ts/Option';
+import {lookupByEmail} from '../../../../src/queries/members/lookup-by-email';
+import {faker} from '@faker-js/faker';
+import {DomainEvent} from '../../../../src/types';
+
 describe('lookupByEmail', () => {
   describe('when no members exist', () => {
-    it.todo('returns none');
+    const events: ReadonlyArray<DomainEvent> = [];
+    const result = lookupByEmail(faker.internet.email())(events);
+    it('returns none', () => {
+      expect(result).toStrictEqual(O.none);
+    });
   });
+
   describe('when a member with the given email exists', () => {
     it.todo('returns their member number');
   });
+
   describe('when no member with the given email exists', () => {
     it.todo('returns none');
   });
