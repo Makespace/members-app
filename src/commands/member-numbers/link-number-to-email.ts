4a39786e659c17f69f2e7bd19ab2cddc708549ec
Daniel Haarhoff <daniel@rknt.de>
2024-05-09 09:51:34 +0100

    Implement link number to email

--- src/commands/member-numbers/link-number-to-email.ts
+++ src/commands/member-numbers/link-number-to-email.ts
@@ -1,9 +1,16 @@
-import {DomainEvent, EmailAddressCodec} from '../../types';
+import * as RA from 'fp-ts/ReadonlyArray';
+import {
+  DomainEvent,
+  EmailAddressCodec,
+  constructEvent,
+  isEventOfType,
+} from '../../types';
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
 import * as O from 'fp-ts/Option';
 import {Command} from '../../types/command';
 import {isAdminOrSuperUser} from '../is-admin-or-super-user';
+import {pipe} from 'fp-ts/lib/function';
 
 const codec = t.strict({
   email: EmailAddressCodec,
@@ -12,11 +19,23 @@ const codec = t.strict({
 
 type LinkNumberToEmail = t.TypeOf<typeof codec>;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-vars
 const process = (input: {
   command: LinkNumberToEmail;
   events: ReadonlyArray<DomainEvent>;
-}): O.Option<DomainEvent> => O.none;
+}): O.Option<DomainEvent> =>
+  pipe(
+    input.events,
+    RA.filter(isEventOfType('MemberNumberLinkedToEmail')),
+    RA.filter(
+      event =>
+        event.email === input.command.email ||
+        event.memberNumber === input.command.memberNumber
+    ),
+    RA.matchW(
+      () => O.some(constructEvent('MemberNumberLinkedToEmail')(input.command)),
+      () => O.none
+    )
+  );
 
 export const linkNumberToEmail: Command<LinkNumberToEmail> = {
   process,
--- tests/commands/memberNumbers/link-number-to-email.test.ts
+++ tests/commands/memberNumbers/link-number-to-email.test.ts
@@ -1,13 +1,20 @@
 import * as O from 'fp-ts/Option';
 import {linkNumberToEmail} from '../../../src/commands/member-numbers/link-number-to-email';
 import {faker} from '@faker-js/faker';
-import {DomainEvent, EmailAddress, constructEvent} from '../../../src/types';
+import {
+  DomainEvent,
+  EmailAddress,
+  constructEvent,
+  isEventOfType,
+} from '../../../src/types';
+import {pipe} from 'fp-ts/lib/function';
+import {shouldNotBeCalled} from '../../should-not-be-called.helper';
 describe('linkNumberToEmail', () => {
-  describe('given a member number that already exists', () => {
-    const command = {
-      email: faker.internet.email() as EmailAddress,
-      memberNumber: faker.number.int(),
-    };
+  const command = {
+    email: faker.internet.email() as EmailAddress,
+    memberNumber: faker.number.int(),
+  };
+  describe('when the member number already exists', () => {
     const events: ReadonlyArray<DomainEvent> = [
       constructEvent('MemberNumberLinkedToEmail')({
         memberNumber: command.memberNumber,
@@ -19,10 +26,23 @@ describe('linkNumberToEmail', () => {
       expect(result).toStrictEqual(O.none);
     });
   });
-  describe('given an email that is already used', () => {
+
+  describe('when the email address is already in use', () => {
     it.todo('returns none');
   });
-  describe('given a new email and member number', () => {
-    it.todo('returns a NumberLinkedToEmail event');
+
+  describe('when both the email and member number are new', () => {
+    const events: ReadonlyArray<DomainEvent> = [];
+    const event = pipe(
+      {command, events},
+      linkNumberToEmail.process,
+      O.filter(isEventOfType('MemberNumberLinkedToEmail')),
+      O.getOrElseW(shouldNotBeCalled)
+    );
+
+    it('raises an event linking the number and email', () => {
+      expect(event.email).toStrictEqual(command.email);
+      expect(event.memberNumber).toStrictEqual(command.memberNumber);
+    });
   });
 });
