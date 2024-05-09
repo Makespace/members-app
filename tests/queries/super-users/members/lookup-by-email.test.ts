fe9154a3a1fcdbf8d28c3d4b793f62172b9db276
Daniel Haarhoff <daniel@rknt.de>
2024-05-09 09:59:21 +0100

    Prepare to use real commands when testing query

--- tests/queries/super-users/members/lookup-by-email.test.ts
+++ tests/queries/super-users/members/lookup-by-email.test.ts
@@ -1,12 +1,32 @@
+import * as TE from 'fp-ts/TaskEither';
 import * as O from 'fp-ts/Option';
 import {lookupByEmail} from '../../../../src/queries/members/lookup-by-email';
 import {faker} from '@faker-js/faker';
+import {getAllEvents} from '../../../../src/init-dependencies/event-store/get-all-events';
+import {initQueryEventsDatabase} from '../../../../src/init-dependencies/event-store/init-events-database';
+import {QueryEventsDatabase} from '../../../../src/init-dependencies/event-store/query-events-database';
+import {ensureEventTableExists} from '../../../../src/init-dependencies/event-store/ensure-event-table-exists';
 import {DomainEvent} from '../../../../src/types';
+import {shouldNotBeCalled} from '../../../should-not-be-called.helper';
+import {pipe} from 'fp-ts/lib/function';
 
 describe('lookupByEmail', () => {
+  let queryEventsDatabase: QueryEventsDatabase;
+  let events: ReadonlyArray<DomainEvent>;
+  beforeEach(async () => {
+    queryEventsDatabase = initQueryEventsDatabase();
+    await ensureEventTableExists(queryEventsDatabase)();
+  });
+
   describe('when no members exist', () => {
-    const events: ReadonlyArray<DomainEvent> = [];
+    beforeEach(async () => {
+      events = await pipe(
+        getAllEvents(queryEventsDatabase)(),
+        TE.getOrElse(shouldNotBeCalled)
+      )();
+    });
     const result = lookupByEmail(faker.internet.email())(events);
+
     it('returns none', () => {
       expect(result).toStrictEqual(O.none);
     });
