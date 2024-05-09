4847dab00239bb62955e69d8974e2181ca4fc367
Daniel Haarhoff <daniel@rknt.de>
2024-05-09 11:40:25 +0100

    Extract test framework to its own file

---
+++ tests/queries/members/lookup-by-email.test.ts
@@ -0,0 +1,56 @@
+import * as O from 'fp-ts/Option';
+import {lookupByEmail} from '../../../src/queries/members/lookup-by-email';
+import {faker} from '@faker-js/faker';
+import {DomainEvent, EmailAddress} from '../../../src/types';
+import {TestFramework, initTestFramework} from '../test-framework';
+
+describe('lookupByEmail', () => {
+  let events: ReadonlyArray<DomainEvent>;
+  let framework: TestFramework;
+  beforeEach(async () => {
+    framework = await initTestFramework();
+  });
+
+  describe('when no members exist', () => {
+    beforeEach(async () => {
+      events = await framework.getAllEvents();
+    });
+
+    it('returns none', () => {
+      const result = lookupByEmail(faker.internet.email())(events);
+      expect(result).toStrictEqual(O.none);
+    });
+  });
+
+  describe('when a member with the given email exists', () => {
+    const command = {
+      memberNumber: faker.number.int(),
+      email: faker.internet.email() as EmailAddress,
+    };
+    beforeEach(async () => {
+      await framework.commands.memberNumbers.linkNumberToEmail(command);
+      events = await framework.getAllEvents();
+    });
+
+    it('returns their member number', () => {
+      const result = lookupByEmail(command.email)(events);
+      expect(result).toStrictEqual(O.some(command.memberNumber));
+    });
+  });
+
+  describe('when no member with the given email exists', () => {
+    const command = {
+      memberNumber: faker.number.int(),
+      email: faker.internet.email() as EmailAddress,
+    };
+    beforeEach(async () => {
+      await framework.commands.memberNumbers.linkNumberToEmail(command);
+      events = await framework.getAllEvents();
+    });
+
+    it('returns none', () => {
+      const result = lookupByEmail(faker.internet.email())(events);
+      expect(result).toStrictEqual(O.none);
+    });
+  });
+});
