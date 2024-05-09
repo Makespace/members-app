038680688e748c7d5df75a7e9751e65cd5259b3a
Daniel Haarhoff <daniel@rknt.de>
2024-05-09 10:10:34 +0100

    Start building a test framework to make it easier to use commands when writing tests for queries

--- tests/queries/super-users/members/lookup-by-email.test.ts
+++ tests/queries/super-users/members/lookup-by-email.test.ts
@@ -4,26 +4,38 @@ import {lookupByEmail} from '../../../../src/queries/members/lookup-by-email';
 import {faker} from '@faker-js/faker';
 import {getAllEvents} from '../../../../src/init-dependencies/event-store/get-all-events';
 import {initQueryEventsDatabase} from '../../../../src/init-dependencies/event-store/init-events-database';
-import {QueryEventsDatabase} from '../../../../src/init-dependencies/event-store/query-events-database';
 import {ensureEventTableExists} from '../../../../src/init-dependencies/event-store/ensure-event-table-exists';
 import {DomainEvent} from '../../../../src/types';
 import {shouldNotBeCalled} from '../../../should-not-be-called.helper';
 import {pipe} from 'fp-ts/lib/function';
 
+type TestFramework = {
+  getAllEvents: () => Promise<ReadonlyArray<DomainEvent>>;
+};
+
+const initTestFramework = async (): Promise<TestFramework> => {
+  const queryEventsDatabase = initQueryEventsDatabase();
+  await ensureEventTableExists(queryEventsDatabase)();
+
+  return {
+    getAllEvents: () =>
+      pipe(
+        getAllEvents(queryEventsDatabase)(),
+        TE.getOrElse(shouldNotBeCalled)
+      )(),
+  };
+};
+
 describe('lookupByEmail', () => {
-  let queryEventsDatabase: QueryEventsDatabase;
   let events: ReadonlyArray<DomainEvent>;
+  let framework: TestFramework;
   beforeEach(async () => {
-    queryEventsDatabase = initQueryEventsDatabase();
-    await ensureEventTableExists(queryEventsDatabase)();
+    framework = await initTestFramework();
   });
 
   describe('when no members exist', () => {
     beforeEach(async () => {
-      events = await pipe(
-        getAllEvents(queryEventsDatabase)(),
-        TE.getOrElse(shouldNotBeCalled)
-      )();
+      events = await framework.getAllEvents();
     });
     const result = lookupByEmail(faker.internet.email())(events);
 
