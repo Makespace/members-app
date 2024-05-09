e932c5aefabe0eb4a41c5e904ce43877a4266d70
Daniel Haarhoff <daniel@rknt.de>
2024-05-09 08:56:15 +0100

    Write a test list for the query

---
+++ tests/queries/super-users/members/lookup-by-email.test.ts
@@ -0,0 +1,11 @@
+describe('lookupByEmail', () => {
+  describe('when no members exist', () => {
+    it.todo('returns none');
+  });
+  describe('when a member with the given email exists', () => {
+    it.todo('returns their member number');
+  });
+  describe('when no member with the given email exists', () => {
+    it.todo('returns none');
+  });
+});
