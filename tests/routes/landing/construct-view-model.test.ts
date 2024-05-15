147b58f33cb803cd57cc0e5587fd965b5fea550a
Daniel Haarhoff <daniel@rknt.de>
2024-05-15 08:22:04 +0100

    Align a folder name between src and test

--- tests/routes/landing/construct-view-model.test.ts
+++ tests/routes/landing/construct-view-model.test.ts
@@ -3,7 +3,7 @@ import * as TE from 'fp-ts/TaskEither';
 import {faker} from '@faker-js/faker';
 import {Dependencies} from '../../../src/dependencies';
 import {constructEvent} from '../../../src/types';
-import {happyPathAdapters} from '../../dependencies/happy-path-adapters.helper';
+import {happyPathAdapters} from '../../init-dependencies/happy-path-adapters.helper';
 import {pipe} from 'fp-ts/lib/function';
 import {arbitraryUser} from '../../types/user.helper';
 import {shouldNotBeCalled} from '../../should-not-be-called.helper';
