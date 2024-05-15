ce94de15a633b7307e292903aa3ffeaa6f190f6d
Daniel Haarhoff <daniel@rknt.de>
2024-05-15 08:25:26 +0100

    write a test list for OCC commit event

---
+++ tests/init-dependencies/commit-event.test.ts
@@ -0,0 +1,9 @@
+describe('commit-event', () => {
+  describe('when the last known version is the latest persisted version', () => {
+    it.todo('persists the event');
+  });
+
+  describe('when the last known version is out of date', () => {
+    it.todo('persists the event');
+  });
+});
