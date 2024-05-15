0bca3c5166e9300b864bec73200173532fc9138c
Daniel Haarhoff <daniel@rknt.de>
2024-05-15 08:35:54 +0100

    Write test that is passing due to generoush behaviour of existing implementation

--- tests/init-dependencies/commit-event.test.ts
+++ tests/init-dependencies/commit-event.test.ts
@@ -1,6 +1,38 @@
+import {faker} from '@faker-js/faker';
+import * as TE from 'fp-ts/TaskEither';
+import {DomainEvent, EmailAddress, constructEvent} from '../../src/types';
+import {getAllEvents} from '../../src/init-dependencies/event-store/get-all-events';
+import {initQueryEventsDatabase} from '../../src/init-dependencies/event-store/init-events-database';
+import {shouldNotBeCalled} from '../should-not-be-called.helper';
+import {pipe} from 'fp-ts/lib/function';
+import {commitEvent} from '../../src/init-dependencies/event-store/commit-event';
+import {error} from 'console';
+import {ensureEventTableExists} from '../../src/init-dependencies/event-store/ensure-event-table-exists';
+
 describe('commit-event', () => {
   describe('when the last known version is the latest persisted version', () => {
-    it.todo('persists the event');
+    const event = constructEvent('MemberNumberLinkedToEmail')({
+      memberNumber: faker.number.int(),
+      email: faker.internet.email() as EmailAddress,
+    });
+
+    let events: ReadonlyArray<DomainEvent>;
+    beforeEach(async () => {
+      const queryDatabase = initQueryEventsDatabase();
+      await ensureEventTableExists(queryDatabase)();
+      await commitEvent(queryDatabase)('MemberNumberEmailPairings', 1)(event)();
+      events = await pipe(
+        getAllEvents(queryDatabase)(),
+        TE.getOrElse(e => {
+          error(e);
+          return shouldNotBeCalled();
+        })
+      )();
+    });
+
+    it('persists the event', () => {
+      expect(events).toStrictEqual([event]);
+    });
   });
 
   describe('when the last known version is out of date', () => {
