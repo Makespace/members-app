edc5c734930e8fcacebff3a65caf1fe6f04f9374
Daniel Haarhoff <daniel@rknt.de>
2024-05-15 08:50:56 +0100

    Write failing tests for OCC

--- tests/init-dependencies/commit-event.test.ts
+++ tests/init-dependencies/commit-event.test.ts
@@ -1,4 +1,5 @@
 import {faker} from '@faker-js/faker';
+import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
 import {DomainEvent, EmailAddress, constructEvent} from '../../src/types';
 import {getAllEvents} from '../../src/init-dependencies/event-store/get-all-events';
@@ -32,6 +33,37 @@ describe('commit-event', () => {
   });
 
   describe('when the last known version is out of date', () => {
-    it.todo('persists the event');
+    const event = constructEvent('MemberNumberLinkedToEmail')({
+      memberNumber: faker.number.int(),
+      email: faker.internet.email() as EmailAddress,
+    });
+    const competingEvent = constructEvent('MemberNumberLinkedToEmail')({
+      memberNumber: faker.number.int(),
+      email: faker.internet.email() as EmailAddress,
+    });
+    let events: ReadonlyArray<DomainEvent>;
+    let result: E.Either<unknown, unknown>;
+    beforeEach(async () => {
+      const queryDatabase = initQueryEventsDatabase();
+      await ensureEventTableExists(queryDatabase)();
+      await commitEvent(queryDatabase)('MemberNumberEmailPairings', 1)(
+        competingEvent
+      )();
+      result = await commitEvent(queryDatabase)('MemberNumberEmailPairings', 1)(
+        event
+      )();
+      events = await pipe(
+        getAllEvents(queryDatabase)(),
+        T.map(getRightOrFail)
+      )();
+    });
+
+    it.failing('does not persist the event', () => {
+      expect(events).toStrictEqual([competingEvent]);
+    });
+
+    it.failing('returns on left', () => {
+      expect(result).toStrictEqual(E.left(expect.anything()));
+    });
   });
 });
