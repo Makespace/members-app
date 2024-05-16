d938aef64e3074d48f0ddfe7a1710c79765ff706
Daniel Haarhoff <daniel@rknt.de>
2024-05-16 08:53:51 +0100

    Consolidate event store tests

--- tests/init-dependencies/event-store/end-to-end.test.ts
+++ tests/init-dependencies/event-store/end-to-end.test.ts
@@ -1,39 +1,86 @@
+import {faker} from '@faker-js/faker';
 import * as E from 'fp-ts/Either';
 import * as T from 'fp-ts/Task';
-import {faker} from '@faker-js/faker';
-import {Dependencies} from '../../../src/dependencies';
-import {ensureEventTableExists} from '../../../src/init-dependencies/event-store/ensure-event-table-exists';
-import {initQueryEventsDatabase} from '../../../src/init-dependencies/event-store/init-events-database';
-import {constructEvent} from '../../../src/types';
-import {UUID} from 'io-ts-types';
-import {commitEvent} from '../../../src/init-dependencies/event-store/commit-event';
+import {DomainEvent, EmailAddress, constructEvent} from '../../../src/types';
 import {getAllEvents} from '../../../src/init-dependencies/event-store/get-all-events';
+import {initQueryEventsDatabase} from '../../../src/init-dependencies/event-store/init-events-database';
 import {pipe} from 'fp-ts/lib/function';
+import {commitEvent} from '../../../src/init-dependencies/event-store/commit-event';
+import {ensureEventTableExists} from '../../../src/init-dependencies/event-store/ensure-event-table-exists';
 import {getRightOrFail} from '../../helpers';
+import {QueryEventsDatabase} from '../../../src/init-dependencies/event-store/query-events-database';
+import {Dependencies} from '../../../src/dependencies';
+import {getResourceEvents} from '../../../src/init-dependencies/event-store/get-resource-events';
+
+const arbitraryMemberNumberLinkedToEmaiEvent = () =>
+  constructEvent('MemberNumberLinkedToEmail')({
+    memberNumber: faker.number.int(),
+    email: faker.internet.email() as EmailAddress,
+  });
 
 describe('event-store end-to-end', () => {
-  describe('commit event on an empty store', () => {
-    const queryEventsDatabase = initQueryEventsDatabase();
-    let deps: Pick<Dependencies, 'commitEvent'> &
-      Pick<Dependencies, 'getAllEvents'>;
-
-    const event = constructEvent('AreaCreated')({
-      id: faker.string.uuid() as UUID,
-      name: faker.company.buzzNoun(),
-      description: faker.string.alphanumeric(),
-    });
+  describe('setup event store', () => {
+    const event = arbitraryMemberNumberLinkedToEmaiEvent();
+    const initialVersion = faker.number.int();
+    let queryDatabase: QueryEventsDatabase;
+    let getTestEvents: () => Promise<ReadonlyArray<DomainEvent>>;
 
     beforeEach(async () => {
-      deps = {
-        commitEvent: commitEvent(queryEventsDatabase),
-        getAllEvents: getAllEvents(queryEventsDatabase),
-      };
-      await ensureEventTableExists(queryEventsDatabase)();
-      await pipe(event, deps.commitEvent('', 0), T.map(getRightOrFail))();
+      queryDatabase = initQueryEventsDatabase();
+      await ensureEventTableExists(queryDatabase)();
+      getTestEvents = () =>
+        pipe(getAllEvents(queryDatabase)(), T.map(getRightOrFail))();
     });
 
-    it('persists and returns the event', async () => {
-      expect(await deps.getAllEvents()()).toStrictEqual(E.right([event]));
+    it('is empty', async () => {
+      expect(await getTestEvents()).toStrictEqual([]);
+    });
+
+    describe.skip('committing when the last known version is the latest persisted version', () => {
+      let resourceEvents: (Awaited<
+        ReturnType<ReturnType<Dependencies['getResourceEvents']>>
+      > & {_tag: 'Right'})['right'];
+      beforeEach(async () => {
+        await commitEvent(queryDatabase)(
+          'MemberNumberEmailPairings',
+          initialVersion
+        )(event)();
+        resourceEvents = await pipe(
+          'MemberNumberEmailPairings',
+          getResourceEvents(queryDatabase),
+          T.map(getRightOrFail)
+        )();
+      });
+
+      it('persists the event', async () => {
+        expect(await getTestEvents()).toStrictEqual([event]);
+      });
+
+      it('increments the version', () => {
+        expect(resourceEvents.version).toBeGreaterThan(initialVersion);
+      });
+    });
+
+    describe('committing when the last known version is out of date', () => {
+      const competingEvent = arbitraryMemberNumberLinkedToEmaiEvent();
+      let result: E.Either<unknown, unknown>;
+      beforeEach(async () => {
+        await commitEvent(queryDatabase)('MemberNumberEmailPairings', 1)(
+          competingEvent
+        )();
+        result = await commitEvent(queryDatabase)(
+          'MemberNumberEmailPairings',
+          1
+        )(event)();
+      });
+
+      it.failing('does not persist the event', async () => {
+        expect(await getTestEvents()).toStrictEqual([competingEvent]);
+      });
+
+      it.failing('returns on left', () => {
+        expect(result).toStrictEqual(E.left(expect.anything()));
+      });
     });
   });
 });
