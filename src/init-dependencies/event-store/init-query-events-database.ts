c4fb67073d412a3f8c66f25dae805498b10bcd93
Daniel Haarhoff <daniel@rknt.de>
2024-05-21 08:51:48 +0100

    Start introducing batch queries for commit events to perform OCC check

--- src/init-dependencies/event-store/init-query-events-database.ts
+++ src/init-dependencies/event-store/init-query-events-database.ts
@@ -3,17 +3,22 @@ import {failureWithStatus} from '../../types/failureWithStatus';
 import {StatusCodes} from 'http-status-codes';
 import * as libsqlClient from '@libsql/client';
 import {QueryEventsDatabase} from './query-events-database';
+import {pipe} from 'fp-ts/lib/function';
 
 export const initQueryEventsDatabase = (): QueryEventsDatabase => {
   const client = libsqlClient.createClient({url: ':memory:'});
   return (query: string, args: libsqlClient.InArgs) =>
-    TE.tryCatch(
-      () =>
-        client.execute({
-          sql: query,
-          args: args,
-        }),
-
-      failureWithStatus('DB query failed', StatusCodes.INTERNAL_SERVER_ERROR)
+    pipe(
+      TE.tryCatch(
+        () =>
+          client.batch([
+            {
+              sql: query,
+              args: args,
+            },
+          ]),
+        failureWithStatus('DB query failed', StatusCodes.INTERNAL_SERVER_ERROR)
+      ),
+      TE.map(results => results[0])
     );
 };
