fc2ae6ccb4d029602371186b1241c63dcd5ac1a6
Daniel Haarhoff <daniel@rknt.de>
2024-05-22 10:25:59 +0100

    Reduce duplication related to asynchandler

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,9 +1,11 @@
+import {flow} from 'fp-ts/lib/function';
 import {apiPost} from './api-post';
 import {formGet} from './form-get';
 import {formPost} from './form-post';
+import asyncHandler from 'express-async-handler';
 
 export const http = {
-  apiPost,
-  formGet,
-  formPost,
+  apiPost: flow(apiPost, asyncHandler),
+  formGet: flow(formGet, asyncHandler),
+  formPost: flow(formPost, asyncHandler),
 };
--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,9 +1,11 @@
+import {flow} from 'fp-ts/lib/function';
 import {areas} from './areas';
 import {landing} from './landing';
 import {superUsers} from './super-users';
+import asyncHandler from 'express-async-handler';
 
 export const queries = {
-  areas,
-  superUsers,
-  landing,
+  areas: flow(areas, asyncHandler),
+  superUsers: flow(superUsers, asyncHandler),
+  landing: flow(landing, asyncHandler),
 };
