6ee05dfac5801cee9bcb4f0f02ed1ea73b434bd1
Daniel Haarhoff <daniel@rknt.de>
2024-05-22 10:25:59 +0100

    Reduce duplication related to asynchandler

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,6 @@ import express, {Router} from 'express';
 import path from 'path';
 import {oopsPage} from './templates';
 import {Dependencies} from './dependencies';
-import asyncHandler from 'express-async-handler';
 import {configureAuthRoutes} from './authentication';
 import {Config} from './configuration';
 import {StatusCodes} from 'http-status-codes';
@@ -13,55 +12,48 @@ import {http} from './http';
 export const createRouter = (deps: Dependencies, conf: Config): Router => {
   const router = Router();
 
-  router.get('/', asyncHandler(queries.landing(deps)));
+  router.get('/', queries.landing(deps));
 
-  router.get('/areas', asyncHandler(queries.areas(deps)));
-  router.get(
-    '/areas/create',
-    asyncHandler(http.formGet(deps, commands.area.create))
-  );
+  router.get('/areas', queries.areas(deps));
+  router.get('/areas/create', http.formGet(deps, commands.area.create));
   router.post(
     '/areas/create',
-    asyncHandler(http.formPost(deps, commands.area.create, '/areas'))
+    http.formPost(deps, commands.area.create, '/areas')
   );
   router.post(
     '/api/create-area',
-    asyncHandler(http.apiPost(deps, conf, commands.area.create))
+    http.apiPost(deps, conf, commands.area.create)
   );
 
-  router.get('/super-users', asyncHandler(queries.superUsers(deps)));
+  router.get('/super-users', queries.superUsers(deps));
   router.get(
     '/super-users/declare',
-    asyncHandler(http.formGet(deps, commands.superUser.declare))
+    http.formGet(deps, commands.superUser.declare)
   );
   router.post(
     '/super-users/declare',
-    asyncHandler(
-      http.formPost(deps, commands.superUser.declare, '/super-users')
-    )
+    http.formPost(deps, commands.superUser.declare, '/super-users')
   );
   router.post(
     '/api/declare-super-user',
-    asyncHandler(http.apiPost(deps, conf, commands.superUser.declare))
+    http.apiPost(deps, conf, commands.superUser.declare)
   );
   router.get(
     '/super-users/revoke',
-    asyncHandler(http.formGet(deps, commands.superUser.revoke))
+    http.formGet(deps, commands.superUser.revoke)
   );
   router.post(
     '/super-users/revoke',
-    asyncHandler(http.formPost(deps, commands.superUser.revoke, '/super-users'))
+    http.formPost(deps, commands.superUser.revoke, '/super-users')
   );
   router.post(
     '/api/revoke-super-user',
-    asyncHandler(http.apiPost(deps, conf, commands.superUser.revoke))
+    http.apiPost(deps, conf, commands.superUser.revoke)
   );
 
   router.post(
     '/api/link-number-to-email',
-    asyncHandler(
-      http.apiPost(deps, conf, commands.memberNumbers.linkNumberToEmail)
-    )
+    http.apiPost(deps, conf, commands.memberNumbers.linkNumberToEmail)
   );
 
   configureAuthRoutes(router);
