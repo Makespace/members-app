efeb9dd927617495da221bda560c52bf192be956
Daniel Haarhoff <daniel@rknt.de>
2024-05-23 10:03:40 +0100

    start reading resource version to create a pull to populate it

--- src/init-dependencies/event-store/get-resource-events.ts
+++ src/init-dependencies/event-store/get-resource-events.ts
@@ -1,15 +1,89 @@
+import {flow, pipe} from 'fp-ts/lib/function';
 import {Dependencies} from '../../dependencies';
-import * as TE from 'fp-ts/TaskEither';
-import {failureWithStatus} from '../../types/failureWithStatus';
-import {StatusCodes} from 'http-status-codes';
 import {QueryEventsDatabase} from './query-events-database';
+import * as TE from 'fp-ts/TaskEither';
+import * as E from 'fp-ts/Either';
+import * as tt from 'io-ts-types';
+import * as t from 'io-ts';
+import {StatusCodes} from 'http-status-codes';
+import {formatValidationErrors} from 'io-ts-reporters';
+import {DomainEvent} from '../../types';
+import {failureWithStatus} from '../../types/failureWithStatus';
+import {sequenceS} from 'fp-ts/lib/Apply';
+
+const EventsFromDb = t.strict({
+  rows: t.readonlyArray(
+    t.strict({
+      id: t.string,
+      resource_id: t.string,
+      resource_type: t.string,
+      resource_version: t.number,
+      event_type: t.string,
+      payload: t.string,
+    })
+  ),
+});
+
+type EventsFromDb = t.TypeOf<typeof EventsFromDb>;
+
+const reshapeRowToEvent = (row: EventsFromDb['rows'][number]) =>
+  pipe(
+    row.payload,
+    tt.JsonFromString.decode,
+    E.chain(tt.JsonRecord.decode),
+    E.map(payload => ({
+      type: row.event_type,
+      ...payload,
+    }))
+  );
 
 export const getResourceEvents =
   (
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-vars
     queryEventsDatabase: QueryEventsDatabase
   ): Dependencies['getResourceEvents'] =>
-  () =>
-    TE.left(
-      failureWithStatus('not implemented', StatusCodes.INTERNAL_SERVER_ERROR)()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, unused-imports/no-unused-vars
+  resource =>
+    pipe(
+      queryEventsDatabase([{sql: 'SELECT * FROM events;', args: {}}]),
+      TE.chainEitherK(
+        flow(
+          EventsFromDb.decode,
+          E.mapLeft(formatValidationErrors),
+          E.mapLeft(
+            failureWithStatus(
+              'failed to decode db response',
+              StatusCodes.INTERNAL_SERVER_ERROR
+            )
+          )
+        )
+      ),
+      TE.map(response => response.rows),
+      TE.chainEitherK(rows =>
+        pipe(
+          {
+            version: E.right(0),
+            events: pipe(
+              rows,
+              E.traverseArray(reshapeRowToEvent),
+              foo => foo,
+              E.chain(t.readonlyArray(DomainEvent).decode),
+              E.mapLeft(formatValidationErrors),
+              E.mapLeft(
+                failureWithStatus(
+                  'failed to decode events',
+                  StatusCodes.INTERNAL_SERVER_ERROR
+                )
+              )
+            ),
+          },
+          sequenceS(E.Apply)
+        )
+      ),
+      TE.mapLeft(
+        failureWithStatus(
+          'Failed to get events from DB',
+          StatusCodes.INTERNAL_SERVER_ERROR
+        )
+      ),
+      foo => foo
     );
