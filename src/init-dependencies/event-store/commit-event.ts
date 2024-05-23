a8ecc61bc3087eb30740be8cca428ba772afa00a
Daniel Haarhoff <daniel@rknt.de>
2024-05-23 15:01:20 +0100

    Prepare to have more behaviour in imperative section

--- src/init-dependencies/event-store/commit-event.ts
+++ src/init-dependencies/event-store/commit-event.ts
@@ -9,9 +9,24 @@ import * as TE from 'fp-ts/TaskEither';
 import {Dependencies} from '../../dependencies';
 import {pipe} from 'fp-ts/lib/function';
 import {v4 as uuidv4} from 'uuid';
-import {Client, InArgs} from '@libsql/client/.';
+import {Client} from '@libsql/client/.';
+import {DomainEvent} from '../../types';
+import {Resource} from '../../types/resource';
 
-const performTransaction = (dbClient: Client, args: InArgs) => {
+const performTransaction = (
+  event: DomainEvent,
+  resource: Resource,
+  lastKnownVersion: number,
+  dbClient: Client
+) => {
+  const args = pipe(event, ({type, ...payload}) => ({
+    id: uuidv4(),
+    resource_id: resource.id,
+    resource_type: resource.type,
+    resource_version: lastKnownVersion + 1,
+    event_type: type,
+    payload: JSON.stringify(payload),
+  }));
   return dbClient.execute({
     sql: 'INSERT INTO events (id, resource_id, resource_type, resource_version, event_type, payload) VALUES ($id, $resource_id, $resource_type, $resource_version, $event_type, $payload); ',
     args,
@@ -27,17 +42,9 @@ export const commitEvent =
     FailureWithStatus,
     {status: StatusCodes.CREATED; message: string}
   > => {
-    const args = pipe(event, ({type, ...payload}) => ({
-      id: uuidv4(),
-      resource_id: resource.id,
-      resource_type: resource.type,
-      resource_version: lastKnownVersion + 1,
-      event_type: type,
-      payload: JSON.stringify(payload),
-    }));
     return pipe(
       TE.tryCatch(
-        () => performTransaction(dbClient, args),
+        () => performTransaction(event, resource, lastKnownVersion, dbClient),
         failureWithStatus('DB query failed', StatusCodes.INTERNAL_SERVER_ERROR)
       ),
       TE.map(() => ({
