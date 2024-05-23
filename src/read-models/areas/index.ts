047c4762df8f9fb2d23777a9771200a5c10947ff
Daniel Haarhoff <daniel@rknt.de>
2024-05-23 21:16:30 +0100

    Extract a query to its own file

--- src/read-models/areas/index.ts
+++ src/read-models/areas/index.ts
@@ -1,12 +1,5 @@
-import {pipe} from 'fp-ts/lib/function';
-import {DomainEvent, isEventOfType} from '../../types';
-import * as RA from 'fp-ts/ReadonlyArray';
+import {getAll} from './get-all';
 
 export const areas = {
-  getAll: (events: ReadonlyArray<DomainEvent>) =>
-    pipe(
-      events,
-      RA.filter(isEventOfType('AreaCreated')),
-      RA.map(areaCreated => ({...areaCreated, owners: [1234, 2333]}))
-    ),
+  getAll,
 };
