58b8eab7a1712518f6e9d0631ad858ff4c3b6da7
Daniel Haarhoff <daniel@rknt.de>
2024-05-23 21:14:31 +0100

    Delegate some behaviour to a read model

--- src/queries/areas/construct-view-model.ts
+++ src/queries/areas/construct-view-model.ts
@@ -1,8 +1,7 @@
 import {pipe} from 'fp-ts/lib/function';
-import {User, isEventOfType} from '../../types';
+import {User} from '../../types';
 import {Dependencies} from '../../dependencies';
 import * as TE from 'fp-ts/TaskEither';
-import * as RA from 'fp-ts/ReadonlyArray';
 import {readModels} from '../../read-models';
 import {FailureWithStatus} from '../../types/failureWithStatus';
 import {ViewModel} from './view-model';
@@ -15,10 +14,6 @@ export const constructViewModel =
       TE.map(events => ({
         user: user,
         isSuperUser: readModels.superUsers.is(user.memberNumber)(events),
-        areas: pipe(
-          events,
-          RA.filter(isEventOfType('AreaCreated')),
-          RA.map(areaCreated => ({...areaCreated, owners: [1234, 2333]}))
-        ),
+        areas: readModels.areas.getAll(events),
       }))
     );
---
+++ src/read-models/areas/index.ts
@@ -0,0 +1,12 @@
+import {pipe} from 'fp-ts/lib/function';
+import {DomainEvent, isEventOfType} from '../../types';
+import * as RA from 'fp-ts/ReadonlyArray';
+
+export const areas = {
+  getAll: (events: ReadonlyArray<DomainEvent>) =>
+    pipe(
+      events,
+      RA.filter(isEventOfType('AreaCreated')),
+      RA.map(areaCreated => ({...areaCreated, owners: [1234, 2333]}))
+    ),
+};
