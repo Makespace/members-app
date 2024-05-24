ec29d849208373cd3a3bd2666546a0d83564f4df
Daniel Haarhoff <daniel@rknt.de>
2024-05-24 10:15:44 +0100

    Prepare to pass events to form constructions

--- src/http/form-get.ts
+++ src/http/form-get.ts
@@ -8,6 +8,16 @@ import {User} from '../types';
 import {FailureWithStatus, failureWithStatus} from '../types/failureWithStatus';
 import * as E from 'fp-ts/Either';
 import {oopsPage} from '../templates';
+import {sequenceS} from 'fp-ts/lib/Apply';
+
+const getUser = (req: Request, deps: Dependencies) =>
+  pipe(
+    req.session,
+    getUserFromSession(deps),
+    TE.fromOption(() =>
+      failureWithStatus('You are not logged in.', StatusCodes.UNAUTHORIZED)()
+    )
+  );
 
 type Form<T> = {
   renderForm: (viewModel: T) => string;
@@ -20,12 +30,11 @@ export const formGet =
   <T>(deps: Dependencies, form: Form<T>) =>
   async (req: Request, res: Response) => {
     await pipe(
-      req.session,
-      getUserFromSession(deps),
-      TE.fromOption(() =>
-        failureWithStatus('You are not logged in.', StatusCodes.UNAUTHORIZED)()
-      ),
-      TE.chainEitherK(form.constructForm(req.query)),
+      {
+        user: getUser(req, deps),
+      },
+      sequenceS(TE.ApplyPar),
+      TE.chainEitherK(({user}) => form.constructForm(req.query)(user)),
       TE.map(form.renderForm),
       TE.mapLeft(failure => {
         deps.logger.warn(
