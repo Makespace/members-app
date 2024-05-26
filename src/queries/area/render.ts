25c10c5b303a7ab6fb22dc7b3135461e516d5c5e
Daniel Haarhoff <daniel@rknt.de>
2024-05-26 11:36:40 +0100

    Render remaining details of an area on its page

--- src/queries/area/render.ts
+++ src/queries/area/render.ts
@@ -3,9 +3,23 @@ import {pageTemplate} from '../../templates';
 import {html} from '../../types/html';
 import * as O from 'fp-ts/Option';
 import {ViewModel} from './view-model';
+import * as RA from 'fp-ts/ReadonlyArray';
+
+const renderOwners = (owners: ViewModel['area']['owners']) =>
+  pipe(
+    owners,
+    RA.map(owner => html`<li>${owner}</li>`),
+    items =>
+      html`<ul>
+        ${items.join('\n')}
+      </ul>`
+  );
 
 export const render = (viewModel: ViewModel) =>
   pipe(
-    html` <h1>${viewModel.area.name}</h1> `,
+    html`<h1>${viewModel.area.name}</h1>
+      <p>${viewModel.area.description}</p>
+      <p>Owners</p>
+      ${renderOwners(viewModel.area.owners)} `,
     pageTemplate(viewModel.area.name, O.some(viewModel.user))
   );
