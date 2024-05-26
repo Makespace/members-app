68d931100deedfd499f25e92d8f796924fb08e7c
Daniel Haarhoff <daniel@rknt.de>
2024-05-26 12:10:18 +0100

    Show user which area they are adding equipment to

--- src/commands/equipment/add-form.ts
+++ src/commands/equipment/add-form.ts
@@ -4,22 +4,24 @@ import * as E from 'fp-ts/Either';
 import {pageTemplate} from '../../templates';
 import {html} from '../../types/html';
 import * as O from 'fp-ts/Option';
-import {User} from '../../types';
+import {DomainEvent, User} from '../../types';
 import {v4} from 'uuid';
 import {Form} from '../../types/form';
 import {formatValidationErrors} from 'io-ts-reporters';
 import {failureWithStatus} from '../../types/failureWithStatus';
 import {StatusCodes} from 'http-status-codes';
+import {readModels} from '../../read-models';
 
 type ViewModel = {
   user: User;
   areaId: string;
+  areaName: string;
 };
 
 const renderForm = (viewModel: ViewModel) =>
   pipe(
     html`
-      <h1>Add equipment</h1>
+      <h1>Add equipment to ${viewModel.areaName}</h1>
       <form action="#" method="post">
         <label for="name">What is this Equipment called</label>
         <input type="text" name="name" id="name" />
@@ -31,20 +33,33 @@ const renderForm = (viewModel: ViewModel) =>
     pageTemplate('Create Equipment', O.some(viewModel.user))
   );
 
+const getAreaId = (input: unknown) =>
+  pipe(
+    input,
+    t.strict({area: t.string}).decode,
+    E.mapLeft(formatValidationErrors),
+    E.mapLeft(failureWithStatus('Invalid parameters', StatusCodes.BAD_REQUEST)),
+    E.map(({area}) => area)
+  );
+
+const getAreaName = (events: ReadonlyArray<DomainEvent>, areaId: string) =>
+  pipe(
+    areaId,
+    readModels.areas.getArea(events),
+    E.fromOption(() =>
+      failureWithStatus('No such area', StatusCodes.NOT_FOUND)()
+    ),
+    E.map(area => area.name)
+  );
+
 const constructForm: Form<ViewModel>['constructForm'] =
   input =>
-  ({user}) =>
+  ({user, events}) =>
     pipe(
-      input,
-      t.strict({area: t.string}).decode,
-      E.mapLeft(formatValidationErrors),
-      E.mapLeft(
-        failureWithStatus('Invalid parameters', StatusCodes.BAD_REQUEST)
-      ),
-      E.map(({area}) => ({
-        user,
-        areaId: area,
-      }))
+      E.Do,
+      E.bind('areaId', () => getAreaId(input)),
+      E.bind('areaName', ({areaId}) => getAreaName(events, areaId)),
+      E.bind('user', () => E.right(user))
     );
 
 export const addForm: Form<ViewModel> = {
