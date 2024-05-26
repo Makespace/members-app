eb0ae31091d06e61c3daf743cfb9ebb83a14c5a6
Daniel Haarhoff <daniel@rknt.de>
2024-05-26 13:19:20 +0100

    List all equipment with associated areas on equipment page

---
+++ src/read-models/equipment/get-all.ts
@@ -0,0 +1,30 @@
+import {pipe} from 'fp-ts/lib/function';
+import * as O from 'fp-ts/Option';
+import {DomainEvent, isEventOfType} from '../../types';
+import * as RA from 'fp-ts/ReadonlyArray';
+import {readModels} from '..';
+
+type Equipment = {
+  name: string;
+  id: string;
+  areaId: string;
+  areaName: string;
+};
+
+export const getAll = (
+  events: ReadonlyArray<DomainEvent>
+): ReadonlyArray<Equipment> =>
+  pipe(
+    events,
+    RA.filter(isEventOfType('EquipmentAdded')),
+    RA.map(equipment =>
+      pipe(
+        readModels.areas.getArea(events)(equipment.areaId),
+        O.map(area => ({
+          ...equipment,
+          areaName: area.name,
+        }))
+      )
+    ),
+    RA.compact
+  );
