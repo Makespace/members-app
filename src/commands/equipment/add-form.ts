2970b25929643b4dd1abece2556538bd11709004
Daniel Haarhoff <daniel@rknt.de>
2024-05-26 11:57:01 +0100

    Make area id available to add equipment form

--- src/commands/equipment/add-form.ts
+++ src/commands/equipment/add-form.ts
@@ -1,4 +1,5 @@
 import {pipe} from 'fp-ts/lib/function';
+import * as t from 'io-ts';
 import * as E from 'fp-ts/Either';
 import {pageTemplate} from '../../templates';
 import {html} from '../../types/html';
@@ -6,9 +7,13 @@ import * as O from 'fp-ts/Option';
 import {User} from '../../types';
 import {v4} from 'uuid';
 import {Form} from '../../types/form';
+import {formatValidationErrors} from 'io-ts-reporters';
+import {failureWithStatus} from '../../types/failureWithStatus';
+import {StatusCodes} from 'http-status-codes';
 
 type ViewModel = {
   user: User;
+  areaId: string;
 };
 
 const renderForm = (viewModel: ViewModel) =>
@@ -19,7 +24,7 @@ const renderForm = (viewModel: ViewModel) =>
         <label for="name">What is this Equipment called</label>
         <input type="text" name="name" id="name" />
         <input type="hidden" name="id" value="${v4()}" />
-        <input type="hidden" name="areaId" value="foo" />
+        <input type="hidden" name="areaId" value="${viewModel.areaId}" />
         <button type="submit">Confirm and send</button>
       </form>
     `,
@@ -29,7 +34,18 @@ const renderForm = (viewModel: ViewModel) =>
 export const addForm: Form<ViewModel> = {
   renderForm,
   constructForm:
-    () =>
+    input =>
     ({user}) =>
-      E.right({user}),
+      pipe(
+        input,
+        t.strict({area: t.string}).decode,
+        E.mapLeft(formatValidationErrors),
+        E.mapLeft(
+          failureWithStatus('Invalid parameters', StatusCodes.BAD_REQUEST)
+        ),
+        E.map(({area}) => ({
+          user,
+          areaId: area,
+        }))
+      ),
 };
