b9c5132207cbd7be5253376c51f1e5e1b46d2ec4
Daniel Haarhoff <daniel@rknt.de>
2024-06-01 10:55:11 +0100

    Limit a test of a command to only cover its responsibilities
    The idea is to follow CQRS principles and separate the concerns of
    commands from queries.
    In this case that means:
    - it is the read-models responsibility to deal with e.g. training sheet
      registrations for equipment that does not exist
    - 'resources' are only allowed to care about events that they raise
    - the 'process' function of a resource may never fail it can either
      produce an event or not
    - 'process' responsibilities are mostly to ensure idempotency
    - turning the history of events into a meaningful and consistent
      respresentation for users is a query/read-model responsibility
    
    The 'unwrap' like functionality is implemented as 'getSomeOrFail' and
    'getRightOrFail' for use in tests.

--- tests/commands/area/register-training-sheet.test.ts
+++ tests/commands/area/register-training-sheet.test.ts
@@ -1,87 +1,39 @@
-import {v4} from 'uuid';
 import {faker} from '@faker-js/faker';
-import {NonEmptyString, UUID} from 'io-ts-types';
-import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
+import {UUID} from 'io-ts-types';
 import {pipe} from 'fp-ts/lib/function';
-import {create} from '../../../src/commands/area/create';
-import {add} from '../../../src/commands/equipment/add';
 import {registerTrainingSheet} from '../../../src/commands/equipment/register-training-sheet';
-import {getAll} from '../../../src/read-models/equipment/get-all';
 import {DomainEvent} from '../../../src/types';
-import {Command} from '../../../src/commands';
-
-function unwrap<T>(val: O.Option<T>): T {
-  if (O.isSome(val)) {
-    return val.value;
-  }
-  throw new Error('Failed to unwrap');
-}
-
-function callCommand<T>(func: Command<T>['process'], command: T) {
-  return (events: ReadonlyArray<DomainEvent>) => {
-    return RA.append(
-      unwrap(
-        func({
-          command,
-          events: RA.empty,
-        })
-      )
-    )(events);
-  };
-}
-
-function createEquipment(events: ReadonlyArray<DomainEvent>) {
-  const areaName = faker.commerce.productName() as NonEmptyString;
-  const areaDescription = faker.commerce.productDescription();
-  const areaId = v4() as UUID;
-  const equipmentId = v4() as UUID;
-  return {
-    events: pipe(
-      events,
-      callCommand(create.process, {
-        id: areaId,
-        name: areaName,
-        description: areaDescription,
-      }),
-      callCommand(add.process, {
-        id: equipmentId,
-        name: faker.commerce.productName() as NonEmptyString,
-        areaId: areaId,
-      })
-    ),
-    equipmentId,
-  };
-}
+import {getSomeOrFail} from '../../helpers';
 
 describe('register-training-sheet', () => {
-  describe('No training sheet, existing area already registered', () => {
-    const {events, equipmentId} = createEquipment(RA.empty);
-    const trainingSheetId = 'ABC=';
-    const eventsAfter = callCommand(registerTrainingSheet.process, {
-      equipmentId,
-      trainingSheetId,
-    })(events);
+  describe('No training sheet registered', () => {
+    const events: ReadonlyArray<DomainEvent> = [];
+    const command = {
+      equipmentId: faker.string.uuid() as UUID,
+      trainingSheetId: faker.string.alphanumeric(8),
+    };
+
+    const result = pipe(
+      registerTrainingSheet.process({command, events}),
+      getSomeOrFail
+    );
+
     it('Registers a new training sheet id', () => {
-      const registerEvents = eventsAfter.filter(
-        event => event.type === 'EquipmentTrainingSheetRegistered'
-      );
-      expect(registerEvents).toHaveLength(1);
-      expect(registerEvents[0]).toStrictEqual(
+      expect(result).toStrictEqual(
         expect.objectContaining({
           type: 'EquipmentTrainingSheetRegistered',
-          equipmentId,
-          trainingSheetId,
+          equipmentId: command.equipmentId,
+          trainingSheetId: command.trainingSheetId,
         })
       );
     });
-    it('The new training sheet id appears when getting equipment', () => {
-      const equipment = getAll(eventsAfter);
-      console.log(equipment);
-      expect(equipment[0]).toHaveProperty(
-        'trainingSheetId',
-        O.some(trainingSheetId)
-      );
-    });
+  });
+
+  describe('Same training sheet registered', () => {
+    it.todo('Does nothing');
+  });
+
+  describe('Different training sheet registered', () => {
+    it.todo('Registers the sheet');
   });
 });
