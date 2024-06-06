0a0a28c2a2046d656e5e38df4c9a4657953cf90e
Daniel Haarhoff <daniel@rknt.de>
2024-06-06 10:54:27 +0100

    Make it easier to see all available routes

---
+++ src/http/command-to-handlers.ts
@@ -0,0 +1,29 @@
+import {Dependencies} from '../dependencies';
+import {Config} from '../configuration';
+import {Command} from '../commands';
+import {Form} from '../types/form';
+import expressAsyncHandler from 'express-async-handler';
+import {flow} from 'fp-ts/lib/function';
+import {formPost} from './form-post';
+import {formGet} from './form-get';
+import {apiPost} from './api-post';
+
+export const commandToHandlers =
+  (deps: Dependencies, conf: Config) =>
+  <C, V>(noun: string, verb: string, cmd: Command<C> & Form<V>) => [
+    {
+      path: `/${noun}/${verb}`,
+      handler: flow(formPost, expressAsyncHandler)(deps, cmd, `/${noun}`),
+      method: 'post' as const,
+    },
+    {
+      path: `/${noun}/${verb}`,
+      handler: flow(formGet, expressAsyncHandler)(deps, cmd),
+      method: 'get' as const,
+    },
+    {
+      path: `/api/${noun}/${verb}`,
+      handler: flow(apiPost, expressAsyncHandler)(deps, conf, cmd),
+      method: 'post' as const,
+    },
+  ];
--- src/http/index.ts
+++ src/http/index.ts
@@ -1,13 +1,4 @@
-import {flow} from 'fp-ts/lib/function';
-import {apiPost} from './api-post';
-import {formGet} from './form-get';
-import {formPost} from './form-post';
-import asyncHandler from 'express-async-handler';
-import {queryGet} from './query-get';
-
-export const http = {
-  apiPost: flow(apiPost, asyncHandler),
-  formGet: flow(formGet, asyncHandler),
-  formPost: flow(formPost, asyncHandler),
-  queryGet: flow(queryGet, asyncHandler),
-};
+export {queryToHandler} from './query-to-handler';
+export {commandToHandlers} from './command-to-handlers';
+export {ping} from './ping';
+export {createRouter} from './router';
