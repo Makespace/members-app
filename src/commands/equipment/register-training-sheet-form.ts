ca85992e8e4224dd47147af3be881982f48148f5
Daniel Haarhoff <daniel@rknt.de>
2024-06-06 21:03:39 +0100

    Add missing page template to register training sheet

--- src/commands/equipment/register-training-sheet-form.ts
+++ src/commands/equipment/register-training-sheet-form.ts
@@ -1,28 +1,39 @@
 import {pipe} from 'fp-ts/lib/function';
+import * as O from 'fp-ts/Option';
 import * as t from 'io-ts';
 import * as E from 'fp-ts/Either';
 import {html} from '../../types/html';
-import {DomainEvent} from '../../types';
+import {DomainEvent, User} from '../../types';
 import {Form} from '../../types/form';
 import {formatValidationErrors} from 'io-ts-reporters';
 import {failureWithStatus} from '../../types/failureWithStatus';
 import {StatusCodes} from 'http-status-codes';
 import {readModels} from '../../read-models';
+import {pageTemplate} from '../../templates';
 
 type ViewModel = {
+  user: User;
   equipmentId: string;
   equipmentName: string;
 };
 
-const renderForm = (viewModel: ViewModel) => html`
-  <h1>Register training sheet for ${viewModel.equipmentName}</h1>
-  <form action="/equipment/add-training-sheet" method="post">
-    <label for="trainingSheetId">What is the sheet id?</label>
-    <input type="text" name="trainingSheetId" id="trainingSheetId" />
-    <input type="hidden" name="equipmentId" value="${viewModel.equipmentId}" />
-    <button type="submit">Confirm and send</button>
-  </form>
-`;
+const renderForm = (viewModel: ViewModel) =>
+  pipe(
+    html`
+      <h1>Register training sheet for ${viewModel.equipmentName}</h1>
+      <form action="/equipment/add-training-sheet" method="post">
+        <label for="trainingSheetId">What is the sheet id?</label>
+        <input type="text" name="trainingSheetId" id="trainingSheetId" />
+        <input
+          type="hidden"
+          name="equipmentId"
+          value="${viewModel.equipmentId}"
+        />
+        <button type="submit">Confirm and send</button>
+      </form>
+    `,
+    pageTemplate('Register training sheet', O.some(viewModel.user))
+  );
 
 const getEquipmentIdCodec = t.strict({
   equipmentId: t.string,
@@ -52,9 +63,10 @@ const getEquipmentName = (
 
 const constructForm: Form<ViewModel>['constructForm'] =
   input =>
-  ({events}) =>
+  ({events, user}) =>
     pipe(
-      E.Do,
+      {user},
+      E.right,
       E.bind('equipmentId', () => getEquipmentId(input)),
       E.bind('equipmentName', ({equipmentId}) =>
         getEquipmentName(events, equipmentId)
