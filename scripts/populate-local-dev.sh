4cbf9385112edbdb6dc4df084585ad12df0f9f0c
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-06-08 09:31:44 +0100

    Populate area/equipment/training sheet by default for dev

--- scripts/populate-local-dev.sh
+++ scripts/populate-local-dev.sh
@@ -11,3 +11,16 @@ curl -X POST -H 'Authorization: Bearer secret' -H 'Content-Type: application/jso
 curl -X POST -H 'Authorization: Bearer secret' -H 'Content-Type: application/json' \
 	--data '{"memberNumber": "1337", "declaredAt": "2023-01-20"}' \
 	http://localhost:8080/api/super-users/declare
+
+curl -X POST -H 'Authorization: Bearer secret' -H 'Content-Type: application/json' \
+	--data '{"id": "eeaf7f8b-77a3-429d-ae9d-2f7ade53736e", "name": "Metal Shop"}' \
+	http://localhost:8080/api/areas/create
+
+curl -X POST -H 'Authorization: Bearer secret' -H 'Content-Type: application/json' \
+	--data '{"id": "4224ee94-09b0-47d4-ae60-fac46b8ca93e", "name": "Metal Lathe", "areaId": "eeaf7f8b-77a3-429d-ae9d-2f7ade53736e"}' \
+	http://localhost:8080/api/equipment/add
+
+curl -X POST -H 'Authorization: Bearer secret' -H 'Content-Type: application/json' \
+	--data '{"equipmentId": "4224ee94-09b0-47d4-ae60-fac46b8ca93e", "trainingSheetId": "fakeTrainingSheetId"}' \
+	http://localhost:8080/api/equipment/add-training-sheet
+
