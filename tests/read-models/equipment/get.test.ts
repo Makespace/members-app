f85560aabf531cde470ca800b498e3a6b7356b37
Daniel Haarhoff <daniel@rknt.de>
2024-06-12 08:58:07 +0100

    Introduce copy pasted skeleton for adding trainers

---
+++ tests/read-models/equipment/get.test.ts
@@ -0,0 +1,47 @@
+import {faker} from '@faker-js/faker';
+import {DomainEvent} from '../../../src/types';
+import {TestFramework, initTestFramework} from '../test-framework';
+import {NonEmptyString, UUID} from 'io-ts-types';
+import {get} from '../../../src/read-models/equipment/get';
+import {pipe} from 'fp-ts/lib/function';
+import {getSomeOrFail} from '../../helpers';
+
+describe('get', () => {
+  let events: ReadonlyArray<DomainEvent>;
+  let framework: TestFramework;
+  beforeEach(async () => {
+    framework = await initTestFramework();
+  });
+
+  describe('when equipment has a trainer', () => {
+    const createArea = {
+      id: faker.string.uuid() as UUID,
+      name: faker.company.buzzNoun() as NonEmptyString,
+    };
+    const addEquipment = {
+      id: faker.string.uuid() as UUID,
+      name: faker.company.buzzNoun() as NonEmptyString,
+      areaId: createArea.id,
+    };
+    const addTrainer = {
+      memberNumber: faker.number.int(),
+      equipmentId: addEquipment.id,
+    };
+    beforeEach(async () => {
+      await framework.commands.area.create(createArea);
+      await framework.commands.equipment.add(addEquipment);
+      await framework.commands.equipment.addTrainer(addTrainer);
+      events = await framework.getAllEvents();
+    });
+
+    it('returns the equipment', () => {
+      const equipment = pipe(addEquipment.id, get(events), getSomeOrFail);
+      expect(equipment.id).toStrictEqual(addEquipment.id);
+    });
+
+    it.failing('returns the trainer', () => {
+      const equipment = pipe(addEquipment.id, get(events), getSomeOrFail);
+      expect(equipment.trainers[0]).toStrictEqual(addTrainer.memberNumber);
+    });
+  });
+});
--- tests/read-models/test-framework.ts
+++ tests/read-models/test-framework.ts
@@ -81,6 +81,7 @@ export const initTestFramework = async (): Promise<TestFramework> => {
       },
       equipment: {
         add: frameworkify(commands.equipment.add),
+        addTrainer: frameworkify(commands.equipment.addTrainer),
         training_sheet: frameworkify(commands.equipment.training_sheet),
       },
       memberNumbers: {
