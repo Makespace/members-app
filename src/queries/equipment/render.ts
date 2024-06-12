5934df643c9453e318a9dbe985617a5a58946e8c
Daniel Haarhoff <daniel@rknt.de>
2024-06-12 08:41:50 +0100

    Create a pull for trainers by adding a section to the equipment page

--- src/queries/equipment/render.ts
+++ src/queries/equipment/render.ts
@@ -1,8 +1,25 @@
+import {pipe} from 'fp-ts/lib/function';
 import {html} from '../../types/html';
 import {ViewModel} from './view-model';
+import * as RA from 'fp-ts/ReadonlyArray';
+
+const renderTrainers = (trainers: ViewModel['equipment']['trainers']) =>
+  pipe(
+    trainers,
+    RA.map(trainer => html`<li>${trainer}</li>`),
+    RA.match(
+      () => html`<p>This equipment needs trainers.</p>`,
+      items =>
+        html`<ul>
+          ${items.join('\n')}
+        </ul>`
+    )
+  );
 
 export const render = (viewModel: ViewModel) => html`
   <h1>${viewModel.equipment.name}</h1>
+  <h2>Trainers</h2>
+  ${renderTrainers(viewModel.equipment.trainers)}
   <a href="/equipment/add-training-sheet?equipmentId=${viewModel.equipment.id}"
     >Register training sheet</a
   >
