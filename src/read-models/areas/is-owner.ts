7f2a0988cff5fa3d85bd5ca46bf2f36b6533aba3
Daniel Haarhoff <daniel@rknt.de>
2024-06-15 10:54:00 +0100

    Make isOwner tests pass

--- src/read-models/areas/is-owner.ts
+++ src/read-models/areas/is-owner.ts
@@ -1,8 +1,16 @@
 /* eslint-disable unused-imports/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {DomainEvent} from '../../types';
+import {pipe} from 'fp-ts/lib/function';
+import {DomainEvent, isEventOfType} from '../../types';
+import * as RA from 'fp-ts/ReadonlyArray';
 
 export const isOwner =
   (events: ReadonlyArray<DomainEvent>) =>
   (areaId: string, memberNumber: number): boolean =>
-    true;
+    pipe(
+      events,
+      RA.filter(isEventOfType('OwnerAdded')),
+      RA.some(
+        event => event.areaId === areaId && event.memberNumber === memberNumber
+      )
+    );
