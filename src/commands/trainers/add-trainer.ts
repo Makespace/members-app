d8c6eceb5d6db142aa23aa120b8b0f406d3007be
Daniel Haarhoff <daniel@rknt.de>
2024-06-15 12:07:06 +0100

    Allow owners of an area to add trainers to equipment in their area

--- src/commands/trainers/add-trainer.ts
+++ src/commands/trainers/add-trainer.ts
@@ -1,4 +1,4 @@
-import {constructEvent, isEventOfType} from '../../types';
+import {DomainEvent, constructEvent, isEventOfType} from '../../types';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
@@ -6,6 +6,8 @@ import * as O from 'fp-ts/Option';
 import {pipe} from 'fp-ts/lib/function';
 import {Command} from '../command';
 import {isAdminOrSuperUser} from '../is-admin-or-super-user';
+import {Actor} from '../../types/actor';
+import {readModels} from '../../read-models';
 
 const codec = t.strict({
   equipmentId: tt.UUID,
@@ -30,9 +32,36 @@ const resource: Command<AddTrainer>['resource'] = command => ({
   id: command.equipmentId,
 });
 
+const isOwnerOfEquipment = (
+  equipmentId: string,
+  actor: Actor,
+  events: ReadonlyArray<DomainEvent>
+) => {
+  if (actor.tag !== 'user') {
+    return false;
+  }
+  return pipe(
+    equipmentId,
+    readModels.equipment.get(events),
+    O.map(({areaId}) =>
+      readModels.areas.isOwner(events)(areaId, actor.user.memberNumber)
+    ),
+    foo => foo,
+    O.getOrElse(() => false)
+  );
+};
+
+const isAdminOrSuperUserOrEquipmentUser: Command<AddTrainer>['isAuthorized'] =
+  ({input, actor, events}) => {
+    return (
+      isAdminOrSuperUser({actor, events}) ||
+      isOwnerOfEquipment(input.equipmentId, actor, events)
+    );
+  };
+
 export const addTrainer: Command<AddTrainer> = {
   process,
   resource,
   decode: codec.decode,
-  isAuthorized: isAdminOrSuperUser,
+  isAuthorized: isAdminOrSuperUserOrEquipmentUser,
 };
