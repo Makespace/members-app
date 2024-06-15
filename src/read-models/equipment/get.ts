a5699237aa1fb5778f054afe4696298bfcfb4ee5
Daniel Haarhoff <daniel@rknt.de>
2024-06-15 09:54:55 +0100

    Return trainers when getting equipment

--- src/read-models/equipment/get.ts
+++ src/read-models/equipment/get.ts
@@ -1,7 +1,10 @@
 import {pipe} from 'fp-ts/lib/function';
+import * as RM from 'fp-ts/ReadonlyMap';
 import * as O from 'fp-ts/Option';
-import {DomainEvent, isEventOfType} from '../../types';
+import {DomainEvent, SubsetOfDomainEvent, filterByName} from '../../types';
 import * as RA from 'fp-ts/ReadonlyArray';
+import {EventName, isEventOfType} from '../../types/domain-event';
+import {Eq as stringEq} from 'fp-ts/string';
 
 type Equipment = {
   name: string;
@@ -9,13 +12,43 @@ type Equipment = {
   trainers: ReadonlyArray<number>;
 };
 
+type EquipmentState = {
+  name: string;
+  id: string;
+  trainers: Set<number>;
+};
+
+const pertinentEvents: Array<EventName> = ['EquipmentAdded', 'TrainerAdded'];
+
+const updateState = (
+  state: Map<string, EquipmentState>,
+  event: SubsetOfDomainEvent<typeof pertinentEvents>
+) => {
+  if (isEventOfType('EquipmentAdded')(event)) {
+    state.set(event.id, {...event, trainers: new Set()});
+  }
+  if (isEventOfType('TrainerAdded')(event)) {
+    const equipment = state.get(event.equipmentId);
+    if (equipment) {
+      state.set(event.equipmentId, {
+        ...equipment,
+        trainers: equipment.trainers.add(event.memberNumber),
+      });
+    }
+  }
+  return state;
+};
+
 export const get =
   (events: ReadonlyArray<DomainEvent>) =>
   (equipmentId: string): O.Option<Equipment> =>
     pipe(
       events,
-      RA.filter(isEventOfType('EquipmentAdded')),
-      RA.filter(event => event.id === equipmentId),
-      RA.head,
-      O.map(partial => ({...partial, trainers: []}))
+      filterByName(pertinentEvents),
+      RA.reduce(new Map(), updateState),
+      RM.lookup(stringEq)(equipmentId),
+      O.map(state => ({
+        ...state,
+        trainers: Array.from(state.trainers.values()),
+      }))
     );
