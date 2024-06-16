1b5920ad5f3796f7f177ecb0f4fe66bca6ad829e
Hector Dearman <hector.dearman@gmail.com>
2024-06-16 19:37:18 +0100

    Only unrevoked super-users have privileges

--- src/commands/is-admin-or-super-user.ts
+++ src/commands/is-admin-or-super-user.ts
@@ -1,7 +1,8 @@
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as O from 'fp-ts/Option';
 import {pipe} from 'fp-ts/lib/function';
 import {Actor} from '../types/actor';
-import {DomainEvent, isEventOfType} from '../types';
+import {DomainEvent, filterByName} from '../types';
 
 export const isAdminOrSuperUser = (input: {
   actor: Actor;
@@ -14,8 +15,13 @@ export const isAdminOrSuperUser = (input: {
     case 'user':
       return pipe(
         events,
-        RA.filter(isEventOfType('SuperUserDeclared')),
-        RA.some(event => event.memberNumber === actor.user.memberNumber)
+        filterByName(['SuperUserDeclared', 'SuperUserRevoked']),
+        RA.filter(event => event.memberNumber === actor.user.memberNumber),
+        RA.last,
+        O.match(
+          () => false,
+          e => e.type === 'SuperUserDeclared'
+        )
       );
     default:
       return false;
