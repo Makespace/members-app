8f757120978c547c42355068f4b8664186afce54
Daniel Haarhoff <daniel@rknt.de>
2024-06-18 08:23:16 +0100

    Create a seam for getting all information needed for a more useful add owner form #25

--- src/read-models/members/index.ts
+++ src/read-models/members/index.ts
@@ -1,9 +1,11 @@
 import {getAll} from './getAll';
 import {getDetails} from './get-details';
 import {lookupByEmail} from './lookup-by-email';
+import {getPotentialOwners} from './getPotentialOwners';
 
 export const members = {
   lookupByEmail,
   getAll,
   getDetails,
+  getPotentialOwners,
 };
