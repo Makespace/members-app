d32db1ef5cace78a97cd76df70996cde02cb82e6
Daniel Haarhoff <daniel@rknt.de>
2024-06-19 08:23:11 +0100

    Make a test pass, provoking need for more tests #25

--- src/read-models/members/getPotentialOwners.ts
+++ src/read-models/members/getPotentialOwners.ts
@@ -1,9 +1,17 @@
 import * as O from 'fp-ts/Option';
-import {Email} from '../../types';
+import * as RA from 'fp-ts/ReadonlyArray';
+import {
+  DomainEvent,
+  EmailAddress,
+  SubsetOfDomainEvent,
+  filterByName,
+} from '../../types';
+import {pipe} from 'fp-ts/lib/function';
+import {EventName, isEventOfType} from '../../types/domain-event';
 
 type Member = {
   number: number;
-  email: Email;
+  email: EmailAddress;
   name: O.Option<string>;
   pronouns: O.Option<string>;
 };
@@ -17,7 +25,43 @@ type AreaOwners = {
   potential: ReadonlyArray<PotentialOwner>;
 };
 
-export const getPotentialOwners = (): AreaOwners => ({
-  existing: [],
-  potential: [],
-});
+const pertinentEventTypes: Array<EventName> = [
+  'MemberNumberLinkedToEmail',
+  'MemberDetailsUpdated',
+];
+
+const handleEvent = (
+  state: Map<Member['number'], Member>,
+  event: SubsetOfDomainEvent<typeof pertinentEventTypes>
+) => {
+  if (isEventOfType('MemberNumberLinkedToEmail')(event)) {
+    state.set(event.memberNumber, {
+      number: event.memberNumber,
+      email: event.email,
+      name: O.none,
+      pronouns: O.none,
+    });
+  }
+  if (isEventOfType('MemberDetailsUpdated')(event)) {
+    const current = state.get(event.memberNumber);
+    const name = O.fromNullable(event.name);
+    const pronouns = O.fromNullable(event.pronouns);
+    if (current) {
+      state.set(event.memberNumber, {...current, name, pronouns});
+    }
+  }
+  return state;
+};
+
+export const getPotentialOwners = (
+  events: ReadonlyArray<DomainEvent>
+): AreaOwners =>
+  pipe(
+    events,
+    filterByName(pertinentEventTypes),
+    RA.reduce(new Map(), handleEvent),
+    existing => ({
+      existing: Array.from(existing.values()),
+      potential: [],
+    })
+  );
