13c664b7482d1dd1635c632f4a7747400a640b83
Daniel Haarhoff <daniel@rknt.de>
2024-06-19 12:26:25 +0100

    Add a page for members to sign the owner agreement #25

--- src/commands/members/sign-owner-agreement-form.ts
+++ src/commands/members/sign-owner-agreement-form.ts
@@ -1,5 +1,3 @@
-/* eslint-disable unused-imports/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import {pipe} from 'fp-ts/lib/function';
 import * as E from 'fp-ts/Either';
 import {pageTemplate} from '../../templates';
@@ -7,6 +5,7 @@ import {html} from '../../types/html';
 import * as O from 'fp-ts/Option';
 import {Form} from '../../types/form';
 import {User} from '../../types';
+import {ownerAgreement} from './owner-agreement';
 
 type ViewModel = {user: User};
 
@@ -14,15 +13,26 @@ const renderForm = (viewModel: ViewModel) =>
   pipe(
     html`
       <h1>Sign the Owner Agreement</h1>
+      ${ownerAgreement}
       <form action="#" method="post">
-        <button type="submit">Confirm</button>
+        <input
+          type="hidden"
+          name="memberNumber"
+          value="${viewModel.user.memberNumber}"
+        />
+        <input
+          type="hidden"
+          name="signedAt"
+          value="${new Date().toISOString()}"
+        />
+        <button type="submit">Sign Agreement</button>
       </form>
     `,
     pageTemplate('Sign Owner Agreement', O.some(viewModel.user))
   );
 
 const constructForm: Form<ViewModel>['constructForm'] =
-  input =>
+  () =>
   ({user}) =>
     E.right({user});
 
