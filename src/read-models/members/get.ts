e3a9137282a559b0050074c8e479d82c2b141ed5
Hector Dearman <hector.dearman@gmail.com>
2024-06-22 09:41:45 +0100

    Use pertinentEvents pattern for members

--- src/read-models/members/get.ts
+++ src/read-models/members/get.ts
@@ -2,19 +2,14 @@ import {pipe} from 'fp-ts/lib/function';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as O from 'fp-ts/Option';
 import {MemberDetails, DomainEvent, filterByName} from '../../types';
-import {getAllDetails} from './get-all';
+import {getAllDetails, pertinentEvents} from './get-all';
 
 export const getDetails =
   (memberNumber: number) =>
   (events: ReadonlyArray<DomainEvent>): O.Option<MemberDetails> =>
     pipe(
       events,
-      filterByName([
-        'MemberNumberLinkedToEmail',
-        'SuperUserRevoked',
-        'SuperUserDeclared',
-        'MemberDetailsUpdated',
-      ]),
+      filterByName(pertinentEvents),
       RA.filter(e => e.memberNumber === memberNumber),
       getAllDetails,
       allDetails => O.fromNullable(allDetails.get(memberNumber))
