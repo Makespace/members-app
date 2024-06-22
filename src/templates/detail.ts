eaad64e025e358303dcbdc92a194343c84b09f22
Hector Dearman <hector.dearman@gmail.com>
2024-06-22 09:41:45 +0100

    Show missing details as emdash

--- src/templates/detail.ts
+++ src/templates/detail.ts
@@ -1,8 +1,9 @@
 import {pipe} from 'fp-ts/lib/function';
 import * as O from 'fp-ts/Option';
+import {html} from '../types/html';
 
 export const renderOptionalDetail = (o: O.Option<string>) =>
   pipe(
     o,
-    O.getOrElse(() => '')
+    O.getOrElse(() => html`—`)
   );
