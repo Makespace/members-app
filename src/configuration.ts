5edf4ad6377e4bf75cfa433fa66dd0a6f1e668aa
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-06-25 21:07:37 +0100

    Pull google config from env as json

--- src/configuration.ts
+++ src/configuration.ts
@@ -43,11 +43,7 @@ const Config = t.strict({
     tt.BooleanFromString,
     false
   ),
-  GOOGLE_CONNECTIVITY_ENABLED: withDefaultIfEmpty(tt.BooleanFromString, false),
-  GOOGLE_SERVICE_ACCOUNT_KEY_FILE_PATH: withDefaultIfEmpty(
-    t.string,
-    '/etc/makespace/google_serviceaccount_key.json'
-  ),
+  GOOGLE_SERVICE_ACCOUNT_KEY_JSON: withDefaultIfEmpty(tt.JsonRecord, {}),
 });
 
 export type Config = t.TypeOf<typeof Config>;
