61e00e1ea5cd7af8869941c5254a0de20587b538
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-06-28 02:20:36 +0100

    Remove html template string, add member number handlebar helper

--- src/types/html.ts
+++ src/types/html.ts
@@ -1,20 +1,21 @@
 import * as Sum from '@unsplash/sum-types';
+import Handlebars from 'handlebars';
 
-export const html = (
-  literals: TemplateStringsArray,
-  ...substitutions: ReadonlyArray<string | number>
-): string => {
-  if (literals.length === 1 && substitutions.length === 0) {
-    return literals[0];
-  }
-  let result = '';
-  for (let index = 0; index < substitutions.length; index++) {
-    result += literals[index];
-    result += substitutions[index];
-  }
-  result += literals[substitutions.length];
-  return result;
+const initHelpers = () => {
+  Handlebars.registerHelper('member_number', memberNumber => {
+    // This may not be strictly needed as memberNumber should always be a number but following the approach of escaping everything going to end users.
+    const escapedMemberNumber = Handlebars.escapeExpression(memberNumber);
+    return new Handlebars.SafeString(
+      '<a class=memberNumberLink href=/member' +
+        escapedMemberNumber +
+        '/><b>' +
+        escapedMemberNumber +
+        '</b></a>'
+    );
+  });
 };
+
+initHelpers();
 
 interface Page {
   body: string;
