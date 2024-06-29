4d20f8d38a5b4c3aa53955567bc811802b3bd5ea
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-06-29 19:44:07 +0100

    update login page

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,5 +1,4 @@
 export {pageTemplate, templatePage} from './page-template';
-export {pageTemplateNoNav} from './page-template-no-nav';
 export {oopsPage} from './oops';
 export {renderAvatarThumbnail, renderAvatarProfile} from './avatar';
 export {renderOptionalDetail} from './detail';
