9ef071d01361d12461d3a0cf756d52fac4a183b2
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-06-29 23:22:17 +0100

    Render log template -> handlebars

--- src/types/html.ts
+++ src/types/html.ts
@@ -1,5 +1,6 @@
 import * as Sum from '@unsplash/sum-types';
 import Handlebars from 'handlebars';
+import {DateTime} from 'luxon';
 
 const initHelpers = () => {
   Handlebars.registerHelper('member_number', memberNumber => {
@@ -14,6 +15,20 @@ const initHelpers = () => {
         escapedMemberNumber +
         '</b></a>'
     );
+  });
+  Handlebars.registerHelper('display_date', date => {
+    // TODO Do this properly. https://github.com/Makespace/members-app/issues/40
+    switch (typeof date) {
+      case 'string':
+        return date;
+      case 'number':
+        return DateTime.fromMillis(date).toLocaleString();
+      default:
+        if (date instanceof DateTime || date instanceof Date) {
+          return date.toLocaleString();
+        }
+        return 'Unknown Date'; // Placeholder.
+    }
   });
 };
 
