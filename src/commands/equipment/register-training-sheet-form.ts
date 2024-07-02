ae6cbe1ee68bac4efc252921595f124ea4bfdcb7
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-02 23:04:48 +0100

    Register training sheet form -> handlebars

--- src/commands/equipment/register-training-sheet-form.ts
+++ src/commands/equipment/register-training-sheet-form.ts
@@ -1,12 +1,12 @@
 import {pipe} from 'fp-ts/lib/function';
 import * as O from 'fp-ts/Option';
 import * as E from 'fp-ts/Either';
-import {html} from '../../types/html';
 import {User} from '../../types';
 import {Form} from '../../types/form';
 import {pageTemplate} from '../../templates';
 import {getEquipmentName} from './get-equipment-name';
 import {getEquipmentIdFromForm} from './get-equipment-id-from-form';
+import {SafeString} from 'handlebars';
 
 type ViewModel = {
   user: User;
@@ -14,23 +14,27 @@ type ViewModel = {
   equipmentName: string;
 };
 
-const renderForm = (viewModel: ViewModel) =>
-  pipe(
-    html`
-      <h1>Register training sheet for ${viewModel.equipmentName}</h1>
+const RENDER_REGISTER_TRAINING_SHEET_TEMPLATE = Handlebars.compile(
+  `
+      <h1>Register training sheet for {{equipmentName}}</h1>
       <form action="/equipment/add-training-sheet" method="post">
         <label for="trainingSheetId">What is the sheet id?</label>
         <input type="text" name="trainingSheetId" id="trainingSheetId" />
         <input
           type="hidden"
           name="equipmentId"
-          value="${viewModel.equipmentId}"
+          value="{{equipmentId}}"
         />
         <button type="submit">Confirm and send</button>
       </form>
-    `,
-    pageTemplate('Register training sheet', O.some(viewModel.user))
-  );
+    `
+);
+
+const renderForm = (viewModel: ViewModel) =>
+  pageTemplate(
+    'Register training sheet',
+    O.some(viewModel.user)
+  )(new SafeString(RENDER_REGISTER_TRAINING_SHEET_TEMPLATE(viewModel)));
 
 const constructForm: Form<ViewModel>['constructForm'] =
   input =>
