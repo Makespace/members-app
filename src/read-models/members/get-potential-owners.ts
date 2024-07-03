738735f2be240af6fe1750d7325595d98d4e2b56
Daniel Haarhoff <daniel@rknt.de>
2024-07-03 15:01:08 +0100

    Write a failing test for querying failed imports #33

--- src/read-models/members/get-potential-owners.ts
+++ src/read-models/members/get-potential-owners.ts
@@ -118,3 +118,12 @@ export const getPotentialOwners =
         )
       )
     );
+
+type FailedLinking = {
+  memberNumber: number;
+  email: string;
+};
+
+export const getFailedImports = (
+  events: ReadonlyArray<DomainEvent>
+): ReadonlyArray<FailedLinking> => [];
--- src/read-models/members/index.ts
+++ src/read-models/members/index.ts
@@ -1,12 +1,13 @@
 import {getAll, getAllDetails} from './get-all';
 import {getDetails} from './get';
 import {lookupByEmail} from './lookup-by-email';
-import {getPotentialOwners} from './get-potential-owners';
+import {getFailedImports, getPotentialOwners} from './get-potential-owners';
 
 export const members = {
   lookupByEmail,
   getAll,
   getAllDetails,
   getDetails,
+  getFailedImports,
   getPotentialOwners,
 };
