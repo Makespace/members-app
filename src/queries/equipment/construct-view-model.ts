2271e9aa153f76c0775a3c0d4fa8449aa03934c2
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-06 23:52:42 +0100

    Separate quiz results with correctly matched member numbers and emails

--- src/queries/equipment/construct-view-model.ts
+++ src/queries/equipment/construct-view-model.ts
@@ -9,10 +9,12 @@ import {
   failureWithStatus,
 } from '../../types/failure-with-status';
 import {QuizResultViewModel, ViewModel} from './view-model';
-import {User} from '../../types';
+import {MemberDetails, User} from '../../types';
 import {StatusCodes} from 'http-status-codes';
-import {DomainEvent} from '../../types/domain-event';
+import {DomainEvent, EventOfType} from '../../types/domain-event';
 import {Equipment} from '../../read-models/equipment/get';
+import {AllMemberDetails} from '../../read-models/members/get-all';
+import {Logger} from 'pino';
 
 const getEquipment = (
   events: ReadonlyArray<DomainEvent>,
@@ -26,7 +28,22 @@ const getEquipment = (
     )
   );
 
+const indexMembersByEmail = (byId: AllMemberDetails) => {
+  return pipe(
+    Object.values(byId),
+    RA.reduce(
+      {} as Record<string, MemberDetails>,
+      (acc, member: MemberDetails) => {
+        acc[member.email] = member;
+        member.prevEmails.forEach(email => (acc[email] = member));
+        return acc;
+      }
+    )
+  );
+};
+
 const getQuizResults = (
+  logger: Logger,
   events: ReadonlyArray<DomainEvent>,
   equipment: Equipment
 ): TE.TaskEither<
@@ -44,10 +61,42 @@ const getQuizResults = (
     equipment.id
   );
   const members = readModels.members.getAllDetails(events);
+  const membersByEmail = indexMembersByEmail(members);
 
-  const member_training_events = {};
+  const member_training_events: Record<
+    number,
+    EventOfType<'EquipmentTrainingQuizResult'>[]
+  > = {};
+  const not_matching_member_training_events: EventOfType<'EquipmentTrainingQuizResult'>[] =
+    [];
 
-  let per_s;
+  for (const quizEntry of events) {
+    if (quizEntry.type !== 'EquipmentTrainingQuizResult') {
+      continue;
+    }
+
+    const memberFoundByNumber = quizEntry.memberNumberProvided
+      ? members.get(quizEntry.memberNumberProvided)
+      : undefined;
+    const memberFoundByEmail =
+      quizEntry.emailProvided !== null && quizEntry.emailProvided !== undefined
+        ? membersByEmail[quizEntry.emailProvided]
+        : undefined;
+
+    if (!memberFoundByNumber && !memberFoundByEmail) {
+      logger.warn(`Filtering quiz event ${quizEntry.id} as member unknown`);
+      continue;
+    }
+
+    if (memberFoundByNumber === memberFoundByEmail) {
+      if (!member_training_events[memberFoundByNumber!.number]) {
+        member_training_events[memberFoundByNumber!.number] = [];
+      }
+      member_training_events[memberFoundByNumber!.number].push(quizEntry);
+    } else {
+      not_matching_member_training_events.push(quizEntry);
+    }
+  }
 };
 pipe(
   readModels.equipment.getTrainingQuizResults(events)(equipmentId, O.none),
