1c5af60db1491136fd97d38e1279af73ace3468f
Daniel Haarhoff <daniel@rknt.de>
2024-07-06 19:20:55 +0100

    Simplify OCC by using WHERE NOT EXISTS #43

--- src/init-dependencies/event-store/commit-event.ts
+++ src/init-dependencies/event-store/commit-event.ts
@@ -1,6 +1,5 @@
 import {Logger} from 'pino';
 import * as E from 'fp-ts/Either';
-import * as RA from 'fp-ts/ReadonlyArray';
 import {StatusCodes} from 'http-status-codes';
 import {
   FailureWithStatus,
@@ -16,57 +15,35 @@ import {Resource} from '../../types/resource';
 
 export const initialVersionNumber = 0;
 
+const insertEventRow = `
+    INSERT INTO events
+    (id, resource_id, resource_type, resource_version, event_type, payload)
+    SELECT $id, $resource_id, $resource_type, $resource_version, $event_type, $payload
+    WHERE NOT EXISTS (
+      SELECT * FROM events
+      WHERE resource_id = $resource_id
+        AND resource_type = $resource_type
+        AND resource_version = $resource_version
+    );
+  `;
+
 const performTransaction = async (
   event: DomainEvent,
   resource: Resource,
   lastKnownVersion: ResourceVersion,
   dbClient: Client
 ): Promise<'raised-event' | 'last-known-version-out-of-date'> => {
-  const transaction = await dbClient.transaction();
-  try {
-    const resourceVersions = await transaction.execute({
-      sql: `
-        SELECT resource_version
-        FROM events
-        WHERE resource_id =?
-        AND resource_type =?
-        `,
-      args: [resource.id, resource.type],
-    });
-    const currentResourceVersion = Math.max(
-      ...pipe(
-        resourceVersions.rows,
-        RA.map(row => row['resource_version'] as number)
-      )
-    );
-    let newResourceVersion: number;
-
-    if (resourceVersions.rows.length === 0) {
-      newResourceVersion = initialVersionNumber;
-    } else {
-      if (currentResourceVersion === lastKnownVersion) {
-        newResourceVersion = lastKnownVersion + 1;
-      } else {
-        return 'last-known-version-out-of-date';
-      }
-    }
-    const args = pipe(event, ({type, ...payload}) => ({
-      id: uuidv4(),
-      resource_id: resource.id,
-      resource_type: resource.type,
-      resource_version: newResourceVersion,
-      event_type: type,
-      payload: JSON.stringify(payload),
-    }));
-    await transaction.execute({
-      sql: 'INSERT INTO events (id, resource_id, resource_type, resource_version, event_type, payload) VALUES ($id, $resource_id, $resource_type, $resource_version, $event_type, $payload); ',
-      args,
-    });
-    await transaction.commit();
-    return 'raised-event';
-  } finally {
-    transaction.close();
-  }
+  const newResourceVersion =
+    lastKnownVersion === 'no-such-resource'
+      ? initialVersionNumber
+      : lastKnownVersion + 1;
+  const result = await dbClient.execute({
+    sql: insertEventRow,
+    args: constructArgsForNewEventRow(event, resource, newResourceVersion),
+  });
+  return result.rowsAffected === 1
+    ? 'raised-event'
+    : 'last-known-version-out-of-date';
 };
 
 export const commitEvent =
@@ -102,3 +79,17 @@ export const commitEvent =
       })
     );
   };
+
+const constructArgsForNewEventRow = (
+  event: DomainEvent,
+  resource: Resource,
+  version: number
+) =>
+  pipe(event, ({type, ...payload}) => ({
+    id: uuidv4(),
+    resource_id: resource.id,
+    resource_type: resource.type,
+    resource_version: version,
+    event_type: type,
+    payload: JSON.stringify(payload),
+  }));
