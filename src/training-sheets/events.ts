d9f517ed5d4ecafd1eed2b34b0373f20f9a6decd
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-10 23:50:20 +0100

    Fix equal quiz results not being treated as such

--- src/training-sheets/events.ts
+++ src/training-sheets/events.ts
@@ -1,12 +1,23 @@
 import {Eq} from 'fp-ts/lib/Eq';
 import {EventOfType} from '../types/domain-event';
+import {getEq} from 'fp-ts/lib/ReadonlyRecord';
+import {string} from 'fp-ts';
 
 export type QzEvent = EventOfType<'EquipmentTrainingQuizResult'>;
 export type RegEvent = EventOfType<'EquipmentTrainingSheetRegistered'>;
 
+export const NullableStringEq: Eq<string | null> = {
+  equals(x: string | null, y: string | null) {
+    if (x === null || y === null) {
+      return x === y;
+    }
+    return string.Eq.equals(x, y);
+  },
+};
+
 export const QzEventDuplicate: Eq<QzEvent> = {
   equals: (a: QzEvent, b: QzEvent) =>
-    a.quizAnswers === b.quizAnswers &&
+    getEq(NullableStringEq).equals(a.quizAnswers, b.quizAnswers) &&
     a.timestampEpochS === b.timestampEpochS &&
     a.trainingSheetId === b.trainingSheetId,
 };
