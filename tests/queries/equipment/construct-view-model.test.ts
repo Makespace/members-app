26f0f2e5104a7bab77b63a3604bf5dca73941a77
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-14 22:22:58 +0100

    Fix imports

--- tests/queries/equipment/construct-view-model.test.ts
+++ tests/queries/equipment/construct-view-model.test.ts
@@ -3,10 +3,10 @@ import {constructViewModel} from '../../../src/queries/equipment/construct-view-
 import {UUID} from 'io-ts-types';
 import {v4} from 'uuid';
 import {StatusCodes} from 'http-status-codes';
-import {getLeftOrFail} from '../../helpers.js';
-import { happyPathAdapters } from '../../init-dependencies/happy-path-adapters.helper';
+import {happyPathAdapters} from '../../init-dependencies/happy-path-adapters.helper';
 import {arbitraryUser} from '../../types/user.helper';
 import {Dependencies} from '../../../src/dependencies';
+import {getLeftOrFail} from '../../helpers';
 
 describe('construct-view-model', () => {
   describe('no equipment events', () => {
