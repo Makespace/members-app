a0c47b7ded11e0319ee1f4da6b22e950e3f7c954
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-14 23:45:05 +0100

    Log attempts to decode magic link

--- src/authentication/magic-link.ts
+++ src/authentication/magic-link.ts
@@ -7,6 +7,8 @@ import jwt from 'jsonwebtoken';
 import {Config} from '../configuration';
 import {Dependencies} from '../dependencies';
 import {User} from '../types/user';
+import {logPassThru} from '../util';
+import {Logger} from 'pino';
 
 const createMagicLink = (conf: Config) => (user: User) =>
   pipe(
@@ -24,19 +26,21 @@ const verifyToken = (token: string, secret: Config['TOKEN_SECRET']) =>
     failure('Could not verify token')
   );
 
-const decodeMagicLinkFromQuery = (conf: Config) => (input: unknown) =>
-  pipe(
-    input,
-    MagicLinkQuery.decode,
-    E.chainW(({token}) => verifyToken(token, conf.TOKEN_SECRET)),
-    E.chainW(User.decode)
-  );
+const decodeMagicLinkFromQuery =
+  (logger: Logger, conf: Config) => (input: unknown) =>
+    pipe(
+      input,
+      logPassThru(logger, 'Attempting to decode magic link from query'), // Logging is required as a basic form of auth enumeration detection.
+      MagicLinkQuery.decode,
+      E.chainW(({token}) => verifyToken(token, conf.TOKEN_SECRET)),
+      E.chainW(User.decode)
+    );
 
 const strategy = (deps: Dependencies, conf: Config) => {
   return new CustomStrategy((req, done) => {
     pipe(
       req.query,
-      decodeMagicLinkFromQuery(conf),
+      decodeMagicLinkFromQuery(deps.logger, conf),
       E.match(
         error => {
           deps.logger.info(
