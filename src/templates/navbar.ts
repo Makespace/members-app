2d6847461223c3e74d0e2a2ead80afb2112225dc
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-15 23:05:03 +0100

    Reduce indent

--- src/templates/navbar.ts
+++ src/templates/navbar.ts
@@ -4,21 +4,21 @@ export const registerNavBar = () => {
   Handlebars.registerPartial(
     'navbar',
     `
-        <nav class="page-nav">
-          <a href="/"
-            ><img
-              width="64"
-              height="64"
-              src="/static/MS-LOGO-txpt-512.png"
-              alt="Makespace"
-              class="page-nav__logo"
-          /></a>
-          <a href="/members">Members</a>
-          <a href="/equipment">Equipment</a>
-          <a href="/areas">Areas</a>
-          <a href="/log-out">Log out</a>
-          {{loggedInUserSquare user}}
-        </nav>
-      `
+      <nav class="page-nav">
+        <a href="/"
+          ><img
+            width="64"
+            height="64"
+            src="/static/MS-LOGO-txpt-512.png"
+            alt="Makespace"
+            class="page-nav__logo"
+        /></a>
+        <a href="/members">Members</a>
+        <a href="/equipment">Equipment</a>
+        <a href="/areas">Areas</a>
+        <a href="/log-out">Log out</a>
+        {{loggedInUserSquare user}}
+      </nav>
+    `
   );
 };
