e031c9add50c4ece462e5d9fd149981bced750bb
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-17 00:28:50 +0100

    Wrap templating stuff away

--- src/templates/templating.ts
+++ src/templates/templating.ts
@@ -3,36 +3,46 @@
  */
 import Handlebars from 'handlebars';
 
-const knownHelpers: {[name: string]: true} = {};
+class TemplateContext {
+  private knownHelpers: {[name: string]: true} = {};
+  private handlebars: typeof Handlebars;
 
-export const compileTemplate = <T>(input: T) => {
-  return Handlebars.compile(input, {
-    // We enforce that a helper must be registered here before it can be used.
-    knownHelpers: knownHelpers,
-    knownHelpersOnly: true,
-    // Unless in a SafeString everything should be escaped.
-    noEscape: false,
-    // Strict mode so that missing fields produce errors.
-    strict: true,
-    // Don't assume objects exist / throw if there are issues.
-    assumeObjects: false,
-    // Partials needing their own context prevents partials accidentally working because of a parent context in certain situations
-    explicitPartialContext: true,
-  });
-};
-
-export const registerPartialTemplate = (
-  name: string,
-  fn: Handlebars.Template
-) => {
-  Handlebars.registerPartial(name, fn);
-};
-
-export const registerHelper = (name: string, fn: Handlebars.HelperDelegate) => {
-  // Note this enforces helpers to be registered before a template using them can be compiled which is what we want.
-  Handlebars.registerHelper(name, fn);
-  if (name in knownHelpers) {
-    throw new Error(`Tried to register existing helper ${name} again`);
+  constructor() {
+    this.handlebars = Handlebars.create();
   }
-  knownHelpers[name] = true;
-};
+
+  compileTemplate = <T>(input: T) => {
+    const template = this.handlebars.compile(input, {
+      // We enforce that a helper must be registered here before it can be used.
+      knownHelpers: this.knownHelpers,
+      knownHelpersOnly: true,
+      // Unless in a SafeString everything should be escaped.
+      noEscape: false,
+      // Strict mode so that missing fields produce errors.
+      strict: true,
+      // Don't assume objects exist / throw if there are issues.
+      assumeObjects: false,
+      // Partials needing their own context prevents partials accidentally working because of a parent context in certain situations
+      explicitPartialContext: true,
+    });
+    // TODO - Can we do any further checking with the template here?
+    return template;
+  };
+
+  registerPartialTemplate = (name: string, fn: Handlebars.Template) => {
+    this.handlebars.registerPartial(name, fn);
+  };
+
+  registerHelper = (name: string, fn: Handlebars.HelperDelegate) => {
+    // Note this enforces helpers to be registered before a template using them can be compiled which is what we want.
+    this.handlebars.registerHelper(name, fn);
+    if (name in this.knownHelpers) {
+      throw new Error(`Tried to register existing helper ${name} again`);
+    }
+    this.knownHelpers[name] = true;
+  };
+}
+
+// Wrap global stuff away in a single Global object which makes isolating context between tests easier while
+// still giving the benefits of a global object.
+export const ctx = new TemplateContext();
