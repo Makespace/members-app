bb47c8c3d484ad7ec97560320ec8bd2f58c1f3a1
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-19 22:12:48 +0100

    member input selector back to html template

--- src/templates/member-input.ts
+++ src/templates/member-input.ts
@@ -1,25 +1,31 @@
+import {MemberDetails} from '../types';
+import {html, Html, optionalSafe, sanitizeString} from '../types/html';
+import {getGravatarThumbnail} from './avatar';
+import {filterList} from './filter-list';
 
+export const memberInputSelector = (member: MemberDetails): Html => html`
+  <div class="fieldset-item">
+    <input
+      type="radio"
+      id="member-${member.memberNumber}"
+      name="memberNumber"
+      value="${member.memberNumber}"
+    />
+    <label for="member-${member.memberNumber}">
+      ${getGravatarThumbnail(member.emailAddress, member.memberNumber)}
+      <span>
+        ${optionalSafe(member.name)} (${optionalSafe(member.pronouns)})
+        (${sanitizeString(member.emailAddress)})
+      </span>
+    </label>
+  </div>
+`;
 
-export const registerMemberInput = () => {
-  Handlebars.registerPartial(
-    'memberInput',
-    `
-      <fieldset>
-        <legend>Select a member:</legend>
-        {{#filterList this "Members"}}
-          <div class="fieldset-item">
-           <input type="radio" id="member-{{number}}" name="memberNumber" value="{{number}}"/>
-           <label for="member-{{number}}">
-           {{avatar_thumbnail this.emailAddress this.memberNumber}}
-            <span>
-              {{optional_detail name}}
-              ({{optional_detail pronouns}})
-              ({{email}})
-            </span>
-           </label>
-         </div>
-        {{/filterList}}
-      </fieldset>
-    `
-  );
-};
+export const memberInput = (
+  members: ReadonlyArray<MemberDetails>
+): Html => html`
+  <fieldset>
+    <legend>Select a member:</legend>
+    ${filterList(members, 'Members', memberInputSelector)}
+  </fieldset>
+`;
