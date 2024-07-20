2186938e507a8e80b13e5b63460662a9a293def3
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-20 10:27:02 +0100

    Revert members command

--- src/commands/members/edit-name-form.ts
+++ src/commands/members/edit-name-form.ts
@@ -1,6 +1,7 @@
 import {flow, pipe} from 'fp-ts/lib/function';
 import * as E from 'fp-ts/Either';
 import {pageTemplate} from '../../templates';
+import {html} from '../../types/html';
 import {User} from '../../types';
 import {Form} from '../../types/form';
 import * as t from 'io-ts';
@@ -9,31 +10,28 @@ import {formatValidationErrors} from 'io-ts-reporters';
 import {failureWithStatus} from '../../types/failure-with-status';
 import {StatusCodes} from 'http-status-codes';
 
-
 type ViewModel = {
   user: User;
   memberNumber: number;
 };
 
-const RENDER_EDIT_NAME_FORM_TEMPLATE = Handlebars.compile(`
-  <h1>Edit name</h1>
-  <form action="?next=/member/{{user.memberNumber}}" method="post">
-    <label for="name">New name</label>
-    <input type="text" name="name" id="name" />
-    <input
-      type="hidden"
-      name="memberNumber"
-      value="{{memberNumber}}"
-    />
-    <button type="submit">Confirm</button>
-  </form>
-`);
-
 const renderForm = (viewModel: ViewModel) =>
-  pageTemplate(
-    'Edit name',
-    viewModel.user
-  )(new SafeString(RENDER_EDIT_NAME_FORM_TEMPLATE(viewModel)));
+  pipe(
+    html`
+      <h1>Edit name</h1>
+      <form action="?next=/member/${viewModel.user.memberNumber}" method="post">
+        <label for="name">New name</label>
+        <input type="text" name="name" id="name" />
+        <input
+          type="hidden"
+          name="memberNumber"
+          value="${viewModel.memberNumber}"
+        />
+        <button type="submit">Confirm</button>
+      </form>
+    `,
+    pageTemplate('Edit name', viewModel.user)
+  );
 
 const paramsCodec = t.strict({
   member: tt.NumberFromString,
