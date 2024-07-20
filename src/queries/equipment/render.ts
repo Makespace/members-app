ab180f199cf30625f7cde4ed82c53c7a1d1b3b86
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-20 10:22:05 +0100

    Update equipment to be more concise using pipe

--- src/queries/equipment/render.ts
+++ src/queries/equipment/render.ts
@@ -1,20 +1,38 @@
+import {pipe} from 'fp-ts/lib/function';
 import {pageTemplate} from '../../templates';
 import {displayDate} from '../../templates/display-date';
 import {renderMemberNumber} from '../../templates/member-number';
-import {html, joinHtml, optionalSafe, sanitizeString} from '../../types/html';
-import {ViewModel} from './view-model';
+import {
+  blankIfEmpty,
+  displayIfEmpty,
+  html,
+  joinHtml,
+  optionalSafe,
+  sanitizeString,
+} from '../../types/html';
+import {
+  QuizResultUnknownMemberViewModel,
+  QuizResultViewModel,
+  ViewModel,
+} from './view-model';
 import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 
 const trainersList = (trainers: ViewModel['equipment']['trainers']) => html`
   <h2>Trainers</h2>
   <ul>
-    ${trainers.length > 0
-      ? joinHtml(
-          trainers.map(
+    ${pipe(
+      trainers,
+      displayIfEmpty(html`<p>This equipment needs trainers.</p>`)(arr =>
+        pipe(
+          arr,
+          RA.map(
             memberNumber => html`<li>${renderMemberNumber(memberNumber)}</li>`
-          )
+          ),
+          joinHtml
         )
-      : html`<p>This equipment needs trainers.</p>`}
+      )
+    )}
   </ul>
 `;
 
@@ -63,59 +81,74 @@ const currentlyTrainedUsersTable = (viewModel: ViewModel) => html`
     <tr>
       <th>Member Number</th>
     </tr>
-    ${joinHtml(
-      viewModel.equipment.trainedMembers.map(
+    ${pipe(
+      viewModel.equipment.trainedMembers,
+      RA.map(
         memberNumber =>
           html`<tr>
             <td>${renderMemberNumber(memberNumber)}</td>
           </tr>`
-      )
+      ),
+      joinHtml
     )}
   </table>
 `;
 
-const waitingForTrainingTable = (viewModel: ViewModel) => html`
-  <table>
-    ${viewModel.trainingQuizResults.quizPassedNotTrained.knownMember.length > 0
-      ? html`
-          <tr>
-            <th hidden>Quiz ID</th>
-            <th>Timestamp</th>
-            <th>Member Number</th>
-            <th>Score</th>
-            <th>Actions</th>
-            <th hidden>Other Attempts</th>
-          </tr>
-          ${joinHtml(
-            viewModel.trainingQuizResults.quizPassedNotTrained.knownMember.map(
-              quiz => html`
-                <tr class="passed_training_quiz_row">
-                  <td hidden>${sanitizeString(quiz.id)}</td>
-                  <td>${displayDate(quiz.timestamp)}</td>
-                  <td>${renderMemberNumber(quiz.memberNumber)}</td>
-                  <td>
-                    ${quiz.score} / ${quiz.maxScore} (${quiz.percentage}%)
-                  </td>
-                  <td><button>Mark as trained</button></td>
-                  <td hidden>
-                    ${joinHtml(
-                      quiz.otherAttempts
-                        .map(sanitizeString)
-                        .map(v => html`${v}`)
-                    )}
-                  </td>
-                </tr>
-              `
-            )
-          )}
-        `
-      : html`<p>No one is waiting for training</p>`}
-  </table>
+const waitingForTrainingRow = (quiz: QuizResultViewModel) => html`
+  <tr class="passed_training_quiz_row">
+    <td hidden>${sanitizeString(quiz.id)}</td>
+    <td>${displayDate(quiz.timestamp)}</td>
+    <td>${renderMemberNumber(quiz.memberNumber)}</td>
+    <td>${quiz.score} / ${quiz.maxScore} (${quiz.percentage}%)</td>
+    <td><button>Mark as trained</button></td>
+    <td hidden>
+      ${joinHtml(quiz.otherAttempts.map(sanitizeString).map(v => html`${v}`))}
+    </td>
+  </tr>
 `;
 
-const unknownMemberWaitingForTrainingTable = (viewModel: ViewModel) => html`
-  ${viewModel.trainingQuizResults.quizPassedNotTrained.unknownMember.length > 0
-    ? html`
+const waitingForTrainingTable = (viewModel: ViewModel) =>
+  pipe(
+    viewModel.trainingQuizResults.quizPassedNotTrained.knownMember,
+    displayIfEmpty(html`<p>No one is waiting for training</p>`)(
+      passedQuizes => html`
+        <tr>
+          <th hidden>Quiz ID</th>
+          <th>Timestamp</th>
+          <th>Member Number</th>
+          <th>Score</th>
+          <th>Actions</th>
+          <th hidden>Other Attempts</th>
+        </tr>
+        ${pipe(passedQuizes, RA.map(waitingForTrainingRow), joinHtml)}
+      `
+    )
+  );
+
+const passedUnknownQuizRow = (
+  unknownQuiz: QuizResultUnknownMemberViewModel
+) => html`
+  <tr class="passed_training_quiz_row">
+    <td hidden>${sanitizeString(unknownQuiz.id)}</td>
+    <td>${displayDate(unknownQuiz.timestamp)}</td>
+    ${O.isSome(unknownQuiz.memberNumberProvided)
+      ? html`<td>
+          ${renderMemberNumber(unknownQuiz.memberNumberProvided.value)}
+        </td>`
+      : html`<td>${optionalSafe(unknownQuiz.memberNumberProvided)}</td>`}
+    <td>${optionalSafe(unknownQuiz.emailProvided)}</td>
+    <td>
+      ${unknownQuiz.score} / ${unknownQuiz.maxScore}
+      (${unknownQuiz.percentage}%)
+    </td>
+  </tr>
+`;
+
+const unknownMemberWaitingForTrainingTable = (viewModel: ViewModel) =>
+  pipe(
+    viewModel.trainingQuizResults.quizPassedNotTrained.unknownMember,
+    blankIfEmpty(
+      unknownQuizes => html`
         <h3>Waiting for Training - Unknown Member</h3>
         <p>
           Quizes completed by members without matching email and member numbers.
@@ -128,73 +161,50 @@ const unknownMemberWaitingForTrainingTable = (viewModel: ViewModel) => html`
             <th>Email Provided</th>
             <th>Score</th>
           </tr>
-          ${joinHtml(
-            viewModel.trainingQuizResults.quizPassedNotTrained.unknownMember.map(
-              unknownQuiz => html`
-                <tr class="passed_training_quiz_row">
-                  <td hidden>${sanitizeString(unknownQuiz.id)}</td>
-                  <td>${displayDate(unknownQuiz.timestamp)}</td>
-                  ${O.isSome(unknownQuiz.memberNumberProvided)
-                    ? html`<td>
-                        ${renderMemberNumber(
-                          unknownQuiz.memberNumberProvided.value
-                        )}
-                      </td>`
-                    : html`<td>
-                        ${optionalSafe(unknownQuiz.memberNumberProvided)}
-                      </td>`}
-                  <td>${optionalSafe(unknownQuiz.emailProvided)}</td>
-                  <td>
-                    ${unknownQuiz.score} / ${unknownQuiz.maxScore}
-                    (${unknownQuiz.percentage}%)
-                  </td>
-                </tr>
-              `
-            )
-          )}
+          ${pipe(unknownQuizes, RA.map(passedUnknownQuizRow), joinHtml)}
         </table>
       `
-    : html``}
+    )
+  );
+
+const failedKnownQuizRow = (knownQuiz: QuizResultViewModel) => html`
+  <tr class="failed_training_quiz_row">
+    <td hidden>${sanitizeString(knownQuiz.id)}</td>
+    <td>${displayDate(knownQuiz.timestamp)}</td>
+    <td>${renderMemberNumber(knownQuiz.memberNumber)}</td>
+    <td>
+      ${knownQuiz.score} / ${knownQuiz.maxScore} (${knownQuiz.percentage}%)
+    </td>
+    <td hidden>
+      ${joinHtml(
+        knownQuiz.otherAttempts.map(sanitizeString).map(v => html`${v}`)
+      )}
+    </td>
+  </tr>
 `;
 
-const failedQuizTrainingTable = (viewModel: ViewModel) => html`
-  ${viewModel.trainingQuizResults.failedQuizNotTrained.knownMember.length > 0
-    ? html`
-        <h3>Failed quizes</h3>
-        <p>Members who haven't passed (but have attempted) the quiz</p>
-        <table>
-          <tr>
-            <th hidden>Quiz ID</th>
-            <th>Timestamp</th>
-            <th>Member Number</th>
-            <th>Score</th>
-            <th hidden>Other Attempts</th>
-          </tr>
-          ${joinHtml(
-            viewModel.trainingQuizResults.failedQuizNotTrained.knownMember.map(
-              member => html`
-                <tr class="failed_training_quiz_row">
-                  <td hidden>${sanitizeString(member.id)}</td>
-                  <td>${displayDate(member.timestamp)}</td>
-                  <td>${renderMemberNumber(member.memberNumber)}</td>
-                  <td>
-                    ${member.score} / ${member.maxScore} (${member.percentage}%)
-                  </td>
-                  <td hidden>
-                    ${joinHtml(
-                      member.otherAttempts
-                        .map(sanitizeString)
-                        .map(v => html`${v}`)
-                    )}
-                  </td>
-                </tr>
-              `
-            )
-          )}
-        </table>
-      `
-    : html``}
-`;
+const failedQuizTrainingTable = (viewModel: ViewModel) =>
+  pipe(
+    viewModel.trainingQuizResults.failedQuizNotTrained.knownMember,
+    blankIfEmpty(
+      pipe(
+        knownQuizes => html`
+          <h3>Failed quizes</h3>
+          <p>Members who haven't passed (but have attempted) the quiz</p>
+          <table>
+            <tr>
+              <th hidden>Quiz ID</th>
+              <th>Timestamp</th>
+              <th>Member Number</th>
+              <th>Score</th>
+              <th hidden>Other Attempts</th>
+            </tr>
+            ${pipe(knownQuizes, RA.map(failedKnownQuizRow), joinHtml)}
+          </table>
+        `
+      )
+    )
+  );
 
 const trainingQuizResults = (viewModel: ViewModel) => html`
   <h2>Training Quiz Results</h2>
@@ -205,11 +215,13 @@ const trainingQuizResults = (viewModel: ViewModel) => html`
 `;
 
 export const render = (viewModel: ViewModel) =>
-  pageTemplate(
-    viewModel.equipment.name,
-    viewModel.user
-  )(html`
-    <h1>${sanitizeString(viewModel.equipment.name)}</h1>
-    ${equipmentActions(viewModel)} ${trainersList(viewModel.equipment.trainers)}
-    ${currentlyTrainedUsersTable(viewModel)} ${trainingQuizResults(viewModel)}
-  `);
+  pipe(
+    viewModel,
+    (viewModel: ViewModel) => html`
+      <h1>${sanitizeString(viewModel.equipment.name)}</h1>
+      ${equipmentActions(viewModel)}
+      ${trainersList(viewModel.equipment.trainers)}
+      ${currentlyTrainedUsersTable(viewModel)} ${trainingQuizResults(viewModel)}
+    `,
+    pageTemplate(viewModel.equipment.name, viewModel.user)
+  );
