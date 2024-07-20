cb0db1963047a2523a3990cf5b55eb07ae8299a6
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-20 09:43:09 +0100

    Revert equipment forms to templates

--- src/commands/equipment/add-form.ts
+++ src/commands/equipment/add-form.ts
@@ -2,6 +2,7 @@ import {pipe} from 'fp-ts/lib/function';
 import * as t from 'io-ts';
 import * as E from 'fp-ts/Either';
 import {pageTemplate} from '../../templates';
+import {html, safe, sanitizeString} from '../../types/html';
 import {DomainEvent, User} from '../../types';
 import {v4} from 'uuid';
 import {Form} from '../../types/form';
@@ -10,30 +11,30 @@ import {failureWithStatus} from '../../types/failure-with-status';
 import {StatusCodes} from 'http-status-codes';
 import {readModels} from '../../read-models';
 
-
 type ViewModel = {
   user: User;
   areaId: string;
   areaName: string;
-  newEquipmentId: string;
 };
 
-const RENDER_ADD_EQUIPMENT_FORM_TEMPLATE = Handlebars.compile(`
-  <h1>Add equipment to {{areaName}}</h1>
-  <form action="/equipment/add" method="post">
-    <label for="name">What is this Equipment called</label>
-    <input type="text" name="name" id="name" />
-    <input type="hidden" name="id" value="{{newEquipmentId}}" />
-    <input type="hidden" name="areaId" value="{{areaId}}" />
-    <button type="submit">Confirm and send</button>
-  </form>
-`);
-
 const renderForm = (viewModel: ViewModel) =>
-  pageTemplate(
-    'Create Equipment',
-    viewModel.user
-  )(new SafeString(RENDER_ADD_EQUIPMENT_FORM_TEMPLATE(viewModel)));
+  pipe(
+    html`
+      <h1>Add equipment to ${sanitizeString(viewModel.areaName)}</h1>
+      <form action="/equipment/add" method="post">
+        <label for="name">What is this Equipment called</label>
+        <input type="text" name="name" id="name" />
+        <input type="hidden" name="id" value="${safe(v4())}" />
+        <input
+          type="hidden"
+          name="areaId"
+          value="${sanitizeString(viewModel.areaId)}"
+        />
+        <button type="submit">Confirm and send</button>
+      </form>
+    `,
+    pageTemplate('Create Equipment', viewModel.user)
+  );
 
 const getAreaId = (input: unknown) =>
   pipe(
@@ -61,8 +62,7 @@ const constructForm: Form<ViewModel>['constructForm'] =
       E.Do,
       E.bind('areaId', () => getAreaId(input)),
       E.bind('areaName', ({areaId}) => getAreaName(events, areaId)),
-      E.bind('user', () => E.right(user)),
-      E.bind('newEquipmentId', () => E.right(v4()))
+      E.bind('user', () => E.right(user))
     );
 
 export const addForm: Form<ViewModel> = {
