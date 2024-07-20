775a971bda4a600ba2f9fc0722734b1471f7df53
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-20 13:31:59 +0100

    Fix isolated pages not being rendered as templates

--- src/authentication/check-your-mail.ts
+++ src/authentication/check-your-mail.ts
@@ -1,10 +1,16 @@
+import {pipe} from 'fp-ts/lib/function';
+import {isolatedPageTemplate} from '../templates/page-template';
 import {html, sanitizeString} from '../types/html';
 
-export const checkYourMailPage = (submittedEmailAddress: string) => html`
-  <h1>Check your mail</h1>
-  <p>
-    If <b>${sanitizeString(submittedEmailAddress)}</b> is linked to a Makespace
-    number you should receive an email with that number.
-  </p>
-  <p>If nothing happens please reach out to the Makespace Database Team.</p>
-`;
+export const checkYourMailPage = (submittedEmailAddress: string) =>
+  pipe(
+    html`
+      <h1>Check your mail</h1>
+      <p>
+        If <b>${sanitizeString(submittedEmailAddress)}</b> is linked to a
+        Makespace number you should receive an email with that number.
+      </p>
+      <p>If nothing happens please reach out to the Makespace Database Team.</p>
+    `,
+    isolatedPageTemplate('Check your mail')
+  );
