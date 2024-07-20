4cb4e70a5c30730646c17458f932ab685ff8724b
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-20 13:31:59 +0100

    Fix isolated pages not being rendered as templates

--- src/authentication/log-in-page.ts
+++ src/authentication/log-in-page.ts
@@ -1,11 +1,16 @@
+import {pipe} from 'fp-ts/lib/function';
 import {html} from '../types/html';
+import {isolatedPageTemplate} from '../templates/page-template';
 
-export const logInPage = () => html`
-  <h1>Log in</h1>
-  <form action="/auth" method="post">
-    <label for="email">E-Mail: </label>
-    <input id="email" type="email" required name="email" value="" />
-    <p>We will email you a magic log in link.</p>
-    <button type="submit">Email me a link</button>
-  </form>
-`;
+export const logInPage = pipe(
+  html`
+    <h1>Log in</h1>
+    <form action="/auth" method="post">
+      <label for="email">E-Mail: </label>
+      <input id="email" type="email" required name="email" value="" />
+      <p>We will email you a magic log in link.</p>
+      <button type="submit">Email me a link</button>
+    </form>
+  `,
+  isolatedPageTemplate('Login')
+);
