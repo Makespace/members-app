c1083e0925c112ed9c14ef35a1fe815aa78eee23
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-20 09:44:12 +0100

    Revert member number linking form to templates

--- src/commands/member-numbers/link-number-to-email-form.ts
+++ src/commands/member-numbers/link-number-to-email-form.ts
@@ -1,32 +1,31 @@
+import {pipe} from 'fp-ts/lib/function';
 import * as E from 'fp-ts/Either';
 import {pageTemplate} from '../../templates';
+import {html} from '../../types/html';
 import {User} from '../../types';
 import {Form} from '../../types/form';
-
 
 type ViewModel = {
   user: User;
 };
 
-const RENDER_LINK_NUMBER_TO_EMAIL_TEMPLATE = Handlebars.compile(`
-    <h1>Link a member number to an e-mail address</h1>
-    <form action="/members/create" method="post">
-      <label for="email"
-        >Which e-mail does the member want to use to identify
-        themselves?</label
-      >
-      <input type="text" name="email" id="email" />
-      <label for="memberNumber">Which member number should they get? </label>
-      <input type="text" name="memberNumber" id="memberNumber" />
-      <button type="submit">Confirm and send</button>
-    </form>
-  `);
-
 const renderForm = (viewModel: ViewModel) =>
-  pageTemplate(
-    'Link a member number to an e-mail address',
-    viewModel.user
-  )(new SafeString(RENDER_LINK_NUMBER_TO_EMAIL_TEMPLATE(viewModel)));
+  pipe(
+    html`
+      <h1>Link a member number to an e-mail address</h1>
+      <form action="/members/create" method="post">
+        <label for="email"
+          >Which e-mail does the member want to use to identify
+          themselves?</label
+        >
+        <input type="text" name="email" id="email" />
+        <label for="memberNumber">Which member number should they get? </label>
+        <input type="text" name="memberNumber" id="memberNumber" />
+        <button type="submit">Confirm and send</button>
+      </form>
+    `,
+    pageTemplate('Link a member number to an e-mail address', viewModel.user)
+  );
 
 const constructForm: Form<ViewModel>['constructForm'] =
   () =>
