854c21e3bc0903bb780762143235a57c3e640018
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-20 11:38:27 +0100

    all equipment -> back to html templates

--- src/queries/all-equipment/render.ts
+++ src/queries/all-equipment/render.ts
@@ -1,50 +1,51 @@
+import {pipe} from 'fp-ts/lib/function';
+import {html, joinHtml, sanitizeString} from '../../types/html';
+import * as RA from 'fp-ts/ReadonlyArray';
 import {ViewModel} from './view-model';
-import {pageTemplate} from '../../templates';
 
-
-Handlebars.registerPartial(
-  'render_equipment_table',
-  `
-  <table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Area</th>
-      </tr>
-    </thead>
-    <tbody>
-      {{#each equipment}}
+const renderEquipment = (allEquipment: ViewModel['equipment']) =>
+  pipe(
+    allEquipment,
+    RA.map(
+      equipment => html`
         <tr>
-          <td><a href="/equipment/{{this.id}}">{{this.name}}</a></td>
           <td>
-            <a href="/areas/{{this.areaId}}">{{this.areaName}}</a>
+            <a href="/equipment/${sanitizeString(equipment.id)}"
+              >${sanitizeString(equipment.name)}</a
+            >
+          </td>
+          <td>
+            <a href="/areas/${sanitizeString(equipment.areaId)}"
+              >${sanitizeString(equipment.areaName)}</a
+            >
           </td>
         </tr>
-      {{else}}
-        <p>Currently no Equipment</p>
-      {{/each}}
-    </tbody>
-  </table>
-  `
-);
+      `
+    ),
+    RA.match(
+      () => html` <p>Currently no Equipment</p> `,
+      rows => html`
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Area</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${joinHtml(rows)}
+          </tbody>
+        </table>
+      `
+    )
+  );
 
-Handlebars.registerPartial(
-  'add_area_link',
-  '<a href="/areas/create">Add area of responsibility</a>'
-);
+const addAreaCallToAction = html`
+  <a href="/areas/create">Add area of responsibility</a>
+`;
 
-const RENDER_ALL_EQUIPMENT_TEMPLATE = Handlebars.compile(
-  `
+export const render = (viewModel: ViewModel) => html`
   <h1>Equipment of Makespace</h1>
-  {{#if isSuperUser}}
-    {{> add_area_link }}
-  {{/if}}
-  {{> render_equipment_table }}
-`
-);
-
-export const render = (viewModel: ViewModel) =>
-  pageTemplate(
-    'Equipment',
-    viewModel.user
-  )(new SafeString(RENDER_ALL_EQUIPMENT_TEMPLATE(viewModel)));
+  ${viewModel.isSuperUser ? addAreaCallToAction : html``}
+  ${renderEquipment(viewModel.equipment)}
+`;
