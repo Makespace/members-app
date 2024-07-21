a75f6093a0eaa30b4926392d8d2512fe19816a2b
Hector Dearman <hector.dearman@gmail.com>
2024-07-21 19:59:20 +0100

    Update oops handler to be styled

--- src/templates/oops.ts
+++ src/templates/oops.ts
@@ -1,15 +1,17 @@
 import Handlebars, {SafeString} from 'handlebars';
+import {isolatedPageTemplate} from './page-template';
 
 const OOPS_PAGE_TEMPLATE = Handlebars.compile(
   `
   <h1>Sorry, we have encountered a problem</h1>
   <p>{{message}}</p>
   <p>
-    Please try again. If the problem persists please reach out in the google
-    group.
+    Please try again. If the problem persists please reach out in the google group.
   </p>
   `
 );
 
 export const oopsPage = (message: string | SafeString) =>
-  OOPS_PAGE_TEMPLATE({message});
+  isolatedPageTemplate('Oops!')(
+    new Handlebars.SafeString(OOPS_PAGE_TEMPLATE({message}))
+  );
