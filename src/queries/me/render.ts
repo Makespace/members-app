b9717d40010385d8e03ec59da36b60ac051c2c28
Daniel Haarhoff <daniel@rknt.de>
2024-07-22 08:36:04 +0100

    Pare down rendering #59

--- src/queries/me/render.ts
+++ src/queries/me/render.ts
@@ -1,11 +1,9 @@
 import {pipe} from 'fp-ts/lib/function';
-import {getGravatarProfile, getGravatarThumbnail} from '../../templates/avatar';
-import {Html, html, optionalSafe, safe, sanitizeString} from '../../types/html';
+import {getGravatarThumbnail} from '../../templates/avatar';
+import {html, optionalSafe, safe, sanitizeString} from '../../types/html';
 import {ViewModel} from './view-model';
 import {pageTemplate} from '../../templates';
 import {renderMemberNumber} from '../../templates/member-number';
-
-const ownPageBanner = html`<h1>This is your profile!</h1>`;
 
 const editName = (viewModel: ViewModel) =>
   html`<a href="/members/edit-name?member=${viewModel.member.memberNumber}"
@@ -20,23 +18,12 @@ const editPronouns = (viewModel: ViewModel) =>
 const editAvatar = () =>
   html`<a href="https://gravatar.com/profile">Edit via Gravatar</a>`;
 
-const ifSelf = (viewModel: ViewModel, fragment: Html) =>
-  viewModel.isSelf ? fragment : '';
-
 export const render = (viewModel: ViewModel) =>
   pipe(
     html`
-      ${ifSelf(viewModel, ownPageBanner)}
-      <div class="profile">
-        ${getGravatarProfile(
-          viewModel.member.emailAddress,
-          viewModel.member.memberNumber
-        )}
-      </div>
+      <h1>Your Makespace profile</h1>
+      <h2>Your details</h2>
       <table>
-        <caption>
-          Details
-        </caption>
         <tbody>
           <tr>
             <th scope="row">Member number</th>
@@ -49,15 +36,14 @@ export const render = (viewModel: ViewModel) =>
           <tr>
             <th scope="row">Name</th>
             <td>
-              ${optionalSafe(viewModel.member.name)}
-              ${ifSelf(viewModel, editName(viewModel))}
+              ${optionalSafe(viewModel.member.name)} ${editName(viewModel)}
             </td>
           </tr>
           <tr>
             <th scope="row">Pronouns</th>
             <td>
               ${optionalSafe(viewModel.member.pronouns)}
-              ${ifSelf(viewModel, editPronouns(viewModel))}
+              ${editPronouns(viewModel)}
             </td>
           </tr>
           <tr>
@@ -67,7 +53,7 @@ export const render = (viewModel: ViewModel) =>
                 viewModel.member.emailAddress,
                 viewModel.member.memberNumber
               )}
-              ${ifSelf(viewModel, editAvatar())}
+              ${editAvatar()}
             </td>
           </tr>
         </tbody>
