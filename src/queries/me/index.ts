19025bc02c69416cf9dcfb49e1483fc20a6af3ef
Daniel Haarhoff <daniel@rknt.de>
2024-07-22 08:28:58 +0100

    Fork the member page to the /me endpoint to create space for building a new landing page #59

--- src/queries/me/index.ts
+++ src/queries/me/index.ts
@@ -1,10 +1,18 @@
+import {pipe} from 'fp-ts/lib/function';
 import * as TE from 'fp-ts/TaskEither';
+import {render} from './render';
+import {constructViewModel} from './construct-view-model';
 import {Query} from '../query';
 import {HttpResponse} from '../../types';
 
-export const me: Query = () => user =>
-  TE.right(
-    HttpResponse.mk.Redirect({
-      url: `/member/${user.memberNumber}`,
-    })
+export const me: Query = deps => user =>
+  pipe(
+    user.memberNumber,
+    constructViewModel(deps, user),
+    TE.map(viewModel => render(viewModel)),
+    TE.map(rendered =>
+      HttpResponse.mk.Page({
+        rendered,
+      })
+    )
   );
