6ed7f1106d9a7c49e0606885c4b7f8ae297d7030
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-26 01:02:26 +0100

    Lint fix

--- src/training-sheets/google.ts
+++ src/training-sheets/google.ts
@@ -293,7 +293,7 @@ export const extractGoogleSheetData =
                       sheetInfo,
                       equipmentId,
                       trainingSheetId,
-                      timezone,
+                      timezone
                     )
                   ),
                   RA.filterMap(e => e)
