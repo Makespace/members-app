edab4ffc5ba7f0ea6fb075b42f212c7a3d3be702
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-07-26 19:52:09 +0100

    Fix date display to always be Cambridge time

--- src/templates/display-date.ts
+++ src/templates/display-date.ts
@@ -1,10 +1,11 @@
-import {DateTime} from 'luxon';
+import {DateTime, IANAZone} from 'luxon';
 import {HtmlSubstitution, safe} from '../types/html';
 
 // TODO Do this properly. https://github.com/Makespace/members-app/issues/40
-export const displayDate = (
-  date: DateTime | Date | number
-): HtmlSubstitution =>
-  typeof date === 'number'
-    ? safe(DateTime.fromMillis(date).toLocaleString())
-    : safe(date.toLocaleString());
+export const displayDate = (date: DateTime | number): HtmlSubstitution =>
+  safe(
+    (typeof date === 'number' ? DateTime.fromMillis(date) : date)
+      .setLocale('en-GB')
+      .setZone(new IANAZone('Europe/London'))
+      .toLocaleString(DateTime.DATETIME_SHORT)
+  );
