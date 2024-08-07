b771d89f7f836306db75a3b77c72fd33e8d74c8a
Daniel Haarhoff <daniel@rknt.de>
2024-08-07 10:49:08 +0100

    Start writing tests for async refresh #59

--- src/read-models/shared-state/index.ts
+++ src/read-models/shared-state/index.ts
@@ -21,89 +21,92 @@ import {asyncRefresh} from './async-refresh';
 
 export {replayState} from './deprecated-replay';
 
-const updateState = (db: BetterSQLite3Database) => (event: DomainEvent) => {
-  switch (event.type) {
-    case 'MemberNumberLinkedToEmail':
-      db.insert(membersTable)
-        .values({
-          memberNumber: event.memberNumber,
-          emailAddress: event.email,
-          gravatarHash: gravatarHashFromEmail(event.email),
-          name: O.none,
-          pronouns: O.none,
-          prevEmails: [],
-          isSuperUser: false,
-        })
-        .run();
-      break;
-    case 'MemberDetailsUpdated':
-      if (event.name) {
-        db.update(membersTable)
-          .set({name: O.some(event.name)})
-          .where(eq(membersTable.memberNumber, event.memberNumber))
-          .run();
-      }
-      if (event.pronouns) {
-        db.update(membersTable)
-          .set({pronouns: O.some(event.pronouns)})
-          .where(eq(membersTable.memberNumber, event.memberNumber))
-          .run();
-      }
-      break;
-    case 'MemberEmailChanged': {
-      const current = db
-        .select()
-        .from(membersTable)
-        .where(eq(membersTable.memberNumber, event.memberNumber))
-        .get();
-      if (current) {
-        db.update(membersTable)
-          .set({
-            emailAddress: event.newEmail,
-            prevEmails: [...current.prevEmails, current.emailAddress],
-            gravatarHash: gravatarHashFromEmail(event.newEmail),
+export const updateState =
+  (db: BetterSQLite3Database) => (event: DomainEvent) => {
+    switch (event.type) {
+      case 'MemberNumberLinkedToEmail':
+        db.insert(membersTable)
+          .values({
+            memberNumber: event.memberNumber,
+            emailAddress: event.email,
+            gravatarHash: gravatarHashFromEmail(event.email),
+            name: O.none,
+            pronouns: O.none,
+            prevEmails: [],
+            isSuperUser: false,
           })
+          .run();
+        break;
+      case 'MemberDetailsUpdated':
+        if (event.name) {
+          db.update(membersTable)
+            .set({name: O.some(event.name)})
+            .where(eq(membersTable.memberNumber, event.memberNumber))
+            .run();
+        }
+        if (event.pronouns) {
+          db.update(membersTable)
+            .set({pronouns: O.some(event.pronouns)})
+            .where(eq(membersTable.memberNumber, event.memberNumber))
+            .run();
+        }
+        break;
+      case 'MemberEmailChanged': {
+        const current = db
+          .select()
+          .from(membersTable)
+          .where(eq(membersTable.memberNumber, event.memberNumber))
+          .get();
+        if (current) {
+          db.update(membersTable)
+            .set({
+              emailAddress: event.newEmail,
+              prevEmails: [...current.prevEmails, current.emailAddress],
+              gravatarHash: gravatarHashFromEmail(event.newEmail),
+            })
+            .where(eq(membersTable.memberNumber, event.memberNumber))
+            .run();
+        }
+        break;
+      }
+      case 'SuperUserDeclared':
+        db.update(membersTable)
+          .set({isSuperUser: true})
           .where(eq(membersTable.memberNumber, event.memberNumber))
           .run();
-      }
-      break;
-    }
-    case 'SuperUserDeclared':
-      db.update(membersTable)
-        .set({isSuperUser: true})
-        .where(eq(membersTable.memberNumber, event.memberNumber))
-        .run();
-      break;
-    case 'SuperUserRevoked':
-      db.update(membersTable)
-        .set({isSuperUser: false})
-        .where(eq(membersTable.memberNumber, event.memberNumber))
-        .run();
-      break;
-    case 'EquipmentAdded':
-      db.insert(equipmentTable).values({id: event.id, name: event.name}).run();
-      break;
-    case 'TrainerAdded':
-      db.insert(trainersTable)
-        .values({
-          memberNumber: event.memberNumber,
-          equipmentId: event.equipmentId,
-        })
-        .run();
-      break;
-    case 'MemberTrainedOnEquipment':
-      db.insert(trainedMemberstable)
-        .values({
-          memberNumber: event.memberNumber,
-          equipmentId: event.equipmentId,
-        })
-        .run();
-      break;
+        break;
+      case 'SuperUserRevoked':
+        db.update(membersTable)
+          .set({isSuperUser: false})
+          .where(eq(membersTable.memberNumber, event.memberNumber))
+          .run();
+        break;
+      case 'EquipmentAdded':
+        db.insert(equipmentTable)
+          .values({id: event.id, name: event.name})
+          .run();
+        break;
+      case 'TrainerAdded':
+        db.insert(trainersTable)
+          .values({
+            memberNumber: event.memberNumber,
+            equipmentId: event.equipmentId,
+          })
+          .run();
+        break;
+      case 'MemberTrainedOnEquipment':
+        db.insert(trainedMemberstable)
+          .values({
+            memberNumber: event.memberNumber,
+            equipmentId: event.equipmentId,
+          })
+          .run();
+        break;
 
-    default:
-      break;
-  }
-};
+      default:
+        break;
+    }
+  };
 
 export type SharedReadModel = {
   db: BetterSQLite3Database;
