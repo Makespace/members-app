cfbdde5a98ddeb8f691068d949d7400caa99f97a
Daniel Haarhoff <daniel@rknt.de>
2024-08-08 08:30:59 +0100

    Keep rolling out new query #59

--- src/read-models/members/get.ts
+++ src/read-models/members/get.ts
@@ -2,21 +2,6 @@ import {pipe} from 'fp-ts/lib/function';
 import * as O from 'fp-ts/Option';
 import {DomainEvent, Actor, User} from '../../types';
 import {getAllDetailsAsActor} from './get-all';
-import {replayState} from '../shared-state';
-import * as RM from 'fp-ts/ReadonlyMap';
-import {Eq as NumberEq} from 'fp-ts/number';
-import {Member} from './return-types';
-
-export const getDetails =
-  (memberNumber: number) =>
-  (events: ReadonlyArray<DomainEvent>): O.Option<Member> => {
-    return pipe(
-      events,
-      replayState,
-      state => state.members,
-      RM.lookup(NumberEq)(memberNumber)
-    );
-  };
 
 export const getDetailsAsActor =
   (actorOrUser: Actor | User) =>
