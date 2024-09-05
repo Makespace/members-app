29f1570048a114bb7812ba4c185312f27687a88e
Daniel Haarhoff <daniel@rknt.de>
2024-09-05 19:39:26 +0100

    Start experimenting with using in-memory relational readmodel directly in a page #68

--- src/queries/areas/index.ts
+++ src/queries/areas/index.ts
@@ -1,14 +1,33 @@
 import {pipe} from 'fp-ts/lib/function';
 import * as TE from 'fp-ts/TaskEither';
 import {render} from './render';
-import {constructViewModel} from './construct-view-model';
 import {Query} from '../query';
 import {HttpResponse} from '../../types';
+import {SharedReadModel} from '../../read-models/shared-state';
+import {ViewModel} from './view-model';
+import {areasTable, ownersTable} from '../../read-models/shared-state/state';
+import * as RA from 'fp-ts/ReadonlyArray';
+import {eq} from 'drizzle-orm';
+
+const getAreas = (db: SharedReadModel['db']): ViewModel['areas'] => {
+  return pipe(
+    db.select().from(areasTable).all(),
+    RA.map(area => ({
+      ...area,
+      owners: db
+        .select()
+        .from(ownersTable)
+        .where(eq(ownersTable.areaId, area.id))
+        .all(),
+    }))
+  );
+};
 
 export const areas: Query = deps => user =>
   pipe(
-    user,
-    constructViewModel(deps),
+    TE.Do,
+    TE.let('user', () => user),
+    TE.let('areas', () => getAreas(deps.sharedReadModel.db)),
     TE.map(render),
     TE.map(rendered => HttpResponse.mk.Page({rendered}))
   );
