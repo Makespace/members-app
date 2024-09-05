92dd0c8e15a89f8147ca4dffa948c231aee95592
Daniel Haarhoff <daniel@rknt.de>
2024-09-05 15:54:10 +0100

    Allow users to only read about how to get trained when they need to #59

--- src/queries/me/render.ts
+++ src/queries/me/render.ts
@@ -15,33 +15,37 @@ import {superUserNav} from '../landing/render';
 import {displayDate} from '../../templates/display-date';
 import {DateTime} from 'luxon';
 
-const howToGetTrained = html`<h3>To get trained</h3>
-  <ol>
-    <li>
-      Do
-      <a href="https://equipment.makespace.org/">the online training</a>
-    </li>
-    <li>
-      Sign up for practical training session on
-      <a href="https://www.meetup.com/makespace">Meetup</a>
-    </li>
-  </ol>
-  <p>Remember:</p>
-  <ul>
-    <li>All trainers are members volunteering their time.</li>
-    <li>
-      Trainings are often only scheduled on demand after people have passed the
-      online quiz.
-    </li>
-    <li>
-      If you are struggling to join a practical training, reach out to the
-      relevant owners via email.
-    </li>
-    <li>
-      We always need more trainers. If you'd like to help others please let the
-      owners know.
-    </li>
-  </ul> `;
+const howToGetTrained = html`<details>
+  <summary>How to get trained</summary>
+  <div>
+    <ol>
+      <li>
+        Do
+        <a href="https://equipment.makespace.org/">the online training</a>
+      </li>
+      <li>
+        Sign up for practical training session on
+        <a href="https://www.meetup.com/makespace">Meetup</a>
+      </li>
+    </ol>
+    <p>Remember:</p>
+    <ul>
+      <li>All trainers are members volunteering their time.</li>
+      <li>
+        Trainings are often only scheduled on demand after people have passed
+        the online quiz.
+      </li>
+      <li>
+        If you are struggling to join a practical training, reach out to the
+        relevant owners via email.
+      </li>
+      <li>
+        We always need more trainers. If you'd like to help others please let
+        the owners know.
+      </li>
+    </ul>
+  </div>
+</details> `;
 
 const editName = (viewModel: ViewModel) =>
   html`<a href="/members/edit-name?member=${viewModel.member.memberNumber}"
