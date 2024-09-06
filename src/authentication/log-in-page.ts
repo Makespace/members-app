a2d6d7eedc9ff8b7e0394185e4d5e200f9e4d159
Daniel Haarhoff <daniel@rknt.de>
2024-09-06 09:29:45 +0100

    Help users remember which email to use to log in with

--- src/authentication/log-in-page.ts
+++ src/authentication/log-in-page.ts
@@ -9,6 +9,10 @@ export const logInPage = pipe(
       <label for="email">E-Mail: </label>
       <input id="email" type="email" required name="email" value="" />
       <p>We will email you a magic log in link.</p>
+      <p>
+        A unique email will have been associated with your member records. This
+        is probably the email you provided during induction.
+      </p>
       <button type="submit">Email me a link</button>
     </form>
   `,
