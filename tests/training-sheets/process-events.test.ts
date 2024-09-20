bc12da7ac3203ea3dd3173d2742425fdee76e1c7
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-09-20 21:37:02 +0100

    Timestamp training filtering tests

--- tests/training-sheets/process-events.test.ts
+++ tests/training-sheets/process-events.test.ts
@@ -3,7 +3,6 @@ import {TestFramework, initTestFramework} from '../read-models/test-framework';
 import {NonEmptyString, UUID} from 'io-ts-types';
 import {EventOfType} from '../../src/types/domain-event';
 import pino from 'pino';
-import * as T from 'io-ts';
 import * as RA from 'fp-ts/lib/ReadonlyArray';
 import * as N from 'fp-ts/number';
 import * as O from 'fp-ts/Option';
@@ -89,54 +88,25 @@ describe('Training sheets worker', () => {
           ...gsheetData.METAL_LATHE.entries[0],
         });
       });
-      it('Only take new rows', async () => {
-        await framework.commands.equipment.trainingSheet({
-          equipmentId: addEquipment.id,
-          trainingSheetId: gsheetData.METAL_LATHE.data.spreadsheetId!,
-        });
-        await framework.commands.equipment.trainingSheetQuizResult({
-          id: faker.string.uuid() as UUID,
-          equipmentId: addEquipment.id,
-          trainingSheetId: gsheetData.METAL_LATHE.data.spreadsheetId!,
-          ...gsheetData.METAL_LATHE.entries[0],
-        });
-        deps = dependenciesForTrainingSheetsWorker(framework);
-        await updateTrainingQuizResults(
-          localPullGoogleSheetData,
-          deps,
-          deps.logger,
-          0 as T.Int
-        );
-        expect(deps.commitedEvents).toHaveLength(0);
-      });
       it('training sheet with a summary page', async () => {
-        await framework.commands.equipment.trainingSheet({
-          equipmentId: addEquipment.id,
-          trainingSheetId: gsheetData.LASER_CUTTER.data.spreadsheetId!,
-        });
-
-        deps = dependenciesForTrainingSheetsWorker(framework);
-        await updateTrainingQuizResults(
-          localPullGoogleSheetData,
-          deps,
-          deps.logger,
-          0 as T.Int
+        const results = await extractEvents(
+          O.some(gsheetData.LASER_CUTTER.data.spreadsheetId!)
         );
         const expected: readonly Partial<
           EventOfType<'EquipmentTrainingQuizResult'>
         >[] = gsheetData.LASER_CUTTER.entries.map(e => ({
           type: 'EquipmentTrainingQuizResult',
-          equipmentId: addEquipment.id,
+          equipmentId: defaultEquipment().id as UUID,
           trainingSheetId: gsheetData.LASER_CUTTER.data.spreadsheetId!,
           actor: {
             tag: 'system',
           },
           ...e,
         }));
-        expect(deps.commitedEvents).toHaveLength(expected.length);
+        expect(results).toHaveLength(expected.length);
 
         for (const [actualEvent, expectedEvent] of RA.zip(
-          sortQuizResults(deps.commitedEvents),
+          sortQuizResults(results),
           sortQuizResults(expected)
         )) {
           expect(actualEvent).toMatchObject<
@@ -145,34 +115,24 @@ describe('Training sheets worker', () => {
         }
       });
       it('training sheet with multiple response pages (different quiz questions)', async () => {
-        await framework.commands.equipment.trainingSheet({
-          equipmentId: addEquipment.id,
-          trainingSheetId: gsheetData.BAMBU.data.spreadsheetId!,
-        });
-
-        deps = dependenciesForTrainingSheetsWorker(framework);
-        await updateTrainingQuizResults(
-          localPullGoogleSheetData,
-          deps,
-          deps.logger,
-          0 as T.Int
+        const results = await extractEvents(
+          O.some(gsheetData.BAMBU.data.spreadsheetId!)
         );
-
         const expected: readonly Partial<
           EventOfType<'EquipmentTrainingQuizResult'>
         >[] = gsheetData.BAMBU.entries.map(e => ({
           type: 'EquipmentTrainingQuizResult',
-          equipmentId: addEquipment.id,
+          equipmentId: defaultEquipment().id as UUID,
           trainingSheetId: gsheetData.BAMBU.data.spreadsheetId!,
           actor: {
             tag: 'system',
           },
           ...e,
         }));
-        expect(deps.commitedEvents).toHaveLength(expected.length);
+        expect(results).toHaveLength(expected.length);
 
         for (const [actualEvent, expectedEvent] of RA.zip(
-          sortQuizResults(deps.commitedEvents),
+          sortQuizResults(results),
           sortQuizResults(expected)
         )) {
           expect(actualEvent).toMatchObject<
@@ -180,36 +140,124 @@ describe('Training sheets worker', () => {
           >(expectedEvent);
         }
       });
-      describe('Integration asyncApplyExternalEventSources', () => {
-        let framework: TestFramework;
-        const createArea = {
-          id: faker.string.uuid() as UUID,
-          name: faker.company.buzzNoun() as NonEmptyString,
-        };
-        const addEquipment = {
-          id: faker.string.uuid() as UUID,
-          name: faker.company.buzzNoun() as NonEmptyString,
-          areaId: createArea.id,
-        };
-        beforeEach(async () => {
-          framework = await initTestFramework();
-          await framework.commands.area.create(createArea);
-          await framework.commands.equipment.add(addEquipment);
-          await framework.commands.equipment.trainingSheet({
-            equipmentId: addEquipment.id,
-            trainingSheetId: gsheetData.EMPTY.data.spreadsheetId!,
-          });
-        });
-
-        it('Check initial test state', () => {
-          const equipment = framework.sharedReadModel.equipment.getAll();
-          expect(equipment).toHaveLength(1);
-        });
-
-        it('Handle multiple equipment', () => {});
-        it('Handle no equipment', () => {});
-        it('Rate limit equipment pull', () => {});
+      it('Only take new rows, date in future', async () => {
+        const results = await extractEvents(
+          O.some(gsheetData.BAMBU.data.spreadsheetId!),
+          O.some(DateTime.now())
+        );
+        expect(results).toHaveLength(0);
       });
+      it('Only take new rows, date in far past', async () => {
+        const results = await extractEvents(
+          O.some(gsheetData.BAMBU.data.spreadsheetId!),
+          O.some(DateTime.fromSeconds(0))
+        );
+        expect(results).toHaveLength(gsheetData.BAMBU.entries.length);
+      });
+
+      // The quiz results have dates:
+      // 1700768963 Thursday, November 23, 2023 7:49:23 PM
+      // 1700769348 Thursday, November 23, 2023 7:55:48 PM
+      // 1710249052 Tuesday, March 12, 2024 1:10:52 PM
+      // 1710249842 Tuesday, March 12, 2024 1:24:02 PM
+
+      it('Only take new rows, exclude 1', async () => {
+        const results = await extractEvents(
+          O.some(gsheetData.BAMBU.data.spreadsheetId!),
+          O.some(
+            DateTime.fromObject({
+              year: 2023,
+              month: 11,
+              day: 23,
+              hour: 19,
+              minute: 49,
+              second: 23,
+            })
+          )
+        );
+        expect(results).toHaveLength(3);
+      });
+
+      it('Only take new rows, exclude 2', async () => {
+        const results = await extractEvents(
+          O.some(gsheetData.BAMBU.data.spreadsheetId!),
+          O.some(
+            DateTime.fromObject({
+              year: 2023,
+              month: 11,
+              day: 23,
+              hour: 19,
+              minute: 55,
+              second: 48,
+            })
+          )
+        );
+        expect(results).toHaveLength(2);
+      });
+
+      it('Only take new rows, exclude 3', async () => {
+        const results = await extractEvents(
+          O.some(gsheetData.BAMBU.data.spreadsheetId!),
+          O.some(
+            DateTime.fromObject({
+              year: 2024,
+              month: 3,
+              day: 12,
+              hour: 13,
+              minute: 10,
+              second: 52,
+            })
+          )
+        );
+        expect(results).toHaveLength(1);
+      });
+
+      it('Only take new rows, exclude all (already have latest)', async () => {
+        const results = await extractEvents(
+          O.some(gsheetData.BAMBU.data.spreadsheetId!),
+          O.some(
+            DateTime.fromObject({
+              year: 2024,
+              month: 3,
+              day: 12,
+              hour: 13,
+              minute: 24,
+              second: 2,
+            })
+          )
+        );
+        expect(results).toHaveLength(0);
+      });
+    });
+    describe('Integration asyncApplyExternalEventSources', () => {
+      let framework: TestFramework;
+      const createArea = {
+        id: faker.string.uuid() as UUID,
+        name: faker.company.buzzNoun() as NonEmptyString,
+      };
+      const addEquipment = {
+        id: faker.string.uuid() as UUID,
+        name: faker.company.buzzNoun() as NonEmptyString,
+        areaId: createArea.id,
+      };
+      beforeEach(async () => {
+        framework = await initTestFramework();
+        await framework.commands.area.create(createArea);
+        await framework.commands.equipment.add(addEquipment);
+        await framework.commands.equipment.trainingSheet({
+          equipmentId: addEquipment.id,
+          trainingSheetId: gsheetData.EMPTY.data.spreadsheetId!,
+        });
+      });
+
+      it('Check initial test state', () => {
+        const equipment = framework.sharedReadModel.equipment.getAll();
+        expect(equipment).toHaveLength(1);
+      });
+
+      it('Handle multiple equipment', () => {});
+      it('Handle no equipment', () => {});
+      it('Rate limit equipment pull', () => {});
     });
   });
 });
