eb5d906f2e56eaa7115369a595b32afdb0fcf88f
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-09-21 00:06:56 +0100

    Fix construct view model to just error if equipment unknown

--- src/queries/equipment/construct-view-model.ts
+++ src/queries/equipment/construct-view-model.ts
@@ -2,34 +2,14 @@ import {pipe} from 'fp-ts/lib/function';
 import {Dependencies} from '../../dependencies';
 import * as TE from 'fp-ts/TaskEither';
 import * as O from 'fp-ts/Option';
-import {readModels} from '../../read-models';
 import {
+  failureWithStatus,
   FailureWithStatus,
 } from '../../types/failure-with-status';
-import {
-  ViewModel,
-} from './view-model';
+import {ViewModel} from './view-model';
 import {User} from '../../types';
-import {DomainEvent} from '../../types/domain-event';
-import {Equipment} from '../../read-models/equipment/get';
 import {UUID} from 'io-ts-types';
-import {Member} from '../../read-models/members';
-
-const isSuperUserOrOwnerOfArea = (
-  events: ReadonlyArray<DomainEvent>,
-  areaId: UUID,
-  memberNumber: number
-): boolean =>
-  readModels.superUsers.is(memberNumber)(events) ||
-  readModels.areas.isOwner(events)(areaId, memberNumber);
-
-const isSuperUserOrTrainerOfEquipment = (
-  events: ReadonlyArray<DomainEvent>,
-  equipment: Equipment,
-  memberNumber: number
-): boolean =>
-  readModels.superUsers.is(memberNumber)(events) ||
-  equipment.trainers.includes(memberNumber);
+import {StatusCodes} from 'http-status-codes';
 
 export const constructViewModel =
   (deps: Dependencies, user: User) =>
@@ -37,14 +17,41 @@ export const constructViewModel =
     pipe(
       {user},
       TE.right,
-      TE.bind('equipment', () => deps.sharedReadModel.equipment.get(equipmentId)),
-      TE.let('isSuperUserOrOwnerOfArea', ({events, equipment}) => {
-
-      },
-        O.some(deps.sharedReadModel.members.get(user.memberNumber)) ? 
-        isSuperUserOrOwnerOfArea(events, equipment.areaId, user.memberNumber)
+      TE.bind('equipment', () => {
+        const equipment = deps.sharedReadModel.equipment.get(equipmentId);
+        if (O.isNone(equipment)) {
+          return TE.left(
+            failureWithStatus('Unknown equipment', StatusCodes.NOT_FOUND)()
+          );
+        }
+        return TE.right(equipment.value);
+      }),
+      TE.let('member', () =>
+        deps.sharedReadModel.members.get(user.memberNumber)
       ),
-      TE.let('isSuperUserOrTrainerOfArea', ({events, equipment}) =>
-        isSuperUserOrTrainerOfEquipment(events, equipment, user.memberNumber)
+      TE.let('isSuperUser', ({member}) =>
+        O.isNone(member) ? false : member.value.isSuperUser
+      ),
+      TE.let('isOwner', ({equipment, member}) =>
+        O.isNone(member)
+          ? false
+          : member.value.ownerOf.findIndex(
+              ownerOf => ownerOf.id === equipment.area.id
+            ) !== -1
+      ),
+      TE.let(
+        'isTrainer',
+        ({equipment}) =>
+          equipment.trainers.findIndex(
+            trainer => trainer.memberNumber === user.memberNumber
+          ) !== -1
+      ),
+      TE.let(
+        'isSuperUserOrOwnerOfArea',
+        ({isSuperUser, isOwner}) => isSuperUser || isOwner
+      ),
+      TE.let(
+        'isSuperUserOrTrainerOfArea',
+        ({isSuperUser, isTrainer}) => isSuperUser || isTrainer
       )
     );
