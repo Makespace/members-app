99c1f42154b4df6fd70139cbfe0e2aefa5c58ef9
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-09-22 11:03:14 +0100

    Type fixing from change to pass readModel rather than readModelDB

--- src/read-models/index.ts
+++ src/read-models/index.ts
@@ -1,9 +1,11 @@
 import {areas} from './areas';
 import {members} from './members';
+import {equipment} from './equipment';
 import {superUsers} from './super-users';
 
 export const readModels = {
   areas,
   members,
   superUsers,
+  equipment,
 };
