6b30e4d46d818f3e4abaa46d7370d47d3fc79bf0
Paul Lancaster <paul.la.lancaster@gmail.com>
2024-09-25 21:27:41 +0100

    Update extract from row to new method

--- src/training-sheets/google.ts
+++ src/training-sheets/google.ts
@@ -1,15 +1,20 @@
 import {pipe} from 'fp-ts/lib/function';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as O from 'fp-ts/Option';
+import * as E from 'fp-ts/Either';
 
 import {Logger} from 'pino';
 import {constructEvent, EventOfType} from '../types/domain-event';
 import {v4} from 'uuid';
 import {UUID} from 'io-ts-types';
 import {DateTime} from 'luxon';
-import {sheets_v4} from '@googleapis/sheets';
 import {EpochTimestampMilliseconds} from '../read-models/shared-state/return-types';
-import {GoogleSheetMetadataInital} from './extract-metadata';
+import {
+  GoogleSheetMetadata,
+  GoogleSheetMetadataInital,
+  SpreadsheetData,
+} from './extract-metadata';
+import {GoogleSpreadsheetDataForSheet} from '../init-dependencies/google/pull_sheet_data';
 
 // Bounds to prevent clearly broken parsing.
 const MIN_RECOGNISED_MEMBER_NUMBER = 0;
@@ -125,56 +130,48 @@ const extractTimestamp = (
 const extractFromRow =
   (
     logger: Logger,
-    sheetInfo: SheetInfo,
+    metadata: GoogleSheetMetadata,
     equipmentId: UUID,
     trainingSheetId: string,
     timezone: string
   ) =>
-  (
-    row: sheets_v4.Schema$RowData
-  ): O.Option<EventOfType<'EquipmentTrainingQuizResult'>> => {
-    if (!row.values) {
-      return O.none;
-    }
-
-    const email = O.isSome(sheetInfo.columnIndexes.email)
+  (row: {
+    values: {formattedValue: string}[];
+  }): O.Option<EventOfType<'EquipmentTrainingQuizResult'>> => {
+    const email = O.isSome(metadata.mappedColumns.email)
       ? extractEmail(
-          row.values[sheetInfo.columnIndexes.email.value].formattedValue
+          row.values[metadata.mappedColumns.email.value].formattedValue
         )
       : O.none;
-    const memberNumber = O.isSome(sheetInfo.columnIndexes.memberNumber)
+    const memberNumber = O.isSome(metadata.mappedColumns.memberNumber)
       ? extractMemberNumber(
-          row.values[sheetInfo.columnIndexes.memberNumber.value].formattedValue
+          row.values[metadata.mappedColumns.memberNumber.value].formattedValue
         )
       : O.none;
-    const score = O.isSome(sheetInfo.columnIndexes.score)
-      ? extractScore(
-          row.values[sheetInfo.columnIndexes.score.value].formattedValue
-        )
-      : O.none;
-    const timestampEpochMS = O.isSome(sheetInfo.columnIndexes.timestamp)
-      ? extractTimestamp(
-          timezone,
-          row.values[sheetInfo.columnIndexes.timestamp.value].formattedValue
-        )
-      : O.none;
+    const score = extractScore(
+      row.values[metadata.mappedColumns.score].formattedValue
+    );
+    const timestampEpochMS = extractTimestamp(
+      timezone,
+      row.values[metadata.mappedColumns.timestamp].formattedValue
+    );
 
     if (O.isNone(email) && O.isNone(memberNumber)) {
       // Note that some quizes only require the member number.
       logger.warn(
         'Failed to extract email or member number from row, skipping quiz result'
       );
-      logger.trace('Skipped quiz row: %O', row.values);
+      logger.trace('Skipped quiz row: %O', row);
       return O.none;
     }
     if (O.isNone(score)) {
       logger.warn('Failed to extract score from row, skipped row');
-      logger.trace('Skipped quiz row: %o', row.values);
+      logger.trace('Skipped quiz row: %o', row);
       return O.none;
     }
     if (O.isNone(timestampEpochMS)) {
       logger.warn('Failed to extract timestamp from row, skipped row');
-      logger.trace('Skipped quiz row: %o', row.values);
+      logger.trace('Skipped quiz row: %o', row);
       return O.none;
     }
     return O.some(
@@ -197,72 +194,31 @@ export const extractGoogleSheetData =
     logger: Logger,
     trainingSheetId: string,
     equipmentId: UUID,
+    metadata: GoogleSheetMetadata,
+    timezone: string,
     eventsFromExclusive: O.Option<EpochTimestampMilliseconds>
   ) =>
   (
-    spreadsheet: sheets_v4.Schema$Spreadsheet
-  ): ReadonlyArray<EventOfType<'EquipmentTrainingQuizResult'>> =>
-    !spreadsheet.sheets || spreadsheet.sheets.length < 1
-      ? []
-      : spreadsheet.sheets.map(sheet => {
-          const title = sheet.properties?.title;
-          if (!title) {
-            logger.warn('Skipping sheet due to missing title');
-            return [];
-          }
-
-          if (
-            !sheet.data ||
-            sheet.data.length < 1 ||
-            !sheet.data[0].rowData ||
-            sheet.data[0].rowData.length < 1
-          ) {
-            logger.warn(`Skipping sheet '${title}' as missing data`);
-            return [];
-          }
-
-          let timezone = spreadsheet.properties?.timeZone;
-          if (!timezone || !DateTime.local().setZone(timezone).isValid) {
-            // Not all the google form sheets are actually in Europe/London.
-            // Issue first noticed because CI is in a different zone (UTC) than local test machine (BST).
-            logger.info(
-              `Unable to determine timezone for google sheet '${spreadsheet.properties?.title}', '${timezone}' - defaulting to Europe/London`
-            );
-            timezone = 'Europe/London';
-          }
-
-          const [headers, ...data] = sheet.data[0].rowData;
-
-          return pipe(
-            headers,
-            extractQuizSheetInformation(logger),
-            O.match(
-              () => {
-                logger.warn('Failed to extract sheet info');
-                return [];
-              },
-              sheetInfo =>
-                pipe(
-                  data,
-                  RA.map(
-                    extractFromRow(
-                      logger,
-                      sheetInfo,
-                      equipmentId,
-                      trainingSheetId,
-                      timezone
-                    )
-                  ),
-                  RA.filterMap(e => e),
-                  RA.filter(
-                    e =>
-                      O.isNone(eventsFromExclusive) ||
-                      e.timestampEpochMS > eventsFromExclusive.value
-                  )
-                )
-            )
-          );
-        });
+    spreadsheet: GoogleSpreadsheetDataForSheet
+  ): ReadonlyArray<EventOfType<'EquipmentTrainingQuizResult'>> => {
+    const data = SpreadsheetData.decode(spreadsheet);
+    if (E.isLeft(data)) {
+      logger.warn('Skipping sheet %s due to missing data', trainingSheetId);
+      return [];
+    }
+    return pipe(
+      data.right.sheets[0].data[0].rowData,
+      RA.map(
+        extractFromRow(logger, metadata, equipmentId, trainingSheetId, timezone)
+      ),
+      RA.filterMap(e => e),
+      RA.filter(
+        e =>
+          O.isNone(eventsFromExclusive) ||
+          e.timestampEpochMS > eventsFromExclusive.value
+      )
+    );
+  };
 
 export const shouldPullFromSheet = (
   sheet: GoogleSheetMetadataInital
