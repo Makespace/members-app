200269f6dc5383169685a4ec4dbf9c80bc8e5f2b
Daniel Haarhoff <daniel@rknt.de>
2024-11-16 10:33:54 +0000

    Pick an email address #76

--- src/queries/raise-issue/render.ts
+++ src/queries/raise-issue/render.ts
@@ -1,20 +1,28 @@
-import {html} from '../../types/html';
+import {html, safe} from '../../types/html';
 
 type ViewModel = {
   memberNumber: number;
 };
+
+const infoSystemsGroupEmailAddress = safe('infosystems@makespace.org');
 
 export const render = (viewModel: ViewModel) => html`
   <div class="stack">
     <h1>Raise an issue</h1>
     <h2>If your records are wrong</h2>
     <p>
-      Send the app team an email:
-      <a href="mailto:foo@example.com">foo@example.com</a>
+      Send the app and records team an email:
+      <a href="mailto:${infoSystemsGroupEmailAddress}"
+        >${infoSystemsGroupEmailAddress}</a
+      >
     </p>
     <p>
       Please include your member number (${viewModel.memberNumber}) in the
       email.
+    </p>
+    <p>
+      This will reach the members buildings this app as well as part of the
+      management team that maintains our records.
     </p>
   </div>
 `;
