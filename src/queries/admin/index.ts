b71e94c92022d1271f05426b70fba0dc08f3a590
Daniel Haarhoff <daniel@rknt.de>
2024-11-16 12:04:08 +0000

    Create space for a dedicated admin page #86

---
+++ src/queries/admin/index.ts
@@ -0,0 +1,31 @@
+import {pipe} from 'fp-ts/lib/function';
+import {Query} from '../query';
+import {toLoggedInContent, safe} from '../../types/html';
+import {Dependencies} from '../../dependencies';
+import * as O from 'fp-ts/Option';
+import {User} from '../../types';
+import {failureWithStatus} from '../../types/failure-with-status';
+import {StatusCodes} from 'http-status-codes';
+import * as TE from 'fp-ts/TaskEither';
+import {render} from './render';
+
+const isSuperUser =
+  (sharedReadModel: Dependencies['sharedReadModel']) => (user: User) =>
+    pipe(
+      user.memberNumber,
+      sharedReadModel.members.get,
+      O.filter(member => member.isSuperUser),
+      O.getOrElseW(() => false)
+    );
+
+export const admin: Query = deps => user => {
+  if (!isSuperUser(deps.sharedReadModel)(user)) {
+    return TE.left(
+      failureWithStatus(
+        'You are not authorised to see this page',
+        StatusCodes.UNAUTHORIZED
+      )()
+    );
+  }
+  return pipe(render(), toLoggedInContent(safe('Admin')), TE.right);
+};
