78c6eaa6edc88c58f68bb6526e98d025733f6527
Daniel Haarhoff <daniel@rknt.de>
2024-11-25 08:38:28 +0000

    Further simplify form-get after removal of getAllEvents #86

--- src/http/form-get.ts
+++ src/http/form-get.ts
@@ -1,25 +1,13 @@
 import {Request, Response} from 'express';
+import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
-import * as TE from 'fp-ts/TaskEither';
 import {pipe} from 'fp-ts/lib/function';
-import {StatusCodes} from 'http-status-codes';
 import {getUserFromSession} from '../authentication';
 import {Dependencies} from '../dependencies';
 import {oopsPage, pageTemplate} from '../templates';
-import {sequenceS} from 'fp-ts/lib/Apply';
 import {Form} from '../types/form';
-import {failureWithStatus} from '../types/failure-with-status';
 import {CompleteHtmlDocument, sanitizeString} from '../types/html';
 import {logInPath} from '../authentication/auth-routes';
-
-const getUser = (req: Request, deps: Dependencies) =>
-  pipe(
-    req.session,
-    getUserFromSession(deps),
-    TE.fromOption(() =>
-      failureWithStatus('You are not logged in.', StatusCodes.UNAUTHORIZED)()
-    )
-  );
 
 // See formPost for a more indepth discussion about the design decisions around why this is how it is.
 // formGet is like formPost but rather than processing a command formGet handles calling a read model to
@@ -27,25 +15,24 @@ const getUser = (req: Request, deps: Dependencies) =>
 // is where conflict resolution etc. is handled as described in form-post.
 export const formGet =
   <T>(deps: Dependencies, form: Form<T>) =>
-  async (req: Request, res: Response<CompleteHtmlDocument>) => {
+  (req: Request, res: Response<CompleteHtmlDocument>) => {
     const user = getUserFromSession(deps)(req.session);
-    const isSuperUser = false;
     if (O.isNone(user)) {
       res.redirect(logInPath);
       return;
     }
-    await pipe(
+    const isSuperUser = false;
+    pipe(
       {
-        user: getUser(req, deps),
+        user: user.value,
+        readModel: deps.sharedReadModel,
       },
-      sequenceS(TE.ApplyPar),
-      TE.let('readModel', () => deps.sharedReadModel),
-      TE.chainEitherK(form.constructForm({...req.query, ...req.params})),
-      TE.map(form.renderForm),
-      TE.map(({title, body}) =>
+      form.constructForm({...req.query, ...req.params}),
+      E.map(form.renderForm),
+      E.map(({title, body}) =>
         pageTemplate(title, user.value, isSuperUser)(body)
       ),
-      TE.matchW(
+      E.matchW(
         failure => {
           deps.logger.error(failure, 'Failed to show form to a user');
           res
@@ -54,5 +41,5 @@ export const formGet =
         },
         page => res.status(200).send(page)
       )
-    )();
+    );
   };
