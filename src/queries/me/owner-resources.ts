947b2deba89c15b432442b259e95978a6c260aec
Daniel Haarhoff <daniel@rknt.de>
2025-01-10 20:36:52 +0000

    Copy still relevant resources from trainers page before removing said page in favour of this app #80

---
+++ src/queries/me/owner-resources.ts
@@ -0,0 +1,57 @@
+import {html} from '../../types/html';
+
+export const ownerResources = html`
+  <details>
+    <summary>Resources for owners</summary>
+    <ul>
+      <li>
+        <a href="https://sites.google.com/makespace.org/equipment/home"
+          >Equipment pages</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://sites.google.com/makespace.org/equipment/home?authuser=1"
+          >magic link</a
+        >
+        to edit the Equipment pages
+      </li>
+      <li>
+        <a
+          href="https://drive.google.com/drive/folders/1tPSEo7ZfaASiwHcbcmnPD4nDN14JYZe6"
+        ></a
+        >Google Drive folder
+      </li>
+      to store all equipment related files including training slides and quizzes
+      <li>
+        <a
+          href="https://drive.google.com/drive/folders/163eSpehv_L8E1I59VPVzyKIkN8TJthVA?usp=sharing"
+          >Google Drive folder</a
+        >for sharing material like videos on equipment pages
+      </li>
+      <li>
+        Each piece of Red equipment should have a safety poster next to it.
+        <ul>
+          <li>
+            <a
+              href="https://drive.google.com/drive/folders/1Jo-Lt-Vh_GAThN29nxA_tHzqOwFneaTt?usp=sharing"
+              >existing posters</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://drive.google.com/drive/folders/1PyhOvqQ6XLWUzF3K9vojGRw-qPfeN5Nx?usp=sharing"
+              >icons for safety posters</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://docs.google.com/drawings/d/1i5wEO2BOPcKvKaF3nc5zD_O23kEomSYosWLIKaUvQSI/edit?usp=sharing"
+              >poster template</a
+            >
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </details>
+`;
