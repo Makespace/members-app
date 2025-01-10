39d708a0f2d5acc096c98e68977e7a8aa0321b48
Daniel Haarhoff <daniel@rknt.de>
2025-01-10 20:14:40 +0000

    Describe how to become a trainer

--- src/queries/shared-render/trainer-status.ts
+++ src/queries/shared-render/trainer-status.ts
@@ -5,6 +5,17 @@ import {displayDate} from '../../templates/display-date';
 import {DateTime} from 'luxon';
 import {TrainerFor} from '../../read-models/shared-state/return-types';
 
+export const howToBecomeATrainer = html`
+  <details>
+    <summary>How to become a trainer</summary>
+    <p>
+      Help out by becoming an equipment owner and volunteering to train other
+      members. It is the responsibility of each group of owners to determine who
+      can train members. All trainers must be active owners.
+    </p>
+  </details>
+`;
+
 export const renderTrainerStatus = (trainerFor: ReadonlyArray<TrainerFor>) => {
   if (trainerFor.length === 0) {
     return '';
