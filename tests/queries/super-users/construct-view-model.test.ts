efe0122f868223e6c0a748ce8f78b1d5cb6817fe
Daniel Haarhoff <daniel@rknt.de>
2025-01-25 12:59:14 +0000

    Prefer shared state readmodel over traversing all events in a viewmodel construction #94

--- tests/queries/super-users/construct-view-model.test.ts
+++ tests/queries/super-users/construct-view-model.test.ts
@@ -2,41 +2,49 @@ import * as E from 'fp-ts/Either';
 import {pipe} from 'fp-ts/lib/function';
 import {arbitraryUser} from '../../types/user.helper';
 import {constructViewModel} from '../../../src/queries/super-users/construct-view-model';
-import {Dependencies} from '../../../src/dependencies';
-import {happyPathAdapters} from '../../init-dependencies/happy-path-adapters.helper';
-import * as TE from 'fp-ts/TaskEither';
-import {constructEvent} from '../../../src/types';
 import * as T from 'fp-ts/Task';
 import * as RA from 'fp-ts/ReadonlyArray';
 import {getRightOrFail} from '../../helpers';
+import {
+  TestFramework,
+  initTestFramework,
+} from '../../read-models/test-framework';
 
-describe.skip('construct-view-model', () => {
-  it('succeeds if the logged in user is not a super user', async () => {
-    const memberToBeSuperUser = arbitraryUser();
-    const deps: Dependencies = {
-      ...happyPathAdapters,
-      getAllEvents: () =>
-        TE.right([
-          constructEvent('SuperUserDeclared')({
-            memberNumber: memberToBeSuperUser.memberNumber,
-          }),
-        ]),
-    };
+describe('construct-view-model', () => {
+  let framework: TestFramework;
+  beforeEach(async () => {
+    framework = await initTestFramework();
+  });
+
+  const loggedInUser = arbitraryUser();
+  beforeEach(async () => {
+    await framework.commands.memberNumbers.linkNumberToEmail({
+      memberNumber: loggedInUser.memberNumber,
+      email: loggedInUser.emailAddress,
+    });
+  });
+
+  it('succeeds if the logged in user is a super user', async () => {
+    await framework.commands.superUser.declare({
+      memberNumber: loggedInUser.memberNumber,
+    });
 
     const result = await pipe(
-      memberToBeSuperUser,
-      constructViewModel(deps),
+      loggedInUser,
+      constructViewModel(framework.sharedReadModel),
       T.map(getRightOrFail),
       T.map(viewModel => viewModel.superUsers),
       T.map(RA.map(superUser => superUser.memberNumber))
     )();
 
-    expect(result).toStrictEqual([memberToBeSuperUser.memberNumber]);
+    expect(result).toStrictEqual([loggedInUser.memberNumber]);
   });
 
   it('fails if the logged in user is not a super user', async () => {
-    const deps: Dependencies = happyPathAdapters;
-    const result = await pipe(arbitraryUser(), constructViewModel(deps))();
+    const result = await pipe(
+      loggedInUser,
+      constructViewModel(framework.sharedReadModel)
+    )();
 
     expect(result).toStrictEqual(E.left(expect.anything()));
   });
