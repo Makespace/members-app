e4e059aac0fc4abb30902125885f81d9b68f8c6c
Daniel Haarhoff <daniel@rknt.de>
2025-01-25 11:52:31 +0000

    Increase test specificity by asserting against exact dates #94

--- tests/read-models/shared-state/get-member.test.ts
+++ tests/read-models/shared-state/get-member.test.ts
@@ -119,10 +119,22 @@ describe('get-via-shared-read-model', () => {
     });
 
     describe('and they have been declared a super user', () => {
+      const firstMadeSuperUserAt = faker.date.anytime();
+      const superUserRevokedAt = faker.date.future({
+        refDate: firstMadeSuperUserAt,
+      });
+      const madeSuperUserAgainAt = faker.date.future({
+        refDate: superUserRevokedAt,
+      });
       beforeEach(async () => {
+        jest.useFakeTimers();
+        jest.setSystemTime(firstMadeSuperUserAt);
         await framework.commands.superUser.declare({
           memberNumber,
         });
+      });
+      afterEach(() => {
+        jest.useRealTimers();
       });
 
       it('they are a superuser', () => {
@@ -135,13 +147,14 @@ describe('get-via-shared-read-model', () => {
         () => {
           const result = runQuery();
           expect(result.superUserSince).toStrictEqual(
-            O.some(expect.anything())
+            O.some(firstMadeSuperUserAt)
           );
         }
       );
 
       describe('and when their superuser status has been revoked', () => {
         beforeEach(async () => {
+          jest.setSystemTime(superUserRevokedAt);
           await framework.commands.superUser.revoke({
             memberNumber,
           });
@@ -159,6 +172,7 @@ describe('get-via-shared-read-model', () => {
 
         describe('and they have been again declared to be a super user', () => {
           beforeEach(async () => {
+            jest.setSystemTime(madeSuperUserAgainAt);
             await framework.commands.superUser.declare({
               memberNumber,
             });
@@ -174,7 +188,7 @@ describe('get-via-shared-read-model', () => {
             () => {
               const result = runQuery();
               expect(result.superUserSince).toStrictEqual(
-                O.some(expect.anything())
+                O.some(madeSuperUserAgainAt)
               );
             }
           );
