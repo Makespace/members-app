aad04d0ffbd2a7e72b7ca06303bd6f0579b39b53
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-02-09 16:14:28 +0000

    More testing for caching events

--- tests/training-sheets/cached-sheet-data.test.ts
+++ tests/training-sheets/cached-sheet-data.test.ts
@@ -16,6 +16,17 @@ describe('Cache sheet data', () => {
     const equipmentId = '326e8fda-7be8-4cd8-87d5-7cdfafebf996' as UUID;
     const sheetId = 'myTestingSheetId';
     const cacheTimestamp = new Date(2024, 1, 23, 4, 23, 45);
+    const trainingQuizResult = {
+      equipmentId: equipmentId,
+      trainingSheetId: sheetId,
+      memberNumberProvided: faker.number.int(),
+      emailProvided: 'beans@bob.co.uk',
+      score: 23,
+      id: '1a3eeb99-dd7a-4f55-a018-6f5e2678fc14' as UUID,
+      maxScore: 23,
+      percentage: 100,
+      timestampEpochMS: 1738623209,
+    };
     let cachedData: ReadonlyArray<{
       cached_at: Date;
       sheet_id: string;
@@ -35,17 +46,7 @@ describe('Cache sheet data', () => {
           constructEvent('EquipmentTrainingQuizSync')({
             equipmentId,
           }),
-          constructEvent('EquipmentTrainingQuizResult')({
-            equipmentId: equipmentId,
-            trainingSheetId: sheetId,
-            memberNumberProvided: faker.number.int(),
-            emailProvided: 'beans@bob.co.uk',
-            score: 23,
-            id: '1a3eeb99-dd7a-4f55-a018-6f5e2678fc14' as UUID,
-            maxScore: 23,
-            percentage: 100,
-            timestampEpochMS: 1738623209,
-          }),
+          constructEvent('EquipmentTrainingQuizResult')(trainingQuizResult),
         ])()
       );
       cachedData = getRightOrFail(await getCachedSheetData(db)()());
@@ -54,7 +55,14 @@ describe('Cache sheet data', () => {
       expect(cachedData).toHaveLength(1); // 1 sheet
     });
     it('All events cached are returned', () => {
-      expect(getRightOrFail(cachedData[0].cached_data)).toHaveLength(2); // 2 events.
+      const returnedCachedData = getRightOrFail(cachedData[0].cached_data);
+      expect(returnedCachedData).toHaveLength(2); // 2 events.
+      returnedCachedData.forEach(e =>
+        expect(e.equipmentId).toStrictEqual(equipmentId)
+      );
+      expect(
+        returnedCachedData.find(e => e.type === 'EquipmentTrainingQuizResult')!
+      ).toMatchObject(trainingQuizResult);
     });
     it('Event cache is correctly labeled', () => {
       expect(cachedData[0].sheet_id).toStrictEqual(sheetId);
