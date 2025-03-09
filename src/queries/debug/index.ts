b9308be293b6023ea460f6799a6bfd18a3cc5e55
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-03-09 12:41:24 +0000

    Document the changable nature of the dump endpoint

--- src/queries/debug/index.ts
+++ src/queries/debug/index.ts
@@ -5,6 +5,7 @@ import {constructViewModel} from './construct-view-model';
 import {Query} from '../query';
 import {HttpResponse} from '../../types';
 
+// https://github.com/Makespace/members-app/issues/108
 export const dumpSharedDbAsJson: Query = deps => user =>
   pipe(
     user,
--- src/read-models/shared-state/debug/dump.ts
+++ src/read-models/shared-state/debug/dump.ts
@@ -1,4 +1,7 @@
 // Dump the entire state of the internal database into a file which can be loaded and inspected locally.
+// https://github.com/Makespace/members-app/issues/108
+// The output of this is subject to change (inc. enable/disable) at any point including during patch version
+// (non-breaking) updates to the program.
 
 import {BetterSQLite3Database} from 'drizzle-orm/better-sqlite3';
 import {
