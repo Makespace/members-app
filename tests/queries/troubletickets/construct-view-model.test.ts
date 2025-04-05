1909c86587cd3f3d90a075f9fbf05327457ca3bc
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-04-05 11:09:19 +0100

    Check only super users can see trouble tickets

---
+++ tests/queries/troubletickets/construct-view-model.test.ts
@@ -0,0 +1,47 @@
+import * as E from 'fp-ts/Either';
+import {pipe} from 'fp-ts/lib/function';
+import {arbitraryUser} from '../../types/user.helper';
+import {constructViewModel} from '../../../src/queries/trouble-tickets/construct-view-model';
+import * as T from 'fp-ts/Task';
+import {getRightOrFail} from '../../helpers';
+import {
+  TestFramework,
+  initTestFramework,
+} from '../../read-models/test-framework';
+
+describe('construct-view-model', () => {
+  let framework: TestFramework;
+  beforeEach(async () => {
+    framework = await initTestFramework();
+  });
+
+  const loggedInUser = arbitraryUser();
+  beforeEach(async () => {
+    await framework.commands.memberNumbers.linkNumberToEmail({
+      memberNumber: loggedInUser.memberNumber,
+      email: loggedInUser.emailAddress,
+    });
+  });
+
+  it('succeeds if the logged in user is a super user', async () => {
+    await framework.commands.superUser.declare({
+      memberNumber: loggedInUser.memberNumber,
+    });
+
+    const result = await pipe(
+      loggedInUser,
+      constructViewModel(framework.sharedReadModel),
+      T.map(getRightOrFail)
+    )();
+    expect(result).toBeDefined();
+  });
+
+  it('fails if the logged in user is not a super user', async () => {
+    const result = await pipe(
+      loggedInUser,
+      constructViewModel(framework.sharedReadModel)
+    )();
+
+    expect(result).toStrictEqual(E.left(expect.anything()));
+  });
+});
