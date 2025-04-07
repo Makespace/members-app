c4eda0f54a3d99ac09be9306e8489cd912c67fb0
Hector Dearman <hector.dearman@gmail.com>
2025-04-07 16:00:05 +0100

    Add case insensitive email lookup

--- tests/read-models/members/lookup-by-email.test.ts
+++ tests/read-models/members/lookup-by-email.test.ts
@@ -1,5 +1,8 @@
 import * as O from 'fp-ts/Option';
-import {lookupByEmail} from '../../../src/read-models/members/lookup-by-email';
+import {
+  lookupByEmail,
+  lookupByCaseInsensitiveEmail,
+} from '../../../src/read-models/members/lookup-by-email';
 import {faker} from '@faker-js/faker';
 import {DomainEvent, EmailAddress} from '../../../src/types';
 import {TestFramework, initTestFramework} from '../test-framework';
@@ -51,6 +54,72 @@ describe('lookupByEmail', () => {
     it('returns none', () => {
       const result = lookupByEmail(faker.internet.email())(events);
       expect(result).toStrictEqual(O.none);
+    });
+  });
+});
+
+describe('lookupByCaseInsensitiveEmail', () => {
+  let events: ReadonlyArray<DomainEvent>;
+  let framework: TestFramework;
+  beforeEach(async () => {
+    framework = await initTestFramework();
+  });
+
+  describe('when no members exist', () => {
+    beforeEach(async () => {
+      events = await framework.getAllEvents();
+    });
+
+    it('returns none', () => {
+      const result = lookupByCaseInsensitiveEmail(faker.internet.email())(
+        events
+      );
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('when a member with the given email exists', () => {
+    const command = {
+      memberNumber: faker.number.int(),
+      email: faker.internet.email() as EmailAddress,
+    };
+    beforeEach(async () => {
+      await framework.commands.memberNumbers.linkNumberToEmail(command);
+      events = await framework.getAllEvents();
+    });
+
+    it('returns their member number', () => {
+      const result = lookupByCaseInsensitiveEmail(command.email)(events);
+      expect(result).toEqual([
+        {emailAddress: command.email, memberNumber: command.memberNumber},
+      ]);
+    });
+
+    it('even given the wrong case', () => {
+      const result = lookupByCaseInsensitiveEmail(command.email.toUpperCase())(
+        events
+      );
+      expect(result).toEqual([
+        {emailAddress: command.email, memberNumber: command.memberNumber},
+      ]);
+    });
+  });
+
+  describe('when no member with the given email exists', () => {
+    const command = {
+      memberNumber: faker.number.int(),
+      email: faker.internet.email() as EmailAddress,
+    };
+    beforeEach(async () => {
+      await framework.commands.memberNumbers.linkNumberToEmail(command);
+      events = await framework.getAllEvents();
+    });
+
+    it('returns none', () => {
+      const result = lookupByCaseInsensitiveEmail(faker.internet.email())(
+        events
+      );
+      expect(result).toEqual([]);
     });
   });
 });
