2157d3584455ede96c6c14aa25c5ee28cdc1c3ed
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-05-05 15:41:56 +0100

    Fix issue with member linking

--- tests/read-models/shared-state/group-membership-numbers.test.ts
+++ tests/read-models/shared-state/group-membership-numbers.test.ts
@@ -1,3 +1,5 @@
+import {sort} from 'fp-ts/lib/ReadonlyArray';
+import * as S from 'fp-ts/string';
 import {MemberLinking} from '../../../src/read-models/shared-state/member-linking';
 
 describe('Group membership numbers', () => {
@@ -17,6 +19,14 @@ describe('Group membership numbers', () => {
       ],
       output: [new Set([1, 2, 3]), new Set([4, 5])],
     },
+    {
+      input: [[1, 2], [2]],
+      output: [new Set([1, 2])],
+    },
+    {
+      input: [[1], [2], [3], [1, 3]],
+      output: [new Set([1, 3]), new Set([2])],
+    },
   ];
 
   tests.forEach((data, index) => {
@@ -25,7 +35,15 @@ describe('Group membership numbers', () => {
       for (const i of data.input) {
         linking.link(i);
       }
-      expect(linking.all()).toStrictEqual(data.output);
+      const sorting = sort<ReadonlySet<number>>({
+        compare: (a, b) =>
+          S.Ord.compare(
+            Array.from(a.values()).join(','),
+            Array.from(b.values()).join(',')
+          ),
+        equals: (a, b) => a === b,
+      });
+      expect(sorting(linking.all())).toStrictEqual(sorting(data.output));
     });
   });
 });
