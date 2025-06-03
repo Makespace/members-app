b472f0882540d3ba22e62080cb5b299540e28332
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-06-03 22:10:39 +0100

    Add sentry tracing to async refresh as a tester to check it works

--- src/read-models/shared-state/async-refresh.ts
+++ src/read-models/shared-state/async-refresh.ts
@@ -4,6 +4,7 @@ import {getAllEvents} from '../../init-dependencies/event-store/get-all-events';
 import {pipe} from 'fp-ts/lib/function';
 import {DomainEvent} from '../../types';
 import * as TE from 'fp-ts/TaskEither';
+import {Span, startSpan} from '@sentry/node';
 
 function payloadToString(payload: unknown): string {
   return JSON.stringify(payload);
@@ -14,19 +15,36 @@ export const asyncRefresh = (
   updateState: (event: DomainEvent) => void
 ) => {
   let knownEvents = 0;
-  return () => async () => {
-    const events = await pipe(
-      getAllEvents(eventStoreDb)(),
-      TE.getOrElse(failure => {
-        throw new Error(
-          `unexpected Left from getAllEvents: ${failure.message} ${payloadToString(failure.payload)}`
+  return () => () =>
+    startSpan(
+      {
+        name: 'Async Refresh',
+        attributes: {
+          known_events_before: knownEvents,
+        },
+      },
+      async (parentSpan: Span) => {
+        const events = await startSpan(
+          {name: 'Get All Events'},
+          async (span: Span) => {
+            const result = await pipe(
+              getAllEvents(eventStoreDb)(),
+              TE.getOrElse(failure => {
+                throw new Error(
+                  `unexpected Left from getAllEvents: ${failure.message} ${payloadToString(failure.payload)}`
+                );
+              })
+            )();
+            span.setAttribute('event_count', result.length);
+            return result;
+          }
         );
-      })
-    )();
 
-    if (events.length > knownEvents) {
-      events.slice(knownEvents - events.length).forEach(updateState);
-      knownEvents = events.length;
-    }
-  };
+        if (events.length > knownEvents) {
+          events.slice(knownEvents - events.length).forEach(updateState);
+          knownEvents = events.length;
+        }
+        parentSpan.setAttribute('known_events_after', knownEvents);
+      }
+    );
 };
