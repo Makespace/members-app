5a31f627c88605a418232a5acc77c7fd4b580013
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-06-16 00:04:08 +0100

    Remove unused

--- src/read-models/equipment/index.ts
+++ src/read-models/equipment/index.ts
@@ -1,11 +1,9 @@
 import {get} from './get';
 import {getForArea} from './get-for-area';
-import {getTrainingQuizResults} from './get-training-quiz-results';
 import {getTrainingSheetId} from './get-training-sheet-id';
 
 export const equipment = {
   get,
   getForArea,
   getTrainingSheetId,
-  getTrainingQuizResults,
 };
