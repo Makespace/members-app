b207621ba0833c2ae49df9dca8d114de29a78731
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-06-17 19:27:28 +0100

    Use a simple bash script to run the sync and web workers in a single fly instance

---
+++ fly-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# I had a lots of backwards/forwards on how to handle doing some work in the background
+# and decided on this approach as a balance between simplicity and ease to reason about.
+
+set -m
+"node", "--import", "./instrument.mjs", "build/src/sync_worker/index.js" &
+"node", "--import", "./instrument.mjs", "build/src/index.js" &
+fg %1
