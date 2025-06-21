907ae3ed8347349cec460ccb2615f49a72fc5e51
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-06-21 13:54:24 +0100

    Db tests for getting rows to sync

--- tests/sync-worker/util.ts
+++ tests/sync-worker/util.ts
@@ -1,7 +1,7 @@
 import {UUID} from 'io-ts-types/lib/UUID';
 import {constructEvent} from '../../src/types';
 import {EventOfType} from '../../src/types/domain-event';
-import pino from 'pino';
+import pino, {Logger} from 'pino';
 import {SyncTrainingSheetDependencies} from '../../src/sync-worker/sync_training_sheet';
 import {Client} from '@libsql/client';
 import {getTrainingSheetsToSync} from '../../src/sync-worker/db/get_training_sheets_to_sync';
@@ -14,14 +14,22 @@ import * as N from 'fp-ts/number';
 import {contramap} from 'fp-ts/lib/Ord';
 import {ManualParsedTrainingSheetEntry} from '../data/google_sheet_data';
 import {SheetDataTable} from '../../src/sync-worker/google/sheet-data-table';
+import {commitEvent} from '../../src/init-dependencies/event-store/commit-event';
 
-export const generateRegisterEvent = (
+export const generateRegisterSheetEvent = (
   equipmentId: UUID,
   trainingSheetId: string
 ): EventOfType<'EquipmentTrainingSheetRegistered'> =>
   constructEvent('EquipmentTrainingSheetRegistered')({
     equipmentId,
     trainingSheetId,
+  });
+
+export const generateRemoveSheetEvent = (
+  equipmentId: UUID
+): EventOfType<'EquipmentTrainingSheetRemoved'> =>
+  constructEvent('EquipmentTrainingSheetRemoved')({
+    equipmentId,
   });
 
 export const testLogger = () =>
@@ -49,3 +57,34 @@ export const byTimestamp = pipe(
       : d.timestampEpochMS
   )
 );
+
+export const pushEvents = async (
+  db: Client,
+  logger: Logger,
+  events: ReadonlyArray<
+    | EventOfType<'EquipmentTrainingSheetRegistered'>
+    | EventOfType<'EquipmentTrainingSheetRemoved'>
+  >
+) => {
+  if (events.length === 0) {
+    return;
+  }
+  await commitEvent(db, logger, () => async () => {})(
+    {
+      type: 'equipment',
+      id: '0', // For the purpose of these tests we can just use the same 'equipment' for concurrency control.
+    },
+    'no-such-resource'
+  )(events[0])();
+  let resourceVersion = 1;
+  for (const event of events.slice(1)) {
+    await commitEvent(db, logger, () => async () => {})(
+      {
+        type: 'equipment',
+        id: '0',
+      },
+      resourceVersion
+    )(event)();
+    resourceVersion++;
+  }
+};
