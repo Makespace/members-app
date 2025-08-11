54712e3b7e579807a66e4a70b80e67b2638ce5ba
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-08-11 18:43:44 +0100

    Render members list to include all member numbers

--- src/queries/members/view-model.ts
+++ src/queries/members/view-model.ts
@@ -1,4 +1,4 @@
-import {Member} from '../../read-models/members';
+import {Member} from '../../read-models/shared-state/return-types';
 
 export type ViewModel = {
   members: ReadonlyArray<Member>;
