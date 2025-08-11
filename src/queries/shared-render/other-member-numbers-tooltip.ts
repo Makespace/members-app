01428ea02a301abb1e4adecaf0c0008a11669906
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-08-11 20:17:07 +0100

    Tool tip on member page for other member numbers

---
+++ src/queries/shared-render/other-member-numbers-tooltip.ts
@@ -0,0 +1,10 @@
+import {html} from '../../types/html';
+
+export const otherMemberNumbersTooltip = html`
+  <div class="tooltip">
+    <i class="fa-regular fa-circle-question"></i>
+    <span class="tooltiptext">
+      Users may have multiple member numbers if they have left and then rejoined
+    </span>
+  </div>
+`;
