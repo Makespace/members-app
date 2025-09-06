d744e101d9f9eb24dcd666d90da3362d57f46ef0
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-09-06 08:55:01 +0100

    Continuing work on member input minimal

--- src/templates/member-input-minimal.ts
+++ src/templates/member-input-minimal.ts
@@ -1,6 +1,11 @@
+import {Member} from '../read-models/shared-state/return-types';
 import {html, Html} from '../types/html';
 
-export const memberNumberInputMinimal = (field: string): Html => {
+export const memberNumberInputMinimal = (
+  field: string,
+  label: string,
+  members: Pick<Member, 'memberNumber' | 'name' | 'emailAddress'>[]
+): Html => {
   return html`
     <label for="trainedByMemberNumber">Who did the training?</label>
     <input
