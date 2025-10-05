7015e97fb6610b35a2b1e38370b090cfdd64d64e
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-10-05 10:23:04 +0100

    Get members not already trained on a piece of equipment

--- src/templates/member-input-minimal.ts
+++ src/templates/member-input-minimal.ts
@@ -4,7 +4,7 @@ import {html, Html} from '../types/html';
 export const memberNumberInputMinimal = (
   field: string,
   label: string,
-  members: Pick<Member, 'memberNumber' | 'name' | 'emailAddress'>[]
+  members: ReadonlyArray<Pick<Member, 'memberNumber' | 'name' | 'emailAddress'>>
 ): Html => {
   return html`
     <label for="trainedByMemberNumber">Who did the training?</label>
