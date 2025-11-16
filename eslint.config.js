0e66cdbd1b8571f226729630df3d947c3a961a13
Paul Lancaster <paul.la.lancaster@gmail.com>
2025-11-16 12:56:44 +0000

    To flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,68 +1,67 @@
-const {
-    defineConfig,
-} = require("eslint/config");
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const { defineConfig } = require('eslint/config');
+const unusedImports = require('eslint-plugin-unused-imports');
+const pluginJest = require('eslint-plugin-jest');
 
-const unusedImports = require("eslint-plugin-unused-imports");
-const js = require("@eslint/js");
+module.exports = defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        files: ["**/*.ts"],
+        rules: {
+            "@typescript-eslint/unbound-method": "off",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/require-await": "off",
+            "unused-imports/no-unused-imports": "error",
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-module.exports = defineConfig([{
-    plugins: {
-        "unused-imports": unusedImports,
-    },
-}, {
-    files: ["**/*.ts"],
-
-    extends: compat.extends(
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    ),
-
-    languageOptions: {
-        parserOptions: {
-            project: ["./tsconfig.json"],
+            "unused-imports/no-unused-vars": ["error", {
+                vars: "all",
+                varsIgnorePattern: "^_",
+                args: "after-used",
+                argsIgnorePattern: "^_",
+            }],
         },
-    },
-
-    rules: {
-        "@typescript-eslint/unbound-method": "off",
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/require-await": "off",
-        "unused-imports/no-unused-imports": "error",
-
-        "unused-imports/no-unused-vars": ["error", {
-            vars: "all",
-            varsIgnorePattern: "^_",
-            args: "after-used",
-            argsIgnorePattern: "^_",
-        }],
-    },
-}, {
-    files: ["**/*.test.ts"],
-
-    extends: compat.extends(
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    ),
-
-    languageOptions: {
-        parserOptions: {
-            project: ["./tsconfig.json"],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
         },
+        plugins: {
+            "unused-imports": unusedImports,
+        }
     },
+    {
+        files: ["**/*.test.ts"],
+        ...pluginJest.configs['flat/recommended'],
+        rules: {
+            ...pluginJest.configs['flat/recommended'].rules,
+            "@typescript-eslint/unbound-method": "off",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/require-await": "off",
+            "unused-imports/no-unused-imports": "error",
 
-    rules: {
-        "@typescript-eslint/unbound-method": "off",
-        "@typescript-eslint/no-unsafe-assignment": "off",
+            "unused-imports/no-unused-vars": ["error", {
+                vars: "all",
+                varsIgnorePattern: "^_",
+                args: "after-used",
+                argsIgnorePattern: "^_",
+            }],
+        },
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        plugins: {
+            "unused-imports": unusedImports,
+            jest: pluginJest,
+        }
     },
-}]);
+    {
+        files: ['**/*.js', '**/*.mjs'],
+        extends: [tseslint.configs.disableTypeChecked],
+    }
+);
