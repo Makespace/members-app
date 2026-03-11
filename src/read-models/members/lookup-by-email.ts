c026072f658aacfcb9be2f16a399f759f0f33616
Paul Lancaster <paul.la.lancaster@gmail.com>
2026-03-11 19:45:37 +0000

    Remove unused email lookup (#174)

--- src/read-models/members/lookup-by-email.ts
+++ src/read-models/members/lookup-by-email.ts
@@ -1,17 +1,6 @@
 import * as RA from 'fp-ts/ReadonlyArray';
-import * as O from 'fp-ts/Option';
 import {DomainEvent, EmailAddress, isEventOfType} from '../../types';
 import {pipe} from 'fp-ts/lib/function';
-
-export const lookupByEmail =
-  (email: string) =>
-  (events: ReadonlyArray<DomainEvent>): O.Option<number> =>
-    pipe(
-      events,
-      RA.filter(isEventOfType('MemberNumberLinkedToEmail')),
-      RA.findFirst(event => event.email === email),
-      O.map(event => event.memberNumber)
-    );
 
 export const lookupByCaseInsensitiveEmail =
   (email: string) =>
