20da9256c63434def153a0dd8c04e135faa82a18
Paul Lancaster <paul.la.lancaster@gmail.com>
2026-03-11 19:45:37 +0000

    Remove unused email lookup (#174)

--- tests/read-models/members/lookup-by-email.test.ts
+++ tests/read-models/members/lookup-by-email.test.ts
@@ -1,69 +1,9 @@
-import * as O from 'fp-ts/Option';
 import {
-  lookupByEmail,
   lookupByCaseInsensitiveEmail,
 } from '../../../src/read-models/members/lookup-by-email';
 import {faker} from '@faker-js/faker';
 import {DomainEvent, EmailAddress} from '../../../src/types';
 import {TestFramework, initTestFramework} from '../test-framework';
-
-describe('lookupByEmail', () => {
-  let events: ReadonlyArray<DomainEvent>;
-  let framework: TestFramework;
-  beforeEach(async () => {
-    framework = await initTestFramework();
-  });
-  afterEach(() => {
-    framework.close();
-  });
-
-  describe('when no members exist', () => {
-    beforeEach(async () => {
-      events = await framework.getAllEvents();
-    });
-
-    it('returns none', () => {
-      const result = lookupByEmail(faker.internet.email())(events);
-      expect(result).toStrictEqual(O.none);
-    });
-  });
-
-  describe('when a member with the given email exists', () => {
-    const command = {
-      memberNumber: faker.number.int(),
-      email: faker.internet.email() as EmailAddress,
-      name: undefined,
-      formOfAddress: undefined,
-    };
-    beforeEach(async () => {
-      await framework.commands.memberNumbers.linkNumberToEmail(command);
-      events = await framework.getAllEvents();
-    });
-
-    it('returns their member number', () => {
-      const result = lookupByEmail(command.email)(events);
-      expect(result).toStrictEqual(O.some(command.memberNumber));
-    });
-  });
-
-  describe('when no member with the given email exists', () => {
-    const command = {
-      memberNumber: faker.number.int(),
-      email: faker.internet.email() as EmailAddress,
-      name: undefined,
-      formOfAddress: undefined,
-    };
-    beforeEach(async () => {
-      await framework.commands.memberNumbers.linkNumberToEmail(command);
-      events = await framework.getAllEvents();
-    });
-
-    it('returns none', () => {
-      const result = lookupByEmail(faker.internet.email())(events);
-      expect(result).toStrictEqual(O.none);
-    });
-  });
-});
 
 describe('lookupByCaseInsensitiveEmail', () => {
   let events: ReadonlyArray<DomainEvent>;
