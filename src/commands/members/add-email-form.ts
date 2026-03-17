1590c1d4daf1710b7b6f0ee8ed6e3ef0ec482060
Paul Lancaster <paul.la.lancaster@gmail.com>
2026-03-17 22:11:34 +0000

    Working form to add email

--- src/commands/members/add-email-form.ts
+++ src/commands/members/add-email-form.ts
@@ -1,32 +1,24 @@
-import {flow, pipe} from 'fp-ts/lib/function';
+import {pipe} from 'fp-ts/lib/function';
 import * as E from 'fp-ts/Either';
 import {html, safe, toLoggedInContent} from '../../types/html';
 import {User} from '../../types';
 import {Form} from '../../types/form';
-import * as t from 'io-ts';
-import * as tt from 'io-ts-types';
-import {formatValidationErrors} from 'io-ts-reporters';
-import {failureWithStatus} from '../../types/failure-with-status';
-import {StatusCodes} from 'http-status-codes';
 
 type ViewModel = {
   user: User; // The user logged in
-  memberNumber: number; // The user we are updating the email for.
-
-  // TODO - Show the emails already registered for this user.
 };
 
 const renderForm = (viewModel: ViewModel) =>
   pipe(
     html`
       <h1>Add email</h1>
-      <form action="?next=/member/${viewModel.memberNumber}" method="post">
+      <form action="?next=/member/${viewModel.user.memberNumber}" method="post">
         <label for="email">Email address</label>
         <input type="email" name="email" id="email" />
         <input
           type="hidden"
           name="memberNumber"
-          value="${viewModel.memberNumber}"
+          value="${viewModel.user.memberNumber}"
         />
         <button type="submit">Add email</button>
       </form>
@@ -34,30 +26,11 @@ const renderForm = (viewModel: ViewModel) =>
     toLoggedInContent(safe('Add email'))
   );
 
-const paramsCodec = t.strict({
-  member: tt.NumberFromString,
-});
-
 const constructForm: Form<ViewModel>['constructForm'] =
-  input =>
-  ({user}) =>
-    pipe(
-      input,
-      paramsCodec.decode,
-      E.mapLeft(
-        flow(
-          formatValidationErrors,
-          failureWithStatus(
-            'Parameters submitted to the form were invalid',
-            StatusCodes.BAD_REQUEST
-          )
-        )
-      ),
-      E.map(params => ({
-        user,
-        memberNumber: params.member,
-      }))
-    );
+  _input =>
+  ({user}) => E.right({
+    user
+  });
 
 export const addEmailForm: Form<ViewModel> = {
   renderForm,
