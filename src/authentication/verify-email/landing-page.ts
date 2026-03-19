66ba9f1f87819771cbb222f58d178d47c4bdfeac
Paul Lancaster <paul.la.lancaster@gmail.com>
2026-03-19 22:45:43 +0000

    Landing page for verifying email working

--- src/authentication/verify-email/landing-page.ts
+++ src/authentication/verify-email/landing-page.ts
@@ -21,26 +21,16 @@ const invalidLink = () => oopsPage(
 const verificationSuccessful = pipe(
   html`
     <div class="container">
-      <div class="max-w-md mx-auto">
-        <h1 class="mb-6">Log in</h1>
-        <form action="/auth" method="post">
-          <label for="email">E-Mail: </label>
-          <input
-            id="email"
-            type="email"
-            required
-            name="email"
-            value=""
-            class="mb-2"
-          />
-          <p class="text-sm text-gray mb-6">
-            Enter email associated with your MakeSpace membership.
-          </p>
-          <button type="submit" class="w-full mt-6 mb-6">
-            Email me a login link
-          </button>
-        </form>
-      </div>
+      <a href="/"><img
+          width="64"
+          height="64"
+          src="/static/MS-LOGO-txpt-512.png"
+          alt="Makespace"
+          class="page-nav__logo"
+      /></a>
+        <h1 class="mb-6">Email Verified Successfully</h1>
+        Thank you!
+        <a href=/me>Back to your homepage</a>
     </div>
   `,
   isolatedPageTemplate(safe('MakeSpace Members App'))
