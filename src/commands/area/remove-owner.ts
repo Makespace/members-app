0d00427b7f959af9aa2d6740d7bda79de3aaafbd
Paul Lancaster <paul.la.lancaster@gmail.com>
2026-03-29 19:02:15 +0100

    Make remove-owner more susinct

--- src/commands/area/remove-owner.ts
+++ src/commands/area/remove-owner.ts
@@ -9,6 +9,7 @@ import * as RA from 'fp-ts/ReadonlyArray';
 import {constructEvent, isEventOfType} from '../../types';
 import {StatusCodes} from 'http-status-codes';
 import {failureWithStatus} from '../../types/failure-with-status';
+import {filterByName} from '../../types/domain-event';
 
 const codec = t.strict({
   memberNumber: tt.NumberFromString,
@@ -18,55 +19,37 @@ const codec = t.strict({
 type RemoveOwner = t.TypeOf<typeof codec>;
 
 const process: Command<RemoveOwner>['process'] = input => {
-   if (input.events.length === 0) {
-     return TE.left(
-       failureWithStatus(
-         'The requested area does not exist',
-         StatusCodes.NOT_FOUND
-       )()
-     );
-   }
-   if (pipe(input.events, RA.some(isEventOfType('AreaRemoved')))) {
-     return TE.left(
-       failureWithStatus(
-         'The requested area does not exist',
-         StatusCodes.NOT_FOUND
-       )()
-     );
-   }
+  if (
+    input.events.length === 0 ||
+    pipe(input.events, RA.some(isEventOfType('AreaRemoved')))
+  ) {
+    return TE.left(
+      failureWithStatus(
+        'The requested area does not exist',
+        StatusCodes.NOT_FOUND
+      )()
+    );
+  }
 
   return pipe(
-    pipe(
-      input.events,
-      RA.filter(e => {
-        const isAdded = isEventOfType('OwnerAdded')(e);
-        const isRemoved  = isEventOfType('OwnerRemoved')(e);
-        if (!isAdded && !isRemoved) {
-          return false;
-        }
-
-        if (e.areaId !== input.command.areaId || e.memberNumber !== input.command.memberNumber) {
-          return false;
-        }
-
-        return true;
-      }),
-      RA.last,
-      O.filter(isEventOfType('OwnerAdded')),
-      O.map(() => constructEvent('OwnerRemoved')(input.command))
+    input.events,
+    filterByName(['OwnerAdded', 'OwnerRemoved']),
+    RA.filter(
+      event =>
+        event.areaId === input.command.areaId &&
+        event.memberNumber === input.command.memberNumber
     ),
-    O.match(
-      () =>
-        TE.left(
-          failureWithStatus(
-            'The requested member is not an owner of the requested area',
-            StatusCodes.BAD_REQUEST
-          )()
-        ),
-      event => TE.right(O.some(event))
-    )
+    RA.last,
+    O.filter(isEventOfType('OwnerAdded')),
+    TE.fromOption(() =>
+      failureWithStatus(
+        'The requested member is not an owner of the requested area',
+        StatusCodes.BAD_REQUEST
+      )()
+    ),
+    TE.map(() => O.some(constructEvent('OwnerRemoved')(input.command)))
   );
-}
+};
 
 const resource: Command<RemoveOwner>['resource'] = command => ({
   type: 'Area',
