6e4024b34bc34cd79fbfc3de78db4b69769bb1f1
Paul Lancaster <paul.la.lancaster@gmail.com>
2026-04-19 09:20:36 +0100

    Group and display failed events by event type (#215)
    * Prevent failed event duplication
    
    * Test that shows failed events not duplicated
    
    * Record failed event type in table
    
    * Group and display failed events by event type

--- src/queries/failed-event-log/view-model.ts
+++ src/queries/failed-event-log/view-model.ts
@@ -1,16 +1,11 @@
 import {User} from '../../types';
 
-export interface FailedEventLogSearch {
-  offset: number;
-  limit: number;
-}
-
 export type ViewModel = {
   user: User;
   count: number;
-  search: FailedEventLogSearch;
   failures: ReadonlyArray<{
     error: string;
+    eventType: string;
     payload: unknown;
   }>;
 };
