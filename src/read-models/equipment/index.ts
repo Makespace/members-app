e1cea194e4a66f3a152f426fa0c8333112d7923f
Paul Lancaster <paul.la.lancaster@gmail.com>
2026-04-20 22:38:29 +0100

    Remove obsolete equipment read model

--- src/read-models/equipment/index.ts
+++
@@ -1,9 +0,0 @@
-import {get} from './get';
-import {getForArea} from './get-for-area';
-import {getTrainingSheetId} from './get-training-sheet-id';
-
-export const equipment = {
-  get,
-  getForArea,
-  getTrainingSheetId,
-};
--- src/read-models/index.ts
+++ src/read-models/index.ts
@@ -1,11 +1,9 @@
 import {members} from './members';
-import {equipment} from './equipment';
 import {superUsers} from './super-users';
 import {trainingStatNotifications} from './training-stat-notifications';
 
 export const readModels = {
   members,
   superUsers,
-  equipment,
   trainingStatNotifications,
 };
